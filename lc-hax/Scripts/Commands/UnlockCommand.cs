15b2e457aca4b83b75c33d7868b2ac0ea4089702
winstxnhdw <winstxnhdw@gmail.com>
2023-12-04 18:52:15 +0800

    feat: sync door unlock to server

--- lc-hax/Scripts/Commands/UnlockCommand.cs
+++ lc-hax/Scripts/Commands/UnlockCommand.cs
@@ -6,7 +6,19 @@ namespace Hax;
 
 public class UnlockCommand : ICommand {
     public void Execute(string[] args) {
-        Object.FindObjectsOfType<DoorLock>().ToList().ForEach(door => door.UnlockDoor());
+        Object.FindObjectsOfType<DoorLock>().ToList().ForEach(door => {
+            door.UnlockDoor();
+            door.UnlockDoorSyncWithServer();
+
+            InteractTrigger? doorTrigger = Reflector.Target(door).GetInternalField<InteractTrigger>("doorTrigger");
+
+            if (doorTrigger == null) {
+                return;
+            }
+
+            doorTrigger.timeToHold = 0.0f;
+        });
+
         Console.Print("SYSTEM", "All doors unlocked!");
     }
 }
