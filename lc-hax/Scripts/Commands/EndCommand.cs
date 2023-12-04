eff6c09504855c068bdf3c896a9fb568e5997451
winstxnhdw <winstxnhdw@gmail.com>
2023-12-04 19:26:23 +0800

    fix: end game must call server rpc

--- lc-hax/Scripts/Commands/EndCommand.cs
+++ lc-hax/Scripts/Commands/EndCommand.cs
@@ -2,6 +2,6 @@ namespace Hax;
 
 public class EndCommand : ICommand {
     public void Execute(string[] args) {
-        StartOfRound.Instance.EndGameClientRpc(-1);
+        StartOfRound.Instance.EndGameServerRpc(-1);
     }
 }
