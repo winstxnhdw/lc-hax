e8105cfd85208b2acdde637164722cb2fddf9278
winstxnhdw <winstxnhdw@gmail.com>
2023-12-05 04:48:36 +0800

    feat: apply home command to others

--- lc-hax/Scripts/Commands/HomeCommand.cs
+++ lc-hax/Scripts/Commands/HomeCommand.cs
@@ -1,8 +1,43 @@
+using System.Linq;
+using GameNetcodeStuff;
 
 namespace Hax;
 
 public class HomeCommand : ICommand {
+    PlayerControllerB? GetPlayer(string name) => StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerUsername == name);
+
+    MultiObjectPool<ShipTeleporter>? ShipTeleporters => HaxObjects.Instance?.ShipTeleporters;
+
+    Result TeleportPlayerToBase(string[] args) {
+        string sourcePlayerName = args[0];
+        PlayerControllerB? sourcePlayer = this.GetPlayer(sourcePlayerName);
+
+        if (sourcePlayer == null) {
+            return new Result(message: "Player not found!");
+        }
+
+        Helpers.BuyUnlockable(Unlockables.TELEPORTER);
+        this.ShipTeleporters?.Renew();
+        ShipTeleporter? teleporter = this.ShipTeleporters?.Objects.FirstOrDefault(teleporter => !teleporter.isInverseTeleporter);
+
+        if (teleporter == null) {
+            return new Result(message: "ShipTeleporter not found!");
+        }
+
+        teleporter.PressTeleportButtonServerRpc();
+        return new Result(true);
+    }
+
     public void Execute(string[] args) {
-        StartOfRound.Instance.ForcePlayerIntoShip();
+        if (args.Length < 1) {
+            StartOfRound.Instance.ForcePlayerIntoShip();
+            return;
+        }
+
+        Result result = this.TeleportPlayerToBase(args);
+
+        if (!result.Success) {
+            Console.Print("SYSTEM", result.Message);
+        }
     }
 }
