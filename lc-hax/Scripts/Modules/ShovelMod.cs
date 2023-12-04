01994375526588c90770c0254e079f9f0235072d
winstxnhdw <winstxnhdw@gmail.com>
2023-12-04 16:20:42 +0800

    refactor: propagate nulls

--- lc-hax/Scripts/Modules/ShovelMod.cs
+++ lc-hax/Scripts/Modules/ShovelMod.cs
@@ -6,18 +6,19 @@ namespace Hax;
 
 public class ShovelMod : MonoBehaviour {
     void Update() {
-        PlayerControllerB? player = HaxObjects.Instance?.GameNetworkManager.Object?.localPlayerController;
+        PlayerControllerB? localPlayer = HaxObjects.Instance?.GameNetworkManager.Object?.localPlayerController;
+        Shovel? localPlayerShovel =
+            HaxObjects.Instance?
+                      .Shovels
+                      .Objects?
+                      .FirstOrDefault(shovel =>
+            shovel.playerHeldBy.playerClientId == localPlayer?.playerClientId
+        );
 
-        if (player == null) {
+        if (localPlayerShovel == null) {
             return;
         }
 
-        Shovel[]? shovels = HaxObjects.Instance?.Shovels.Objects;
-
-        if (shovels == null) {
-            return;
-        }
-
-        shovels.First(shovel => shovel.playerHeldBy.playerClientId == player.playerClientId).shovelHitForce = Settings.ShovelHitForce;
+        localPlayerShovel.shovelHitForce = Settings.ShovelHitForce;
     }
 }
