56391099b9f4031f82f1379d9826cb0fbcd6c869
winstxnhdw <winstxnhdw@gmail.com>
2023-12-07 14:53:39 +0800

    feat/docs: add `heal` command

---
+++ lc-hax/Scripts/Commands/HealCommand.cs
@@ -0,0 +1,20 @@
+using GameNetcodeStuff;
+
+namespace Hax;
+
+public class HealCommand : ICommand {
+    public void Execute(string[] _) {
+        if (!Helpers.Extant(Helpers.LocalPlayer, out PlayerControllerB localPlayer)) {
+            Console.Print("SYSTEM", "Player not found");
+            return;
+        }
+
+        if (!Helpers.Extant(Helpers.HUDManager, out HUDManager hudManager)) {
+            Console.Print("SYSTEM", "HUDManager not found");
+            return;
+        }
+
+        localPlayer.health = 100;
+        hudManager.UpdateHealthUI(localPlayer.health, false);
+    }
+}
