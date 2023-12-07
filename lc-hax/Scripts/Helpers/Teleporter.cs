8f3f5807bf6e0d50c150cddab7e799d7c69bf17b
winstxnhdw <winstxnhdw@gmail.com>
2023-12-07 22:42:13 +0800

    refactor: use singleton instance when possible

--- lc-hax/Scripts/Helpers/Teleporter.cs
+++ lc-hax/Scripts/Helpers/Teleporter.cs
@@ -5,7 +5,7 @@ namespace Hax;
 public static partial class Helpers {
     static ShipTeleporter[]? ShipTeleporters => HaxObjects.Instance?.ShipTeleporters.Objects;
 
-    public static ShipTeleporter? InverseTeleporter => ShipTeleporters?.FirstOrDefault(teleporter => teleporter.isInverseTeleporter);
+    public static ShipTeleporter? InverseTeleporter => Helpers.ShipTeleporters?.FirstOrDefault(teleporter => teleporter.isInverseTeleporter);
 
-    public static ShipTeleporter? Teleporter => ShipTeleporters?.FirstOrDefault(teleporter => !teleporter.isInverseTeleporter);
+    public static ShipTeleporter? Teleporter => Helpers.ShipTeleporters?.FirstOrDefault(teleporter => !teleporter.isInverseTeleporter);
 }
