16de56ac87216f8d57aeed020f1e1efc6fb0ea54
winstxnhdw <winstxnhdw@gmail.com>
2023-12-08 16:40:04 +0800

    refactor: clean up night vision

---
+++ lc-hax/Scripts/Modules/ClearVisionMod.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace Hax;
+
+public class ClearVisionMod : MonoBehaviour {
+    IEnumerator SetClearVision() {
+        HDAdditionalLightData? lightData = null;
+
+        while (true) {
+            if (!Helpers.Extant(TimeOfDay.Instance, out TimeOfDay timeOfDay)) {
+                yield return new WaitForEndOfFrame();
+                continue;
+            }
+
+            if (!Helpers.Extant(StartOfRound.Instance, out StartOfRound startOfRound)) {
+                yield return new WaitForEndOfFrame();
+                continue;
+            }
+
+            if (timeOfDay.sunIndirect == null) {
+                yield return new WaitForEndOfFrame();
+                continue;
+            }
+
+            if (lightData == null) {
+                lightData = timeOfDay.sunIndirect.GetComponent<HDAdditionalLightData>();
+                yield return new WaitForEndOfFrame();
+                continue;
+            }
+
+            lightData.lightDimmer = float.MaxValue;
+            lightData.distance = float.MaxValue;
+            timeOfDay.insideLighting = false;
+            startOfRound.blackSkyVolume.weight = 0;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    void Start() {
+        _ = this.StartCoroutine(this.SetClearVision());
+    }
+}
