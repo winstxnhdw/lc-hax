1612c5c19fbbdaa3c8077330f8467e75988007cb
winstxnhdw <winstxnhdw@gmail.com>
2023-12-08 16:40:04 +0800

    refactor: clean up night vision

--- lc-hax/Scripts/Loader.cs
+++ lc-hax/Scripts/Loader.cs
@@ -61,7 +61,7 @@ public class Loader : MonoBehaviour {
         Loader.AddHaxModules<ShovelMod>();
         Loader.AddHaxModules<WeightMod>();
         Loader.AddHaxModules<SaneMod>();
-        Loader.AddHaxModules<NightVisionMod>();
+        Loader.AddHaxModules<ClearVisionMod>();
         Loader.AddHaxModules<AlwaysChatMod>();
     }
 
