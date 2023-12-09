dec4af50e297275365d058a751ab5edade80880c
winstxnhdw <winstxnhdw@gmail.com>
2023-12-09 18:20:44 +0800

    feat: try closing the door every frame

--- lc-hax/Scripts/Commands/LockCommand.cs
+++ lc-hax/Scripts/Commands/LockCommand.cs
@@ -1,13 +1,25 @@
 
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 namespace Hax;
 
 public class LockCommand : ICommand {
-    public void Execute(string[] _) {
+    public void Execute(string[] args) {
+        List<DoorLock> doors = [.. Object.FindObjectsOfType<DoorLock>()];
+
+        _ = Helper.CreateComponent<TransientBehaviour>().Init((_) => {
+            doors.ForEach(door => {
+                AnimatedObjectTrigger animatedObjectTrigger = door.gameObject.GetComponent<AnimatedObjectTrigger>();
+                animatedObjectTrigger.boolValue = false;
+                animatedObjectTrigger.TriggerAnimation(Helper.GetPlayer(0));
+            });
+        }, 60.0f);
+
         Object.FindObjectsOfType<TerminalAccessibleObject>()
               .ToList()
               .ForEach(terminalAccessibleObject => terminalAccessibleObject.SetDoorOpenServerRpc(false));
+
     }
 }
