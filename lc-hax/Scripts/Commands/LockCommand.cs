32192fa1718ee0f6e9d15976e3d6e83ccf1b814a
winstxnhdw <winstxnhdw@gmail.com>
2023-12-11 21:40:57 +0800

    fix: remove door close experiment

--- lc-hax/Scripts/Commands/LockCommand.cs
+++ lc-hax/Scripts/Commands/LockCommand.cs
@@ -5,16 +5,6 @@ namespace Hax;
 
 public class LockCommand : ICommand {
     public void Execute(string[] args) {
-        ParallelQuery<DoorLock> doors = Object.FindObjectsOfType<DoorLock>().ToList().AsParallel();
-
-        _ = Helper.CreateComponent<TransientBehaviour>().Init((_) => {
-            doors.ForAll(door => {
-                AnimatedObjectTrigger animatedObjectTrigger = door.gameObject.GetComponent<AnimatedObjectTrigger>();
-                animatedObjectTrigger.boolValue = false;
-                animatedObjectTrigger.TriggerAnimation(Helper.GetPlayer(0));
-            });
-        }, 60.0f);
-
         Object.FindObjectsOfType<TerminalAccessibleObject>()
               .ToList()
               .ForEach(terminalAccessibleObject => terminalAccessibleObject.SetDoorOpenServerRpc(false));
