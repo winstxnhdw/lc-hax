593084a23d413d0511793483c104e51b3f6f5129
winstxnhdw <winstxnhdw@gmail.com>
2023-12-11 23:20:34 +0800

    style: rename to `StaminaMod`

--- lc-hax/Scripts/Modules/StaminaMod.cs
+++ lc-hax/Scripts/Modules/StaminaMod.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Hax;
 
-public class SprintMod : MonoBehaviour {
+public class StaminaMod : MonoBehaviour {
     IEnumerator SetSprint() {
         while (true) {
             if (!Helper.Extant(Helper.LocalPlayer, out PlayerControllerB player)) {
@@ -17,7 +17,7 @@ public class SprintMod : MonoBehaviour {
             player.isExhausted = false;
             player.sprintMeter = 1.0f;
 
-            yield return new WaitForSeconds(2.0f);
+            yield return new WaitForSeconds(3.0f);
         }
     }
 
