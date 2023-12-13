1485925c1347815324d7fc28366567472109d61f
LTYGUY <ltyguydev@gmail.com>
2023-12-13 19:56:19 +0800

    implemented teleporting to player by arrow keys, only in phantom mode

--- lc-hax/Scripts/Modules/PhantomMod.cs
+++ lc-hax/Scripts/Modules/PhantomMod.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using GameNetcodeStuff;
+using System;
 
 namespace Hax;
 
@@ -14,12 +15,64 @@ public class PhantomMod : MonoBehaviour {
     Quaternion ogLocalRot;
     Transform? ogParent;
 
+    int currentPlayerToTeleportToIndex = 0;
+
+    Action? goNextPlayerFunc = null;
+    Action? goPrevPlayerFunc = null;
+
     void OnEnable() {
+        this.goNextPlayerFunc = () => { this.GoToPlayer(1); };
+        this.goPrevPlayerFunc = () => { this.GoToPlayer(-1); };
+
         InputListener.onEqualsPress += this.TogglePhantom;
+        InputListener.onRightArrowKeyPress += this.goNextPlayerFunc;
+        InputListener.onLeftArrowKeyPress += this.goPrevPlayerFunc;
     }
 
     void OnDisable() {
         InputListener.onEqualsPress -= this.TogglePhantom;
+        InputListener.onRightArrowKeyPress -= this.goNextPlayerFunc;
+        InputListener.onLeftArrowKeyPress -= this.goPrevPlayerFunc;
+    }
+
+    void GoToPlayer(int indexChange) {
+        if (!this.ghostCamMode) return;
+        if (!Helper.Extant(Helper.CurrentCamera, out Camera camera)) return;
+
+        int currentPlayerCount = Helper.Players?.Length ?? 0;
+
+        if (currentPlayerCount <= 0) {
+            this.currentPlayerToTeleportToIndex = -1;
+            return;
+        }
+
+        int currentIndex = this.currentPlayerToTeleportToIndex;
+        currentIndex += indexChange;
+
+        currentIndex =
+        //if more than playercount, loop back to 0
+        currentIndex >= currentPlayerCount
+        ?
+        0
+        :
+        //if less than 0, loop forward
+            currentIndex < 0
+            ?
+            currentPlayerCount - 1
+            :
+            currentIndex;
+
+        PlayerControllerB? targetPlayer = Helper.Players?[currentIndex];
+        if (targetPlayer == null) {
+            this.Log($"Player is somehow null");
+            return;
+        }
+
+        this.Log($"Teleporting to {targetPlayer.playerUsername}, index={currentIndex}");
+        camera.transform.position = targetPlayer.playerEye.position;
+
+        this.currentPlayerToTeleportToIndex = currentIndex;
+
     }
 
     void TogglePhantom() {
@@ -34,12 +87,12 @@ public class PhantomMod : MonoBehaviour {
 
         this.toggleOn = !this.toggleOn;
 
-        this.Log("GHOST", $"{this.toggleOn} cam={camera}");
+        this.Log($"{this.toggleOn} cam={camera}");
 
         if (this.toggleOn) {
             if (this.ghostCamMode) return;
 
-            this.Log("GHOST", $"Enable cam");
+            this.Log($"Enable cam");
             player.enabled = false;
             this.ogLocalPos = camera.transform.localPosition;
             this.ogLocalRot = camera.transform.localRotation;
@@ -65,9 +118,9 @@ public class PhantomMod : MonoBehaviour {
         }
     }
 
-    private void Log(string tag, string msg) {
+    private void Log(string msg) {
         try {
-            Console.Print(tag, msg);
+            Console.Print("GHOST", msg);
 
         }
         catch {
