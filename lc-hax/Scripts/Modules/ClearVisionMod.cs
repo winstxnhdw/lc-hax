8a08250264c6002f36683e3247f870ad4ace9e58
LTYGUY <ltyguydev@gmail.com>
2023-12-13 19:27:07 +0800

    fixed clear vision, was not checking if sunindirect in null

--- lc-hax/Scripts/Modules/ClearVisionMod.cs
+++ lc-hax/Scripts/Modules/ClearVisionMod.cs
@@ -7,19 +7,15 @@ namespace Hax;
 public class ClearVisionMod : MonoBehaviour {
     IEnumerator SetClearVision() {
         HDAdditionalLightData? lightData = null;
-
         while (true) {
-            if (!Helper.Extant(TimeOfDay.Instance, out TimeOfDay timeOfDay)) {
+            if (!Helper.Extant(TimeOfDay.Instance, out TimeOfDay timeOfDay)
+                || !Helper.Extant(timeOfDay.sunIndirect, out Light sunIndirect)
+                || !Helper.Extant(Helper.StartOfRound, out StartOfRound startOfRound)) {
                 yield return new WaitForEndOfFrame();
                 continue;
             }
 
-            if (!Helper.Extant(Helper.StartOfRound, out StartOfRound startOfRound)) {
-                yield return new WaitForEndOfFrame();
-                continue;
-            }
-
-            lightData ??= timeOfDay.sunIndirect.GetComponent<HDAdditionalLightData>();
+            lightData ??= sunIndirect.GetComponent<HDAdditionalLightData>();
             lightData.lightDimmer = float.MaxValue;
             lightData.distance = float.MaxValue;
             timeOfDay.insideLighting = false;
