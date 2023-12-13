f91b6062f58840468b4a4b052d969aca0722517b
winstxnhdw <winstxnhdw@gmail.com>
2023-12-13 21:49:28 +0800

    refactor: use methods to get object placements

--- lc-hax/Scripts/Commands/RandomCommand.cs
+++ lc-hax/Scripts/Commands/RandomCommand.cs
@@ -4,43 +4,98 @@ using GameNetcodeStuff;
 namespace Hax;
 
 public class RandomCommand : ICommand {
+    ObjectPlacements<Transform, ShipTeleporter>? GetInverseTeleporterPlacements(Component target) {
+        Helper.BuyUnlockable(Unlockable.INVERSE_TELEPORTER);
+        HaxObject.Instance?.ShipTeleporters.Renew();
+
+        if (!Helper.Extant(Helper.InverseTeleporter, out ShipTeleporter inverseTeleporter)) {
+            return null;
+        }
+
+        Vector3 positionOffset = new(0.0f, 1.5f, 0.0f);
+        Vector3 rotationOffset = new(-90.0f, 0.0f, 0.0f);
+
+        ObjectPlacement<Transform, ShipTeleporter> teleporterPlacement = new(
+            target.transform,
+            inverseTeleporter,
+            positionOffset,
+            rotationOffset
+        );
+
+        ObjectPlacement<Transform, ShipTeleporter> previousTeleporterPlacement = new(
+            Helper.Copy(inverseTeleporter.transform).transform,
+            inverseTeleporter,
+            positionOffset,
+            rotationOffset
+        );
+
+        return new ObjectPlacements<Transform, ShipTeleporter>(
+            teleporterPlacement,
+            previousTeleporterPlacement
+        );
+    }
+
+    ObjectPlacements<Transform, PlaceableShipObject>? GetCupboardPlacements(Component target) {
+        Helper.BuyUnlockable(Unlockable.CUPBOARD);
+
+        if (!Helper.Extant(Helper.GetUnlockable(Unlockable.CUPBOARD), out PlaceableShipObject cupboard)) {
+            return null;
+        }
+
+        Vector3 positionOffset = new(0.0f, 1.75f, 0.0f);
+        Vector3 rotationOffset = new(-90.0f, 0.0f, 0.0f);
+
+        ObjectPlacement<Transform, PlaceableShipObject> cupboardPlacement = new(
+            target.transform,
+            cupboard,
+            positionOffset,
+            rotationOffset
+        );
+
+        ObjectPlacement<Transform, PlaceableShipObject> previousCupboardPlacement = new(
+            Helper.Copy(cupboard.transform).transform,
+            cupboard,
+            positionOffset,
+            rotationOffset
+        );
+
+        return new ObjectPlacements<Transform, PlaceableShipObject>(
+            cupboardPlacement,
+            previousCupboardPlacement
+        );
+    }
+
     Result TeleportPlayerToRandom(string[] args) {
         if (!Helper.Extant(Helper.GetPlayer(args[0]), out PlayerControllerB targetPlayer)) {
             return new Result(message: "Player not found!");
         }
 
-        Helper.BuyUnlockable(Unlockable.INVERSE_TELEPORTER);
-        HaxObject.Instance?.ShipTeleporters.Renew();
+        ObjectPlacements<Transform, ShipTeleporter>? teleporterPlacements = this.GetInverseTeleporterPlacements(targetPlayer);
 
-        if (!Helper.Extant(Helper.InverseTeleporter, out ShipTeleporter inverseTeleporter)) {
-            return new Result(message: "ShipTeleporter not found!");
+        if (teleporterPlacements is null) {
+            return new Result(message: "Inverse Teleporter not found!");
         }
 
-        if (!Helper.Extant(Helper.GetUnlockable(Unlockable.CUPBOARD), out PlaceableShipObject cupboard)) {
+        _ = Helper.CreateComponent<TransientBehaviour>()
+                  .Init(Helper.PlaceObjectAtTransform(teleporterPlacements.Value.Placement), 6.0f)
+                  .Dispose(() => Helper.PlaceObjectAtTransform(teleporterPlacements.Value.PreviousPlacement).Invoke(0));
+
+        ObjectPlacements<Transform, PlaceableShipObject>? cupboardPlacements = this.GetCupboardPlacements(targetPlayer);
+
+        if (cupboardPlacements is null) {
             return new Result(message: "Cupboard not found!");
         }
 
-        GameObject previousTeleporterTransform = Helper.Copy(inverseTeleporter.transform);
-        GameObject previousCupboardTransform = Helper.Copy(cupboard.transform);
-
-        Vector3 teleporterPositionOffset = new(0.0f, 1.5f, 0.0f);
-        Vector3 teleporterRotationOffset = new(-90.0f, 0.0f, 0.0f);
-
-        inverseTeleporter.PressTeleportButtonServerRpc();
-
         _ = Helper.CreateComponent<TransientBehaviour>()
-            .Init(Helper.PlaceObjectAtTransform(targetPlayer.transform, inverseTeleporter, teleporterPositionOffset, teleporterRotationOffset), 6.0f)
-            .Dispose(() => Helper.PlaceObjectAtTransform(previousTeleporterTransform.transform, inverseTeleporter, teleporterPositionOffset, teleporterRotationOffset).Invoke(0));
+                  .Init(Helper.PlaceObjectAtPosition(cupboardPlacements.Value.Placement), 6.0f)
+                  .Dispose(() => Helper.PlaceObjectAtTransform(cupboardPlacements.Value.Placement).Invoke(0));
 
-        _ = Helper.CreateComponent<TransientBehaviour>()
-            .Init(Helper.PlaceObjectAtPosition(targetPlayer.transform, cupboard, new Vector3(0.0f, 1.75f, 0.0f), new Vector3(-90.0f, 0.0f, 0.0f)), 6.0f)
-            .Dispose(() => Helper.PlaceObjectAtTransform(previousCupboardTransform.transform, cupboard).Invoke(0));
-
+        teleporterPlacements.Value.Placement.GameObject.PressTeleportButtonServerRpc();
         return new Result(true);
     }
 
     public void Execute(string[] args) {
-        if (args.Length < 1) {
+        if (args.Length is 0) {
             Helper.PrintSystem("Usage: /random <player>");
             return;
         }
--- lc-hax/Scripts/Helpers/PlaceObject.cs
+++ lc-hax/Scripts/Helpers/PlaceObject.cs
@@ -7,65 +7,76 @@ namespace Hax;
 public static partial class Helper {
     static ShipBuildModeManager? ShipBuildModeManager => ShipBuildModeManager.Instance;
 
+    static NetworkObject GetNetworkObject<M>(M gameObject) where M : MonoBehaviour =>
+        gameObject.GetComponentInChildren<PlaceableShipObject>()
+                  .parentObject
+                  .GetComponent<NetworkObject>();
+
     public static Action<float> PlaceObjectAtTransform<T, M>(
         T targetObject,
         M gameObject,
         Vector3 positionOffset = new(),
         Vector3 rotationOffset = new()
-    ) where T : Transform where M : MonoBehaviour {
-        return (_) => {
-            NetworkObject networkObject =
-                gameObject.GetComponentInChildren<PlaceableShipObject>()
-                          .parentObject
-                          .GetComponent<NetworkObject>();
+    ) where T : Transform where M : MonoBehaviour => (_) => {
+        NetworkObject networkObject = Helper.GetNetworkObject(gameObject);
+        Helper.ShipBuildModeManager?.PlaceShipObjectServerRpc(
+            targetObject.position + positionOffset,
+            targetObject.eulerAngles + rotationOffset,
+            networkObject,
+            -1
+        );
+    };
 
-            Helper.ShipBuildModeManager?.PlaceShipObjectServerRpc(
-                targetObject.position + positionOffset,
-                targetObject.eulerAngles + rotationOffset,
-                networkObject,
-                -1
-            );
-        };
-    }
+    public static Action<float> PlaceObjectAtTransform<T, M>(
+        ObjectPlacement<T, M> placement
+    ) where T : Transform where M : MonoBehaviour => (_) => {
+        NetworkObject networkObject = Helper.GetNetworkObject(placement.GameObject);
+        Helper.ShipBuildModeManager?.PlaceShipObjectServerRpc(
+            placement.TargetObject.position + placement.PositionOffset,
+            placement.TargetObject.eulerAngles + placement.RotationOffset,
+            networkObject,
+            -1
+        );
+    };
 
     public static Action<float> PlaceObjectAtPosition<T, M>(
         T targetObject,
         M gameObject,
         Vector3 positionOffset = new(),
         Vector3 rotationOffset = new()
-    ) where T : Transform where M : MonoBehaviour {
-        return (_) => {
-            NetworkObject networkObject =
-                gameObject.GetComponentInChildren<PlaceableShipObject>()
-                          .parentObject
-                          .GetComponent<NetworkObject>();
-
-            Helper.ShipBuildModeManager?.PlaceShipObjectServerRpc(
-                targetObject.position + positionOffset,
-                rotationOffset,
-                networkObject,
-                -1
-            );
-        };
-    }
+    ) where T : Transform where M : MonoBehaviour => (_) => {
+        NetworkObject networkObject = Helper.GetNetworkObject(gameObject);
+        Helper.ShipBuildModeManager?.PlaceShipObjectServerRpc(
+            targetObject.position + positionOffset,
+            rotationOffset,
+            networkObject,
+            -1
+        );
+    };
 
     public static Action<float> PlaceObjectAtPosition<M>(
         M gameObject,
         Vector3 position,
         Vector3 rotation
-    ) where M : MonoBehaviour {
-        return (_) => {
-            NetworkObject networkObject =
-                gameObject.GetComponentInChildren<PlaceableShipObject>()
-                          .parentObject
-                          .GetComponent<NetworkObject>();
+    ) where M : MonoBehaviour => (_) => {
+        NetworkObject networkObject = Helper.GetNetworkObject(gameObject);
+        Helper.ShipBuildModeManager?.PlaceShipObjectServerRpc(
+            position,
+            rotation,
+            networkObject,
+            -1
+        );
+    };
 
-            Helper.ShipBuildModeManager?.PlaceShipObjectServerRpc(
-                position,
-                rotation,
-                networkObject,
-                -1
-            );
-        };
-    }
+    public static Action<float> PlaceObjectAtPosition<T, M>(
+        ObjectPlacement<T, M> placement
+    ) where T : Transform where M : MonoBehaviour => (_) => {
+        NetworkObject networkObject = Helper.GetNetworkObject(placement.GameObject);
+        Helper.ShipBuildModeManager?.PlaceShipObjectServerRpc(
+            placement.TargetObject.position + placement.PositionOffset,
+            placement.RotationOffset,
+            networkObject,
+            -1
+        );
+    };
 }
