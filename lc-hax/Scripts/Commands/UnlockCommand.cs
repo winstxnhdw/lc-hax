2539c54f85ec9a27e88423325430ec7e7b95a6fb
winstxnhdw <winstxnhdw@gmail.com>
2023-12-13 19:10:56 +0800

    refactor: abstract `unlock` into methods

--- lc-hax/Scripts/Commands/UnlockCommand.cs
+++ lc-hax/Scripts/Commands/UnlockCommand.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 namespace Hax;
 
 public class UnlockCommand : ICommand {
+    InteractTrigger? GetDoorTrigger(DoorLock door) =>
+        Reflector.Target(door).GetInternalField<InteractTrigger>("doorTrigger");
+
+    void UnlockDoor(DoorLock door) {
+        if (!Helper.Extant(this.GetDoorTrigger(door), out InteractTrigger doorTrigger)) return;
+
+        door.UnlockDoorSyncWithServer();
+        doorTrigger.timeToHold = 0.0f;
+    }
+
     public void Execute(string[] _) {
-        Object.FindObjectsOfType<DoorLock>().ToList().ForEach(door => {
-            door.UnlockDoorSyncWithServer();
-
-            if (!Helper.Extant(Reflector.Target(door).GetInternalField<InteractTrigger>("doorTrigger"), out InteractTrigger doorTrigger)) {
-                return;
-            }
-
-            doorTrigger.timeToHold = 0.0f;
-        });
-
+        Object.FindObjectsOfType<DoorLock>().ToList().ForEach(this.UnlockDoor);
         Object.FindObjectsOfType<TerminalAccessibleObject>()
               .ToList()
               .ForEach(terminalAccessibleObject => terminalAccessibleObject.SetDoorOpenServerRpc(true));
