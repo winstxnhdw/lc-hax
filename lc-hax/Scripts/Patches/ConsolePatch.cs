136835f92d97ed968b61424ec5d368519d615d6c
winstxnhdw <winstxnhdw@gmail.com>
2023-12-14 02:48:38 +0800

    refactor: use single-line early return

--- lc-hax/Scripts/Patches/ConsolePatch.cs
+++ lc-hax/Scripts/Patches/ConsolePatch.cs
@@ -7,15 +7,10 @@ namespace Hax;
 [HarmonyPatch("SubmitChat_performed")]
 class ConsolePatch {
     static bool Prefix() {
-        if (!Helper.HUDManager.IsNotNull(out HUDManager hudManager)) {
-            return true;
-        }
+        if (!Helper.HUDManager.IsNotNull(out HUDManager hudManager)) return true;
+        if (!hudManager.chatTextField.text.StartsWith("/")) return true;
 
         string command = hudManager.chatTextField.text;
-
-        if (!command.StartsWith("/")) {
-            return true;
-        }
 
         try {
             Console.ExecuteCommand(command);
