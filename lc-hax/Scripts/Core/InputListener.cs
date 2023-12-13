c7bfff409129439d31a616d30533b3a26c3be854
LTYGUY <ltyguydev@gmail.com>
2023-12-13 19:56:19 +0800

    implemented teleporting to player by arrow keys, only in phantom mode

--- lc-hax/Scripts/Core/InputListener.cs
+++ lc-hax/Scripts/Core/InputListener.cs
@@ -5,14 +5,18 @@ using UnityEngine.InputSystem;
 
 namespace Hax;
 public class InputListener : MonoBehaviour {
+    public static event Action? onLeftArrowKeyPress;
+    public static event Action? onRightArrowKeyPress;
     public static event Action? onEqualsPress;
     public static event Action? onMiddleButtonPress;
     public static event Action? onLeftButtonPress;
 
     Dictionary<Func<bool>, Action> InputActions { get; } = new() {
         { () => Keyboard.current[Key.Equals].wasPressedThisFrame, () => InputListener.onEqualsPress?.Invoke() },
+        { () => Keyboard.current[Key.LeftArrow].wasPressedThisFrame, () => InputListener.onLeftArrowKeyPress?.Invoke() },
+        { () => Keyboard.current[Key.RightArrow].wasPressedThisFrame, () => InputListener.onRightArrowKeyPress?.Invoke() },
         { () => Mouse.current.middleButton.wasPressedThisFrame, () => InputListener.onMiddleButtonPress?.Invoke() },
-        { () => Mouse.current.leftButton.wasPressedThisFrame, () => InputListener.onLeftButtonPress?.Invoke() }
+        { () => Mouse.current.leftButton.wasPressedThisFrame, () => InputListener.onLeftButtonPress?.Invoke() },
     };
 
     void Update() {
