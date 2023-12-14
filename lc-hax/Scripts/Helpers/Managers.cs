b64c3242e0cba8f8f5adaff40d10c13aae984c7d
winstxnhdw <winstxnhdw@gmail.com>
2023-12-15 02:38:31 +0800

    refactor: add managers helper

--- lc-hax/Scripts/Helpers/Managers.cs
+++ lc-hax/Scripts/Helpers/Managers.cs
@@ -2,4 +2,6 @@ namespace Hax;
 
 public static partial class Helper {
     public static HUDManager? HUDManager => HUDManager.Instance;
+
+    public static RoundManager? RoundManager => RoundManager.Instance;
 }
