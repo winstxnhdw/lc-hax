3bcb711587e1e6c6265483ba9576aafcac4420ad
winstxnhdw <winstxnhdw@gmail.com>
2023-12-15 00:55:48 +0800

    perf: wait for a second before setting shovel damage

--- lc-hax/Scripts/Modules/ShovelMod.cs
+++ lc-hax/Scripts/Modules/ShovelMod.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Linq;
+using GameNetcodeStuff;
 using UnityEngine;
 
 namespace Hax;
 
 public class ShovelMod : MonoBehaviour {
-    bool IsLocalPlayerShovel(Shovel shovel) => shovel.playerHeldBy?.playerClientId == Helper.LocalPlayer?.playerClientId;
+    PlayerControllerB? ShovelOwner(Shovel shovel) => shovel.playerHeldBy;
+
+    bool IsLocalPlayerShovel(Shovel shovel) => this.ShovelOwner(shovel)?.actualClientId == Helper.LocalPlayer?.actualClientId;
 
     Shovel? LocalPlayerShovel => HaxObject.Instance?.Shovels.Objects?.FirstOrDefault(this.IsLocalPlayerShovel);
 
@@ -17,7 +20,7 @@ public class ShovelMod : MonoBehaviour {
             }
 
             shovel.shovelHitForce = Settings.ShovelHitForce;
-            yield return new WaitForEndOfFrame();
+            yield return new WaitForSeconds(1.0f);
         }
     }
 
