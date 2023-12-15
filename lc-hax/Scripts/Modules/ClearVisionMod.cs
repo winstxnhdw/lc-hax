17eac40191612dabdb45fcf8b7c67c40c0005705
LTYGUY <ltyguydev@gmail.com>
2023-12-15 18:01:20 +0800

    untested code

--- lc-hax/Scripts/Modules/ClearVisionMod.cs
+++ lc-hax/Scripts/Modules/ClearVisionMod.cs
@@ -6,25 +6,41 @@ namespace Hax;
 
 public class ClearVisionMod : MonoBehaviour {
     IEnumerator SetClearVision() {
-        HDAdditionalLightData? lightData = null;
-
+        float timer = 0;
         while (true) {
-            if (!Helper.StartOfRound.IsNotNull(out StartOfRound startOfRound) ||
-                !TimeOfDay.Instance.IsNotNull(out TimeOfDay timeOfDay) ||
-                !timeOfDay.sunIndirect.IsNotNull(out Light sunIndirect)
-            ) {
-                yield return new WaitForEndOfFrame();
-                continue;
+            timer += Time.deltaTime;
+
+            if (Helper.StartOfRound.IsNotNull(out StartOfRound startOfRound)
+                && startOfRound.blackSkyVolume.IsNotNull(out UnityEngine.Rendering.Volume blackSkyVolume)) {
+                blackSkyVolume.weight = 0;
             }
 
-            timeOfDay.sunAnimator.enabled = false;
-            sunIndirect.transform.eulerAngles = new Vector3(90, 0, 0);
-            timeOfDay.sunDirect.transform.eulerAngles = new Vector3(90, 0, 0);
-            lightData ??= sunIndirect.GetComponent<HDAdditionalLightData>();
-            lightData.lightDimmer = float.MaxValue;
-            lightData.distance = float.MaxValue;
-            timeOfDay.insideLighting = false;
-            startOfRound.blackSkyVolume.weight = 0;
+            if (TimeOfDay.Instance.IsNotNull(out TimeOfDay timeOfDay)) {
+                timeOfDay.insideLighting = false;
+            }
+
+            if (timeOfDay.sunAnimator.IsNotNull(out Animator sunAnimator)) {
+                sunAnimator.enabled = false;
+            }
+
+            if (timeOfDay.sunIndirect.IsNotNull(out Light sunIndirect)) {
+                sunIndirect.transform.eulerAngles = new Vector3(90, 0, 0);
+            }
+
+            if (sunIndirect.GetComponent<HDAdditionalLightData>().IsNotNull(out HDAdditionalLightData lightData)) {
+                lightData.lightDimmer = float.MaxValue;
+                lightData.distance = float.MaxValue;
+            }
+
+            if (timeOfDay.sunDirect.IsNotNull(out Light sunDirect)) {
+                sunDirect.transform.eulerAngles = new Vector3(90, 0, 0);
+            }
+
+
+            try {
+                Console.Print("t", $"test{timer}");
+            }
+            catch { }
 
             yield return new WaitForEndOfFrame();
         }
