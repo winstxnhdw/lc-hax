cf98baa7f549c0ed3d812f84d810ce83b11b37ff
winstxnhdw <winstxnhdw@gmail.com>
2023-12-15 18:48:45 +0800

    chore: `launch` will now restart build instead of quitting

--- launch.bat
+++ launch.bat
@@ -1,7 +1,9 @@
 @echo off
 
+:begin
 set project_name=lc-hax
 dotnet build %project_name%
 start /wait /b ./submodules/SharpMonoInjectorCore/dist/SharpMonoInjector.exe inject -p "Lethal Company" -a bin/%project_name%.dll -n Hax -c Loader -m Load
 
 pause
+goto begin
