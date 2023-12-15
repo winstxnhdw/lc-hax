3ecccc1b71efc2ae7bff45f37e408951380efb0e
winstxnhdw <winstxnhdw@gmail.com>
2023-12-16 04:08:54 +0800

    refactor: use if-ladder instead of early return

--- lc-hax/Scripts/Helpers/PromptEnemiesToTarget.cs
+++ lc-hax/Scripts/Helpers/PromptEnemiesToTarget.cs
@@ -20,23 +20,22 @@ public static partial class Helper {
 
         roundManager.SpawnedEnemies.ForEach((enemy) => {
             if (enemy is DocileLocustBeesAI or DoublewingAI or BlobAI or DressGirlAI or LassoManAI) return;
+
             enemy.targetPlayer = player;
-            enemy.ChangeEnemyOwnerServerRpc(localPlayer.actualClientId);
             enemy.isEnemyDead = false;
             enemy.enemyHP = enemy.enemyHP <= 0 ? 1 : enemy.enemyHP;
             enemyNames.Add(enemy.enemyType.enemyName);
+            enemy.ChangeEnemyOwnerServerRpc(localPlayer.actualClientId);
 
             if (enemy is CrawlerAI thumper) {
                 thumper.BeginChasingPlayerServerRpc((int)player.playerClientId);
-                return;
             }
 
-            if (enemy is MouthDogAI eyelessDog) {
+            else if (enemy is MouthDogAI eyelessDog) {
                 eyelessDog.ReactToOtherDogHowl(player.transform.position);
-                return;
             }
 
-            if (enemy is BaboonBirdAI baboonHawk) {
+            else if (enemy is BaboonBirdAI baboonHawk) {
                 Threat threat = new() {
                     threatScript = player,
                     lastSeenPosition = player.transform.position,
@@ -51,34 +50,29 @@ public static partial class Helper {
                 };
 
                 _ = Reflector.Target(baboonHawk).InvokeInternalMethod("ReactToThreat", threat);
-                return;
             }
 
-            if (enemy is ForestGiantAI giant) {
+            else if (enemy is ForestGiantAI giant) {
                 giant.chasingPlayer = player;
                 giant.timeSpentStaring = 10;
                 giant.SwitchToBehaviourState(1);
                 _ = Reflector.Target(giant).SetInternalField("lostPlayerInChase", false);
-                return;
             }
 
-            if (enemy is SandWormAI or MaskedPlayerEnemy or SpringManAI) {
+            else if (enemy is SandWormAI or MaskedPlayerEnemy or SpringManAI) {
                 enemy.SwitchToBehaviourState(1);
-                return;
             }
 
-            if (enemy is CentipedeAI or PufferAI or JesterAI) {
+            else if (enemy is CentipedeAI or PufferAI or JesterAI) {
                 enemy.SwitchToBehaviourState(2);
-                return;
             }
 
-            if (enemy is FlowermanAI bracken) {
+            else if (enemy is FlowermanAI bracken) {
                 bracken.SwitchToBehaviourState(2);
                 bracken.EnterAngerModeServerRpc(20);
-                return;
             }
 
-            if (enemy is SandSpiderAI spider) {
+            else if (enemy is SandSpiderAI spider) {
                 spider.meshContainer.position = player.transform.position;
                 spider.SwitchToBehaviourState(2);
                 spider.SyncMeshContainerPositionToClients();
@@ -87,7 +81,7 @@ public static partial class Helper {
                              .SetInternalField("watchFromDistance", false);
             }
 
-            if (enemy is HoarderBugAI hoardingBug) {
+            else if (enemy is HoarderBugAI hoardingBug) {
                 hoardingBug.angryAtPlayer = player;
                 hoardingBug.angryTimer = 1000;
                 hoardingBug.SwitchToBehaviourState(2);
@@ -96,19 +90,17 @@ public static partial class Helper {
                              .InvokeInternalMethod("SyncNestPositionServerRpc", player.transform.position);
             }
 
-            if (enemy is RedLocustBees bees) {
+            else if (enemy is RedLocustBees bees) {
                 bees.SwitchToBehaviourState(2);
                 bees.hive.isHeld = true;
-                return;
             }
 
-            if (enemy is NutcrackerEnemyAI nutcracker) {
+            else if (enemy is NutcrackerEnemyAI nutcracker) {
                 nutcracker.SwitchToBehaviourState(2);
                 nutcracker.SeeMovingThreatServerRpc((int)player.playerClientId);
                 _ = Reflector.Target(nutcracker)
                              .SetInternalField("lastSeenPlayerPos", player.transform.position)?
                              .SetInternalField("timeSinceSeeingTarget", 0);
-                return;
             }
         });
 
