2547559397d6a7283ecdb584eee89d44fd8011ef
winstxnhdw <winstxnhdw@gmail.com>
2023-12-17 05:26:40 +0800

    build/perf: optimise assembly

--- launch.bat
+++ launch.bat
@@ -2,7 +2,7 @@
 
 :begin
 set project_name=lc-hax
-dotnet build %project_name%
+dotnet build %project_name% -c Release
 start /wait /b ./submodules/SharpMonoInjectorCore/dist/SharpMonoInjector.exe inject -p "Lethal Company" -a bin/%project_name%.dll -n Hax -c Loader -m Load
 
 pause
