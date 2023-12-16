349b4299b4abe884d0fcad24e3a185f00adfc66b
winstxnhdw <winstxnhdw@gmail.com>
2023-12-16 17:05:19 +0800

    feat: ensure that the raycast will not target self

--- lc-hax/Scripts/Helpers/Raycast.cs
+++ lc-hax/Scripts/Helpers/Raycast.cs
@@ -8,8 +8,8 @@ public static partial class Helper {
         !Helper.CurrentCamera.IsNotNull(out Camera camera) || !camera.enabled
             ? []
             : [.. Physics.SphereCastAll(
-                camera.transform.position + (camera.transform.forward * 0.5f),
-                1f,
+                camera.transform.position + (camera.transform.forward * 1.5f),
+                1.0f,
                 camera.transform.forward,
                 float.MaxValue
             )];
