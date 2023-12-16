c811c3e2b4013fbce3bc52bbcf127a0bfee26497
winstxnhdw <winstxnhdw@gmail.com>
2023-12-16 17:21:27 +0800

    feat/docs: allow user to change `Phantom` control sensitivity

--- lc-hax/Scripts/Core/InputListener.cs
+++ lc-hax/Scripts/Core/InputListener.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Hax;
+
 public class InputListener : MonoBehaviour {
     public static event Action? onLeftArrowKeyPress;
     public static event Action? onRightArrowKeyPress;
     public static event Action? onEqualsPress;
     public static event Action? onMiddleButtonPress;
     public static event Action? onLeftButtonPress;
+    public static event Action? onLeftBracketPress;
+    public static event Action? onRightBracketPress;
+
 
     Dictionary<Func<bool>, Action> InputActions { get; } = new() {
         { () => Keyboard.current[Key.Equals].wasPressedThisFrame, () => InputListener.onEqualsPress?.Invoke() },
         { () => Keyboard.current[Key.LeftArrow].wasPressedThisFrame, () => InputListener.onLeftArrowKeyPress?.Invoke() },
         { () => Keyboard.current[Key.RightArrow].wasPressedThisFrame, () => InputListener.onRightArrowKeyPress?.Invoke() },
+        { () => Keyboard.current[Key.LeftBracket].wasPressedThisFrame, () => InputListener.onLeftBracketPress?.Invoke() },
+        { () => Keyboard.current[Key.RightBracket].wasPressedThisFrame, () => InputListener.onRightBracketPress?.Invoke() },
         { () => Mouse.current.middleButton.wasPressedThisFrame, () => InputListener.onMiddleButtonPress?.Invoke() },
-        { () => Mouse.current.leftButton.wasPressedThisFrame, () => InputListener.onLeftButtonPress?.Invoke() },
+        { () => Mouse.current.leftButton.wasPressedThisFrame, () => InputListener.onLeftButtonPress?.Invoke() }
     };
 
     void Update() {
