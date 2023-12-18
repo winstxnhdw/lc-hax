c57290c64d2112937a2e7df51a9c9b194dac84c7
winstxnhdw <winstxnhdw@gmail.com>
2023-12-18 20:41:18 +0800

    fix: bypass if-guard for entrance teleport

--- lc-hax/Scripts/Commands/EntranceCommand.cs
+++ lc-hax/Scripts/Commands/EntranceCommand.cs
@@ -1,28 +1,27 @@
 using GameNetcodeStuff;
+using UnityEngine;
 
 namespace Hax;
 
 public class EntranceCommand : ICommand {
     public void Execute(string[] args) {
-        EntranceTeleport entranceTeleport = RoundManager.FindMainEntranceScript();
+        EntranceTeleport entranceTeleport = RoundManager.FindMainEntranceScript(true);
 
-        if (args.Length is 0) {
-            if (!Helper.LocalPlayer.IsNotNull(out PlayerControllerB localPlayer)) {
-                Console.Print("Local player not found!");
-                return;
-            }
-
-            entranceTeleport.TeleportPlayerServerRpc((int)localPlayer.playerClientId);
+        if (!entranceTeleport.FindExitPoint()) {
+            Console.Print("Exit point not found!");
+            return;
         }
 
-        else if (args.Length is 1) {
-            if (!Helper.GetPlayer(args[0]).IsNotNull(out PlayerControllerB targetPlayer)) {
-                Console.Print("Player not found!");
-                return;
-            }
-
-            entranceTeleport.TeleportPlayerServerRpc((int)targetPlayer.playerClientId);
+        if (!Reflector.Target(entranceTeleport).GetInternalField<Transform>("exitPoint").IsNotNull(out Transform exitPoint)) {
+            Console.Print("Exit point not found!");
+            return;
         }
 
+        if (!Helper.LocalPlayer.IsNotNull(out PlayerControllerB localPlayer)) {
+            Console.Print("Local player not found!");
+            return;
+        }
+
+        localPlayer.TeleportPlayer(exitPoint.position);
     }
 }
