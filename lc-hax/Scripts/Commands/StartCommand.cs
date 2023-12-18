4f14ae226c070411ed24ddeb08e1b0a1aee8049b
winstxnhdw <winstxnhdw@gmail.com>
2023-12-18 23:18:41 +0800

    refactor: use null propagation

--- lc-hax/Scripts/Commands/StartCommand.cs
+++ lc-hax/Scripts/Commands/StartCommand.cs
@@ -2,7 +2,6 @@ namespace Hax;
 
 public class StartGameCommand : ICommand {
     public void Execute(string[] _) {
-        if (!Helper.StartOfRound.IsNotNull(out StartOfRound startOfRound)) return;
-        startOfRound.StartGameServerRpc();
+        Helper.StartOfRound?.StartGameServerRpc();
     }
 }
