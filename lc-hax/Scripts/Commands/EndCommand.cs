3327166ff3da7ff2cdb31df328766d1be7bb1a4c
winstxnhdw <winstxnhdw@gmail.com>
2023-12-19 03:04:44 +0800

    fix: allow player to end without specifying user

--- lc-hax/Scripts/Commands/EndCommand.cs
+++ lc-hax/Scripts/Commands/EndCommand.cs
@@ -4,8 +4,12 @@ namespace Hax;
 
 public class EndCommand : ICommand {
     public void Execute(string[] args) {
-        Helper.StartOfRound?.EndGameServerRpc(
-            Helper.GetPlayer(args[0]).IsNotNull(out PlayerControllerB player) ? (int)player.playerClientId : -1
-        );
+        if (args.Length is 0) {
+            Helper.StartOfRound?.EndGameServerRpc(-1);
+        }
+
+        else if (Helper.GetPlayer(args[0]).IsNotNull(out PlayerControllerB player)) {
+            Helper.StartOfRound?.EndGameServerRpc((int)player.playerClientId);
+        }
     }
 }
