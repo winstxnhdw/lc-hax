da1660feb14543482b364ca47a4fca851d78292b
winstxnhdw <winstxnhdw@gmail.com>
2023-12-18 20:12:46 +0800

    refactor: shorten command names

--- lc-hax/Scripts/Commands/StartCommand.cs
+++ lc-hax/Scripts/Commands/StartCommand.cs
@@ -1,9 +1,8 @@
 namespace Hax;
+
 public class StartGameCommand : ICommand {
     public void Execute(string[] _) {
-        if (!Helper.StartOfRound.IsNotNull(out StartOfRound startOfRound))
-            return;
-
+        if (!Helper.StartOfRound.IsNotNull(out StartOfRound startOfRound)) return;
         startOfRound.StartGameServerRpc();
     }
 }
