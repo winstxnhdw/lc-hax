118e5ee8cbda328250d866b328842276464864e0
winstxnhdw <winstxnhdw@gmail.com>
2023-12-20 00:37:28 +0800

    perf: increase time before stamina refreshes

--- lc-hax/Scripts/Modules/StaminaMod.cs
+++ lc-hax/Scripts/Modules/StaminaMod.cs
@@ -17,7 +17,7 @@ public class StaminaMod : MonoBehaviour {
             player.isExhausted = false;
             player.sprintMeter = 1.0f;
 
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(5.0f);
         }
     }
 
