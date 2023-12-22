04491b40498008fac2ee42576de235188519f874
winstxnhdw <winstxnhdw@gmail.com>
2023-12-22 10:10:08 +0800

    fix: can't use method overload because we do not know the type

--- lc-hax/Scripts/Helpers/Players.cs
+++ lc-hax/Scripts/Helpers/Players.cs
@@ -12,12 +12,17 @@ public static partial class Helper {
 
     public static PlayerControllerB[]? Players => Helper.StartOfRound?.allPlayerScripts;
 
+    public static PlayerControllerB? GetPlayer(string playerNameOrId) {
+        PlayerControllerB[]? players = Helper.Players;
+
+        return players?.FirstOrDefault(player => player.playerUsername == playerNameOrId) ??
+               players?.FirstOrDefault(player => player.playerClientId.ToString() == playerNameOrId);
+    }
+
     public static PlayerControllerB? GetPlayer(int playerClientId) => Helper.Players?.FirstOrDefault(player => player.playerClientId == (ulong)playerClientId);
 
-    public static PlayerControllerB? GetPlayer(string username) => Helper.Players?.FirstOrDefault(player => player.playerUsername == username);
-
-    public static PlayerControllerB? GetActivePlayer(string username) =>
-        Helper.GetPlayer(username).IsNotNull(out PlayerControllerB player)
+    public static PlayerControllerB? GetActivePlayer(string playerNameOrId) =>
+        Helper.GetPlayer(playerNameOrId).IsNotNull(out PlayerControllerB player)
             ? player.isPlayerDead ? null : !player.isPlayerControlled ? null : player
             : null;
 
