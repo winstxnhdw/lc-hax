a134481651369de2dcbccbd0a1eca8cfa3c2732b
winstxnhdw <winstxnhdw@gmail.com>
2023-12-23 21:20:33 +0800

    style: use arrow syntax

--- lc-hax/Scripts/Patches/NoFallDamagePatch.cs
+++ lc-hax/Scripts/Patches/NoFallDamagePatch.cs
@@ -6,7 +6,5 @@ namespace Hax;
 [HarmonyPatch(typeof(PlayerControllerB))]
 [HarmonyPatch("DamagePlayer")]
 class NoFallDamagePatch {
-    static bool Prefix(CauseOfDeath causeOfDeath) {
-        return !Settings.DisableFallDamage || causeOfDeath != CauseOfDeath.Gravity;
-    }
+    static bool Prefix(CauseOfDeath causeOfDeath) => !Settings.DisableFallDamage || causeOfDeath != CauseOfDeath.Gravity;
 }
