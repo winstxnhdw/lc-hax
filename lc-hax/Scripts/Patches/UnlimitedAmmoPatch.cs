51e6f2acaaab3ba95f259af1dff81a360a4c3db5
winstxnhdw <winstxnhdw@gmail.com>
2023-12-24 01:27:51 +0800

    style: use instance field directly

--- lc-hax/Scripts/Patches/UnlimitedAmmoPatch.cs
+++ lc-hax/Scripts/Patches/UnlimitedAmmoPatch.cs
@@ -7,8 +7,8 @@ namespace Hax;
 [HarmonyPatch(typeof(ShotgunItem))]
 class UnlimitedAmmoPatch {
     [HarmonyPatch(nameof(ShotgunItem.ItemActivate))]
-    static void Prefix(ShotgunItem __instance) => __instance.shellsLoaded = 2;
+    static void Prefix(ref int ___shellsLoaded) => ___shellsLoaded = 2;
 
     [HarmonyPatch(nameof(ShotgunItem.ShootGun))]
-    static void Postfix(ShotgunItem __instance) => __instance.shellsLoaded = 2;
+    static void Postfix(ref int ___shellsLoaded) => ___shellsLoaded = 2;
 }
