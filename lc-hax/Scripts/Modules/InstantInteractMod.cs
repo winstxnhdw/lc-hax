65ee095349ae1f6e01c071ed661b4f22134b8c2c
winstxnhdw <winstxnhdw@gmail.com>
2023-12-24 07:25:29 +0800

    fix: only set delay to fire exit

--- lc-hax/Scripts/Modules/InstantInteractMod.cs
+++ lc-hax/Scripts/Modules/InstantInteractMod.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 namespace Hax;
 
 public sealed class InstantInteractMod : MonoBehaviour {
+
     IEnumerator SetTimeToHold() {
         while (true) {
             HaxObjects.Instance?.InteractTriggers.Objects?.ForEach(interactTrigger =>
-                interactTrigger.timeToHold = interactTrigger.transform.parent.name is "EntranceTeleportB" ? 0.3f : 0.0f
+                interactTrigger.timeToHold = interactTrigger.name is "EntranceTeleportB(Clone)" ? 0.3f : 0.0f
             );
 
             yield return new WaitForSeconds(5.0f);
