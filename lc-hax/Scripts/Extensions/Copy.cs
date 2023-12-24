51300f462c8da25d245fd84d2f6b50be014f2ce2
winstxnhdw <winstxnhdw@gmail.com>
2023-12-25 00:20:11 +0800

    refactor: move extensions to separate class

--- lc-hax/Scripts/Extensions/Copy.cs
+++ lc-hax/Scripts/Extensions/Copy.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Hax;
 
-public static partial class Helper {
+public static partial class Extensions {
     public static Vector3 Copy(this Vector3 vector) {
         return new Vector3(
             vector.x,
--- lc-hax/Scripts/Extensions/ForEach.cs
+++ lc-hax/Scripts/Extensions/ForEach.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Hax;
 
-public static partial class Helper {
+public static partial class Extensions {
     public static void ForEach<T>(this T[] array, Action<T> action) {
         for (int i = 0; i < array.Length; i++) {
             action(array[i]);
