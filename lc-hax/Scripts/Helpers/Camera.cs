09bdf071dda6fa7ca46c39d77fbd31a39f94d483
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2023-12-24 21:14:31 +0800

    refactor: use `IsNotNull`

--- lc-hax/Scripts/Helpers/Camera.cs
+++ lc-hax/Scripts/Helpers/Camera.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using GameNetcodeStuff;
 
 namespace Hax;
 
 public static partial class Helper {
     public static Camera? CurrentCamera =>
-        Helper.LocalPlayer?.gameplayCamera != null && Helper.LocalPlayer.gameplayCamera.enabled
-            ? LocalPlayer.gameplayCamera
+        Helper.LocalPlayer.IsNotNull(out PlayerControllerB localPlayer) &&
+        localPlayer.gameplayCamera.IsNotNull(out Camera gameplayCamera) &&
+        gameplayCamera.enabled
+            ? gameplayCamera
             : Helper.StartOfRound?.spectateCamera;
 }
