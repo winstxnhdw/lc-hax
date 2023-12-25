8c714ede6e265edee5a854b42d02011a6e517543
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2023-12-25 19:06:21 +0800

    feat/docs: `explode` will only activate Jetpacks by default (#65)

--- lc-hax/Scripts/Commands/ExplodeCommand.cs
+++ lc-hax/Scripts/Commands/ExplodeCommand.cs
@@ -5,13 +5,13 @@ namespace Hax;
 public class ExplodeCommand : ICommand {
     public void Execute(string[] args) {
         if (args.Length is 0) {
-            Object.FindObjectsOfType<Landmine>()
-                  .ForEach(mine => mine.TriggerMine());
-        }
-
-        if (args[0] is "jet") {
             Object.FindObjectsOfType<JetpackItem>()
                   .ForEach(jetpack => jetpack.ExplodeJetpackServerRpc());
+        }
+
+        if (args[0] is "mine") {
+            Object.FindObjectsOfType<Landmine>()
+                  .ForEach(landmine => landmine.TriggerMine());
         }
     }
 }
