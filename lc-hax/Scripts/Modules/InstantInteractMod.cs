c29b83fc08688ae680989856f1bf6f26897dd93f
winstxnhdw <winstxnhdw@gmail.com>
2023-12-26 07:22:29 +0800

    feat: do not use null-coalescing operator to prevent caching

--- lc-hax/Scripts/Modules/InstantInteractMod.cs
+++ lc-hax/Scripts/Modules/InstantInteractMod.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 namespace Hax;
 
 public sealed class InstantInteractMod : MonoBehaviour {
-    InteractTrigger[] InteractTriggers => HaxObjects.Instance?.InteractTriggers.Objects ?? [];
+    InteractTrigger[]? InteractTriggers => HaxObjects.Instance?.InteractTriggers.Objects;
 
     IEnumerator SetTimeToHold() {
         while (true) {
-            this.InteractTriggers.ForEach(interactTrigger =>
+            this.InteractTriggers?.ForEach(interactTrigger =>
                 interactTrigger.timeToHold = interactTrigger.name is "EntranceTeleportB(Clone)" ? 0.3f : 0.0f
             );
 
