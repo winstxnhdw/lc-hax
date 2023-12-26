d3f8a3262ae4ee582c9998c4b9624e4f14a6bb70
winstxnhdw <winstxnhdw@gmail.com>
2023-12-27 01:38:25 +0800

    fix: wrap `InteractTriggers` with try-catch to prevent coroutine from stopping

--- lc-hax/Scripts/Modules/InstantInteractMod.cs
+++ lc-hax/Scripts/Modules/InstantInteractMod.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 namespace Hax;
 
 public sealed class InstantInteractMod : MonoBehaviour {
-    InteractTrigger?[]? InteractTriggers => HaxObjects.Instance?.InteractTriggers.Objects;
+    string? GetInteractTriggerName(InteractTrigger? interactTrigger) => interactTrigger?.name;
 
     IEnumerator SetTimeToHold() {
         while (true) {
-            this.InteractTriggers?.ForEach(interactTrigger => {
-                if (interactTrigger is null) return;
-                interactTrigger.timeToHold = interactTrigger.name is "EntranceTeleportB(Clone)" ? 0.3f : 0.0f;
+            Helper.Try(() => {
+                HaxObjects.Instance?.InteractTriggers.Objects?.ForEach(interactTrigger => {
+                    if (interactTrigger is null) return;
+                    interactTrigger.timeToHold = interactTrigger.name is "EntranceTeleportB(Clone)" ? 0.3f : 0.0f;
+                });
             });
 
             yield return new WaitForSeconds(5.0f);
