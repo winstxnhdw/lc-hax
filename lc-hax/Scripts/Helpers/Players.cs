2c25236462daf8511d9ac56cc26d6c26cb7e8234
winstxnhdw <winstxnhdw@gmail.com>
2023-12-27 01:41:08 +0800

    refactor: bring back null-coalescing operator

--- lc-hax/Scripts/Helpers/Players.cs
+++ lc-hax/Scripts/Helpers/Players.cs
@@ -10,10 +10,10 @@ public static partial class Helper {
 
     public static PlayerControllerB? LocalPlayer => GameNetworkManager.Instance.localPlayerController;
 
-    public static PlayerControllerB[]? Players => Helper.StartOfRound?.allPlayerScripts;
+    public static PlayerControllerB[] Players => Helper.StartOfRound?.allPlayerScripts ?? [];
 
     public static PlayerControllerB? GetPlayer(string playerNameOrId) {
-        PlayerControllerB[]? players = Helper.Players;
+        PlayerControllerB[] players = Helper.Players;
 
         return players.FirstOrDefault(player => player.playerUsername == playerNameOrId) ??
                players.FirstOrDefault(player => player.playerClientId.ToString() == playerNameOrId);
