59faf4c2efdf173df482ae85dfc45e6dbfb16bc4
winstxnhdw <winstxnhdw@gmail.com>
2023-12-27 01:41:08 +0800

    refactor: bring back null-coalescing operator

--- lc-hax/Scripts/Helpers/Teleporter.cs
+++ lc-hax/Scripts/Helpers/Teleporter.cs
@@ -3,7 +3,7 @@ using System.Linq;
 namespace Hax;
 
 public static partial class Helper {
-    public static ShipTeleporter?[]? ShipTeleporters => HaxObjects.Instance?.ShipTeleporters.Objects;
+    public static ShipTeleporter?[] ShipTeleporters => HaxObjects.Instance?.ShipTeleporters.Objects ?? [];
 
     public static ShipTeleporter? InverseTeleporter => Helper.ShipTeleporters.FirstOrDefault(
         teleporter => teleporter is not null && teleporter.isInverseTeleporter
