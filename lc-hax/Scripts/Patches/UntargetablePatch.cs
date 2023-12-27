0d5dfdbc6dacde7ab7fad27b09cfe11c94acb92e
winstxnhdw <winstxnhdw@gmail.com>
2023-12-28 02:23:42 +0800

    feat/docs: add block command to be untargetable by enemies

---
+++ lc-hax/Scripts/Patches/UntargetablePatch.cs
@@ -0,0 +1,18 @@
+#pragma warning disable IDE1006
+
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace Hax;
+
+[HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.PlayerIsTargetable))]
+class UntargetablePatch {
+    public static bool Prefix(PlayerControllerB playerScript, ref bool __result) {
+        if (!Settings.EnableUntargetable) return true;
+        if (!Helper.LocalPlayer.IsNotNull(out PlayerControllerB localPlayer)) return true;
+        if (localPlayer.actualClientId != playerScript.actualClientId) return true;
+
+        __result = false;
+        return false;
+    }
+}
--- lc-hax/Scripts/Static/Settings.cs
+++ lc-hax/Scripts/Static/Settings.cs
@@ -5,6 +5,7 @@ public static class Settings {
     public static bool EnableGodMode { get; set; } = false;
     public static bool EnableDemigodMode { get; set; } = false;
     public static bool EnableBlockCredits { get; set; } = false;
+    public static bool EnableUntargetable { get; set; } = false;
     public static int ShovelHitForce { get; set; } = 1;
     public static bool EnableStunOnLeftClick { get; set; } = false;
     public static bool DisableFallDamage { get; set; } = false;
