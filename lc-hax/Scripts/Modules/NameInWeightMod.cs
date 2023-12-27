11f97716cd2ee2f831aab4e2fa4af1e811ffdb09
LTYGUY <ltyguydev@gmail.com>
2023-12-27 23:07:56 +0800

    fix nameinweightmod

--- lc-hax/Scripts/Modules/NameInWeightMod.cs
+++ lc-hax/Scripts/Modules/NameInWeightMod.cs
@@ -1,32 +1,28 @@
 using UnityEngine;
 using GameNetcodeStuff;
-using System.Collections;
 
 namespace Hax;
 
 public sealed class NameInWeightMod : MonoBehaviour {
-    IEnumerator SetWeightCounterText() {
-        while (true) {
-            if (!Helper.HUDManager.IsNotNull(out HUDManager hudManager)) {
-                yield return new WaitForEndOfFrame();
+    void SetWeightCounterText() {
+        if (!Helper.HUDManager.IsNotNull(out HUDManager hudManager) ||
+            !hudManager.weightCounter.IsNotNull(out TMPro.TextMeshProUGUI weightCounter)) {
+            return;
+        }
+
+
+        foreach (RaycastHit raycastHit in Helper.RaycastForward()) {
+            if (!raycastHit.collider.gameObject.TryGetComponent(out PlayerControllerB player)) {
                 continue;
             }
 
-            foreach (RaycastHit raycastHit in Helper.RaycastForward()) {
-                if (!raycastHit.collider.gameObject.TryGetComponent(out PlayerControllerB player)) {
-                    continue;
-                }
-
-                hudManager.weightCounter.enableWordWrapping = false;
-                hudManager.weightCounter.text = $"#{player.playerClientId} {player.playerUsername}";
-                break;
-            }
-
-            yield return new WaitForEndOfFrame();
+            weightCounter.enableWordWrapping = false;
+            weightCounter.text = $"#{player.playerClientId} {player.playerUsername}";
+            break;
         }
     }
 
-    void Start() {
-        _ = this.StartResilientCoroutine(this.SetWeightCounterText());
+    void LateUpdate() {
+        this.SetWeightCounterText();
     }
 }
