441afe6f5bef0a6e41a39d138ce171d760396df6
winstxnhdw <winstxnhdw@gmail.com>
2023-12-27 20:01:14 +0800

    perf: set `CanConfirmPosition` in patch

--- lc-hax/Scripts/Patches/BuildAnywherePatch.cs
+++ lc-hax/Scripts/Patches/BuildAnywherePatch.cs
@@ -8,7 +8,8 @@ namespace Hax;
 [HarmonyPatch(typeof(ShipBuildModeManager))]
 [HarmonyPatch("PlayerMeetsConditionsToBuild")]
 class BuildAnywherePatch {
-    static bool Prefix(ref bool __result) {
+    static bool Prefix(ref bool __result, ref bool ___CanConfirmPosition) {
+        ___CanConfirmPosition = true;
         __result = !Helper.LocalPlayer.IsNotNull(out PlayerControllerB player) || !player.inTerminalMenu;
         return false;
     }
