ccd0c69e63b8fe9e78bc1010263fb534213e8257
armorfid <18181886+armorfid@users.noreply.github.com>
2023-12-28 02:33:35 -0500

    fix: repaired refresh delay patch

--- lc-hax/Scripts/Patches/NoRefreshDelayPatch.cs
+++ lc-hax/Scripts/Patches/NoRefreshDelayPatch.cs
@@ -1,17 +1,10 @@
-using System.Reflection.Emit;
-using System.Collections.Generic;
+#pragma warning disable IDE1006
+
 using HarmonyLib;
 
 namespace Hax;
 
 [HarmonyPatch(typeof(SteamLobbyManager), nameof(SteamLobbyManager.RefreshServerListButton))]
 class NoRefreshDelayPatch {
-    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
-        bool found = false;
-
-        foreach (CodeInstruction instruction in instructions) {
-            if (instruction.opcode == OpCodes.Ldarg_0) found = true;
-            if (found) yield return instruction;
-        }
-    }
+    static void Prefix(ref float ___refreshServerListTimer) => ___refreshServerListTimer = 999f;
 }
