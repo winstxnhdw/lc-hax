b0cf84fe0241d973cd2d36c3c6177896825cba4c
armorfid <18181886+armorfid@users.noreply.github.com>
2023-12-27 20:48:00 -0500

    fix: aiming no longer freezes if your attacker dies mid-kill

--- lc-hax/Scripts/Patches/EnemyPatch.cs
+++ lc-hax/Scripts/Patches/EnemyPatch.cs
@@ -1,10 +1,20 @@
 #pragma warning disable IDE1006
 
+using GameNetcodeStuff;
 using HarmonyLib;
 
 namespace Hax;
 
-[HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.SyncPositionToClients))]
+[HarmonyPatch(typeof(EnemyAI))]
 class EnemyPatch {
+    [HarmonyPatch(nameof(EnemyAI.SyncPositionToClients))]
     static void Prefix(ref float ___updatePositionThreshold) => ___updatePositionThreshold = 0.0f;
+
+    // Fixes game bug where you can't aim anymore if an enemy dies while attempting to kill you.
+    [HarmonyPatch(nameof(EnemyAI.CancelSpecialAnimationWithPlayer))]
+    static void Prefix(ref PlayerControllerB ___inSpecialAnimationWithPlayer) {
+        if (Helper.LocalPlayer == ___inSpecialAnimationWithPlayer) {
+            Helper.LocalPlayer.disableLookInput = false;
+        }
+    }
 }
