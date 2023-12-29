1f121cbd685e09927545917d623e2c22511e4853
armorfid <18181886+armorfid@users.noreply.github.com>
2023-12-29 14:19:34 -0500

    feat: `stunclick` will jam turrets/landmines (#106)
    * feat: made `/stunclick` also jam turrets and landmines
    however, landmine jamming itself is currently broken in v45 🙁
    
    * fix: disable unlimited ammo when gun is held by Nutcracker
    otherwise he just keeps firing without ever reloading 😅
    
    * feat: check colliders within a sphere around player instead
    
    * style: reduce nesting
    
    ---------
    
    Co-authored-by: winstxnhdw <winstxnhdw@gmail.com>

--- lc-hax/Scripts/Modules/StunMod.cs
+++ lc-hax/Scripts/Modules/StunMod.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 namespace Hax;
 
 public sealed class StunMod : MonoBehaviour, IStun {
+    Collider[] Colliders { get; set; } = new Collider[15];
+
     void OnEnable() {
         InputListener.onLeftButtonPress += this.Stun;
     }
@@ -16,5 +18,20 @@ public sealed class StunMod : MonoBehaviour, IStun {
         if (!Helper.CurrentCamera.IsNotNull(out Camera camera)) return;
 
         this.Stun(camera.transform.position, 5.0f);
+        int colliders = Physics.OverlapSphereNonAlloc(camera.transform.position, 5.0f, this.Colliders);
+
+        for (int i = 0; i < colliders; i++) {
+            GameObject gameObject = this.Colliders[i].gameObject;
+
+            if (gameObject.TryGetComponent(out Turret _) && gameObject.TryGetComponent(out Landmine _)) {
+                return;
+            }
+
+            if (!gameObject.TryGetComponent(out TerminalAccessibleObject terminalAccessibleObject)) {
+                return;
+            }
+
+            terminalAccessibleObject.CallFunctionFromTerminal();
+        }
     }
 }
