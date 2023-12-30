8139eea7027ceff26aa48a3705c70e2755a77b23
winstxnhdw <winstxnhdw@gmail.com>
2023-12-30 23:09:39 +0800

    fix: now able to grab in `Phantom`

--- lc-hax/Scripts/Patches/GrabPatch.cs
+++ lc-hax/Scripts/Patches/GrabPatch.cs
@@ -8,18 +8,10 @@ namespace Hax;
 
 [HarmonyPatch(typeof(PlayerControllerB), "SetHoverTipAndCurrentInteractTrigger")]
 class GrabPatch {
-    static void Prefix(ref bool ___isGrabbingObjectAnimation, ref bool __state) {
-        __state = ___isGrabbingObjectAnimation;
-        ___isGrabbingObjectAnimation = false;
-    }
-
     static void Postfix(
-        ref bool ___isGrabbingObjectAnimation,
         ref int ___interactableObjectsMask,
-        ref float ___grabDistance,
-        bool __state
+        ref float ___grabDistance
     ) {
-        ___isGrabbingObjectAnimation = __state;
         ___interactableObjectsMask = LayerMask.GetMask(["Props", "InteractableObject"]);
         ___grabDistance = float.MaxValue;
     }
