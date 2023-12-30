521fe4400684dccd379777521f753c94dce8906d
winstxnhdw <winstxnhdw@gmail.com>
2023-12-30 10:20:49 +0800

    style: separate `Mouse` inputs from keyboard inputs

--- lc-hax/Scripts/Core/InputListener.cs
+++ lc-hax/Scripts/Core/InputListener.cs
@@ -11,17 +11,19 @@ public class InputListener : MonoBehaviour {
     public static event Action<bool>? onFButtonHold;
     public static event Action<bool>? onEButtonHold;
     public static event Action<bool>? onRButtonHold;
+    public static event Action? onMiddleButtonPress;
+    public static event Action? onLeftButtonPress;
     public static event Action? onLeftArrowKeyPress;
     public static event Action? onRightArrowKeyPress;
     public static event Action? onEqualsPress;
-    public static event Action? onMiddleButtonPress;
-    public static event Action? onLeftButtonPress;
     public static event Action? onLeftBracketPress;
     public static event Action? onRightBracketPress;
     public static event Action? onZPress;
     public static event Action? onXPress;
 
     Dictionary<Func<bool>, Action> InputActions { get; } = new() {
+        { () => Mouse.current.middleButton.wasPressedThisFrame, () => InputListener.onMiddleButtonPress?.Invoke() },
+        { () => Mouse.current.leftButton.wasPressedThisFrame, () => InputListener.onLeftButtonPress?.Invoke() },
         { () => true, () => InputListener.onShiftButtonHold?.Invoke(Keyboard.current[Key.LeftShift].isPressed) },
         { () => true, () => InputListener.onFButtonHold?.Invoke(Keyboard.current[Key.F].isPressed) },
         { () => true, () => InputListener.onRButtonHold?.Invoke(Keyboard.current[Key.R].isPressed) },
@@ -31,8 +33,6 @@ public class InputListener : MonoBehaviour {
         { () => Keyboard.current[Key.RightArrow].wasPressedThisFrame, () => InputListener.onRightArrowKeyPress?.Invoke() },
         { () => Keyboard.current[Key.LeftBracket].wasPressedThisFrame, () => InputListener.onLeftBracketPress?.Invoke() },
         { () => Keyboard.current[Key.RightBracket].wasPressedThisFrame, () => InputListener.onRightBracketPress?.Invoke() },
-        { () => Mouse.current.middleButton.wasPressedThisFrame, () => InputListener.onMiddleButtonPress?.Invoke() },
-        { () => Mouse.current.leftButton.wasPressedThisFrame, () => InputListener.onLeftButtonPress?.Invoke() },
         { () => Keyboard.current[Key.Z].wasPressedThisFrame, () => InputListener.onZPress?.Invoke() },
         { () => Keyboard.current[Key.X].wasPressedThisFrame, () => InputListener.onXPress?.Invoke() },
     };
