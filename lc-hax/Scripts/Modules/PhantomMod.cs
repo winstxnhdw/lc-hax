af057522235888c61b8b80189d3ef29b9ad5f218
winstxnhdw <winstxnhdw@gmail.com>
2023-12-30 23:09:39 +0800

    fix: now able to grab in `Phantom`

--- lc-hax/Scripts/Modules/PhantomMod.cs
+++ lc-hax/Scripts/Modules/PhantomMod.cs
@@ -5,7 +5,6 @@ namespace Hax;
 
 public sealed class PhantomMod : MonoBehaviour {
     bool IsShiftHeld { get; set; } = false;
-    bool EnablePhantom { get; set; } = false;
     bool EnabledPossession { get; set; } = false;
     int CurrentSpectatorIndex { get; set; } = 0;
 
@@ -29,12 +28,11 @@ public sealed class PhantomMod : MonoBehaviour {
         if (!camera.gameObject.TryGetComponent(out KeyboardMovement keyboard)) return;
         if (!camera.gameObject.TryGetComponent(out MousePan mouse)) return;
 
-        if (this.EnablePhantom) {
+        if (Setting.EnablePhantom) {
             //if was enabled possession before, but no longer possesing
             if (this.EnabledPossession && !possessionMod.IsPossessed) {
                 this.EnabledPossession = false;
                 possessionMod.enabled = false;
-
                 keyboard.enabled = true;
                 mouse.enabled = true;
             }
@@ -70,7 +68,7 @@ public sealed class PhantomMod : MonoBehaviour {
     void LookAtPreviousPlayer() => this.LookAtPlayer(-1);
 
     void LookAtPlayer(int indexChange) {
-        if (!this.EnablePhantom || !Helper.CurrentCamera.IsNotNull(out Camera camera)) return;
+        if (!Setting.EnablePhantom || !Helper.CurrentCamera.IsNotNull(out Camera camera)) return;
 
         int playerCount = Helper.Players?.Length ?? 0;
         this.CurrentSpectatorIndex = (this.CurrentSpectatorIndex + indexChange) % playerCount;
@@ -83,58 +81,64 @@ public sealed class PhantomMod : MonoBehaviour {
         camera.transform.position = targetPlayer.playerEye.position;
     }
 
+    void PhantomEnabled(Camera camera) {
+        if (!camera.TryGetComponent(out KeyboardMovement keyboard)) {
+            keyboard = camera.gameObject.AddComponent<KeyboardMovement>();
+        }
+
+        if (!camera.TryGetComponent(out MousePan mouse)) {
+            mouse = camera.gameObject.AddComponent<MousePan>();
+        }
+
+        keyboard.enabled = true;
+        mouse.enabled = true;
+        camera.transform.SetParent(null, true);
+    }
+
+    void PhantomDisabled(PlayerControllerB player, Camera camera) {
+        if (!player.cameraContainerTransform.IsNotNull(out Transform cameraParent)) return;
+        if (this.IsShiftHeld) player.TeleportPlayer(camera.transform.position);
+
+        camera.transform.SetParent(cameraParent, false);
+        camera.transform.localPosition = Vector3.zero;
+        camera.transform.localRotation = Quaternion.identity;
+
+        if (camera.gameObject.TryGetComponent(out KeyboardMovement keyboard)) {
+            keyboard.enabled = false;
+        }
+
+        if (camera.gameObject.TryGetComponent(out MousePan mouse)) {
+            mouse.enabled = false;
+        }
+
+        if (!player.gameplayCamera.IsNotNull(out Camera gameplayCamera)) {
+            return;
+        }
+
+        if (gameplayCamera.TryGetComponent(out KeyboardMovement gameplayKeyboard)) {
+            gameplayKeyboard.enabled = false;
+        }
+
+        if (gameplayCamera.TryGetComponent(out MousePan gameplayMouse)) {
+            gameplayMouse.enabled = false;
+        }
+    }
+
     void TogglePhantom() {
         if (!Helper.LocalPlayer.IsNotNull(out PlayerControllerB player)) return;
-        if (!player.cameraContainerTransform.IsNotNull(out Transform cameraParent)) return;
         if (!Helper.CurrentCamera.IsNotNull(out Camera camera) || !camera.enabled) return;
 
-        GameObject cameraGameObject = camera.gameObject;
-        this.EnablePhantom = !this.EnablePhantom;
-        Setting.EnablePhantom = this.EnablePhantom;
-        player.enabled = !this.EnablePhantom;
+        Setting.EnablePhantom = !Setting.EnablePhantom;
+        player.playerBodyAnimator.enabled = !Setting.EnablePhantom;
+        player.thisController.enabled = !Setting.EnablePhantom;
+        player.isFreeCamera = Setting.EnablePhantom;
 
-        if (this.EnablePhantom) {
-            if (!cameraGameObject.TryGetComponent(out KeyboardMovement keyboard)) {
-                keyboard = cameraGameObject.AddComponent<KeyboardMovement>();
-            }
-
-            if (!cameraGameObject.TryGetComponent(out MousePan mouse)) {
-                mouse = cameraGameObject.AddComponent<MousePan>();
-            }
-
-            keyboard.enabled = true;
-            mouse.enabled = true;
-            camera.transform.SetParent(null, true);
+        if (Setting.EnablePhantom) {
+            this.PhantomEnabled(camera);
         }
 
         else {
-            if (this.IsShiftHeld) {
-                player.TeleportPlayer(camera.transform.position);
-            }
-
-            camera.transform.SetParent(cameraParent, false);
-            camera.transform.localPosition = Vector3.zero;
-            camera.transform.localRotation = Quaternion.identity;
-
-            if (cameraGameObject.TryGetComponent(out KeyboardMovement keyboard)) {
-                keyboard.enabled = false;
-            }
-
-            if (cameraGameObject.TryGetComponent(out MousePan mouse)) {
-                mouse.enabled = false;
-            }
-
-            if (!player.gameplayCamera.IsNotNull(out Camera gameplayCam)) {
-                return;
-            }
-
-            if (gameplayCam.TryGetComponent(out KeyboardMovement gameplayKeyboard)) {
-                gameplayKeyboard.enabled = false;
-            }
-
-            if (gameplayCam.TryGetComponent(out MousePan gameplayMouse)) {
-                gameplayMouse.enabled = false;
-            }
+            this.PhantomDisabled(player, camera);
         }
     }
 }
