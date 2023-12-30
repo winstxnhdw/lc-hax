f6ade5f1cec3568642d3eb042c1e4a7342e94e14
winstxnhdw <winstxnhdw@gmail.com>
2023-12-30 10:19:34 +0800

    style: separate `IEnumerable` overload from the rest

--- lc-hax/Scripts/Extensions/ForEach.cs
+++ lc-hax/Scripts/Extensions/ForEach.cs
@@ -5,15 +5,15 @@ using UnityObject = UnityEngine.Object;
 namespace Hax;
 
 public static partial class Extensions {
-    public static void ForEach<T>(this T[] array, Action<T> action) {
-        for (int i = 0; i < array.Length; i++) {
-            action(array[i]);
-        }
-    }
-
     public static void ForEach<T>(this IEnumerable<T> array, Action<T> action) {
         foreach (T item in array) {
             action(item);
+        }
+    }
+
+    public static void ForEach<T>(this T[] array, Action<T> action) {
+        for (int i = 0; i < array.Length; i++) {
+            action(array[i]);
         }
     }
 
