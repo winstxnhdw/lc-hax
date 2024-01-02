423fc043c79f947f9e9ec7b9a74df53101d981f8
winstxnhdw <winstxnhdw@gmail.com>
2024-01-02 10:41:18 +0800

    refactor: instance property can now be removed

--- lc-hax/Scripts/Components/TransientBehaviour.cs
+++ lc-hax/Scripts/Components/TransientBehaviour.cs
@@ -8,15 +8,13 @@ public class TransientBehaviour : MonoBehaviour {
     Action<float>? Action { get; set; }
     Action? DisposeAction { get; set; }
     float ExpireTime { get; set; } = 0.0f;
-    float Delay { get; set; } = 0.0f;
 
     public TransientBehaviour Init(Action<float> action, float expireTime, float delay = 0.0f) {
         this.Action = action;
         this.ExpireTime = expireTime;
-        this.Delay = delay;
 
-        _ = this.Delay > 0.0f
-            ? this.StartCoroutine(this.TransientCoroutineWithDelay())
+        _ = delay > 0.0f
+            ? this.StartCoroutine(this.TransientCoroutine(delay))
             : this.StartCoroutine(this.TransientCoroutine());
 
         return this;
@@ -26,13 +24,13 @@ public class TransientBehaviour : MonoBehaviour {
         this.DisposeAction = disposeAction;
     }
 
-    IEnumerator TransientCoroutineWithDelay() {
+    IEnumerator TransientCoroutine(float delay) {
         while (this.ExpireTime > 0.0f) {
             float deltaTime = Time.deltaTime;
             this.ExpireTime -= deltaTime;
             this.Action?.Invoke(deltaTime);
 
-            yield return new WaitForSeconds(this.Delay);
+            yield return new WaitForSeconds(delay);
         }
 
         this.DisposeAction?.Invoke();
