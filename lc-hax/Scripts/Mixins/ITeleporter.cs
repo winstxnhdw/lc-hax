5e1d38829027b8da1d02b1e07005d8ef8c01c52c
winstxnhdw <winstxnhdw@gmail.com>
2024-01-02 10:31:14 +0800

    feat: reduce teleporter expiration timer to `5s`

--- lc-hax/Scripts/Mixins/ITeleporter.cs
+++ lc-hax/Scripts/Mixins/ITeleporter.cs
@@ -1,4 +1,6 @@
 using System;
+using UnityEngine;
+using GameNetcodeStuff;
 
 namespace Hax;
 
@@ -23,4 +25,54 @@ public static class ITeleporterMixin {
               .SetPredicate(self.TeleporterExists)
               .Init(action);
     }
+
+    public static Action PlaceAndTeleport(
+        this ITeleporter self,
+        PlayerControllerB player,
+        Vector3 position
+    ) => () => {
+        HaxObjects.Instance?.ShipTeleporters.Renew();
+
+        if (!self.TryGetTeleporter(out ShipTeleporter teleporter)) {
+            Console.Print("ShipTeleporter not found!");
+            return;
+        }
+
+        Transform newTransform = player.transform.Copy();
+        newTransform.transform.position = position;
+
+        Vector3 rotationOffset = new(-90.0f, 0.0f, 0.0f);
+        Vector3 positionOffset = new(0.0f, 1.6f, 0.0f);
+
+        ObjectPlacement<Transform, ShipTeleporter> teleporterPlacement = new(
+            newTransform,
+            teleporter,
+            positionOffset,
+            rotationOffset
+        );
+
+        ObjectPlacement<Transform, ShipTeleporter> previousTeleporterPlacement = new(
+            teleporter.transform.Copy(),
+            teleporter,
+            positionOffset,
+            rotationOffset
+        );
+
+        Helper.CreateComponent<TransientBehaviour>()
+              .Init(_ => Helper.PlaceObjectAtPosition(teleporterPlacement), 5.0f)
+              .Dispose(() => Helper.PlaceObjectAtPosition(previousTeleporterPlacement));
+
+        teleporter.PressTeleportButtonServerRpc();
+    };
+
+    public static Action TeleportPlayerToPositionLater(
+        this ITeleporter self,
+        PlayerControllerB player,
+        Vector3 position
+    ) => () => {
+        Helper.SwitchRadarTarget(player);
+        Helper.CreateComponent<WaitForBehaviour>()
+              .SetPredicate(() => Helper.IsRadarTarget(player.playerClientId))
+              .Init(self.PlaceAndTeleport(player, position));
+    };
 }
