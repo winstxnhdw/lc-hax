f22c5f3e79ecbd4f03cee18060108b88e4039912
winstxnhdw <winstxnhdw@gmail.com>
2024-01-02 09:49:28 +0800

    style: use arrow function

--- lc-hax/Scripts/Patches/NameBillboardPatch.cs
+++ lc-hax/Scripts/Patches/NameBillboardPatch.cs
@@ -7,7 +7,5 @@ namespace Hax;
 
 [HarmonyPatch(typeof(PlayerControllerB), "LateUpdate")]
 class NameBillboardPatch {
-    static void Postfix(ref PlayerControllerB __instance) {
-        __instance.ShowNameBillboard();
-    }
+    static void Postfix(ref PlayerControllerB __instance) => __instance.ShowNameBillboard();
 }
