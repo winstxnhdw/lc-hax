26901413e9516f2ff2caf5ae8ae1484672feaa68
winstxnhdw <winstxnhdw@gmail.com>
2024-01-02 08:21:53 +0800

    feat: set `turnAmount` to 0

--- lc-hax/Scripts/Modules/SaneMod.cs
+++ lc-hax/Scripts/Modules/SaneMod.cs
@@ -7,11 +7,13 @@ namespace Hax;
 public sealed class SaneMod : MonoBehaviour {
     IEnumerator SetSanity(object[] args) {
         while (true) {
-            if (!Helper.LocalPlayer.IsNotNull(out PlayerControllerB localPlayer)) {
+            if (!Helper.StartOfRound.IsNotNull(out StartOfRound startOfRound)) {
                 yield return new WaitForEndOfFrame();
                 continue;
             }
 
+            PlayerControllerB localPlayer = startOfRound.localPlayerController;
+            startOfRound.gameStats.allPlayerStats[localPlayer.playerClientId].turnAmount = 0;
             localPlayer.playersManager.fearLevel = 0.0f;
             localPlayer.playersManager.fearLevelIncreasing = false;
             localPlayer.insanityLevel = 0.0f;
