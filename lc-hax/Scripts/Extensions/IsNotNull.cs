1737a9623affee4d0878c28da6e256d688d8d3cc
winstxnhdw <winstxnhdw@gmail.com>
2024-01-04 00:41:25 +0800

    refactor: constrain `Unfake` to `UnityEngine.Object` - Fake nulls only apply to `UnityEngine.Object` types so we won't mistakenly try to unfake a non-faked null

--- lc-hax/Scripts/Extensions/IsNotNull.cs
+++ lc-hax/Scripts/Extensions/IsNotNull.cs
@@ -1,13 +1,11 @@
 public static partial class Extensions {
-    /// <summary>
-    /// Replaces Unity's "fake" null with a real null when applicable, allowing the "?." operator to work properly.
-    /// </summary>
-    public static T? Unfake<T>(this T? obj) where T : class {
-        return obj is null || obj.Equals(null) ? null : obj;
-    }
-
     public static bool IsNotNull<T>(this T? obj, out T notNullObj) where T : class {
-        notNullObj = obj.Unfake()!;
-        return notNullObj is not null;
+        if (obj is null || obj.Equals(null)) {
+            notNullObj = null!;
+            return false;
+        }
+
+        notNullObj = obj;
+        return true;
     }
 }
