a49f847aa56b0eed1d034dea0310d4191ce99f8c
winstxnhdw <winstxnhdw@gmail.com>
2024-01-03 14:23:34 +0800

    feat: allow offensive lobby names

--- lc-hax/Scripts/Patches/NoRefreshDelayPatch.cs
+++
@@ -1,8 +0,0 @@
-#pragma warning disable IDE1006
-
-using HarmonyLib;
-
-[HarmonyPatch(typeof(SteamLobbyManager), nameof(SteamLobbyManager.RefreshServerListButton))]
-class NoRefreshDelayPatch {
-    static void Prefix(ref float ___refreshServerListTimer) => ___refreshServerListTimer = 1.0f;
-}
