a13dd976c3f600a6b84edc05e98c71f230261139
winstxnhdw <winstxnhdw@gmail.com>
2024-01-03 23:22:58 +0800

    refactor: inline `RenderESP` method

--- lc-hax/Scripts/Helpers/Screen.cs
+++ lc-hax/Scripts/Helpers/Screen.cs
@@ -6,9 +6,7 @@ public static partial class Helper {
     public static Vector2 GetScreenCentre() => new Vector2(Screen.width, Screen.height) * 0.5f;
 
     public static Vector3 WorldToEyesPoint(this Camera camera, Vector3 worldPosition) {
-        Vector3 screen = camera.WorldToViewportPoint(worldPosition);
-        screen.x *= Screen.width;
-        screen.y *= Screen.height;
+        Vector3 screen = camera.WorldToViewportPoint(worldPosition) * new Vector2(Screen.width, Screen.height);
         screen.y = Screen.height - screen.y;
 
         return screen;
