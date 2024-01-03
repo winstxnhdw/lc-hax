8e83e24988aefd5770398e3a8884326b8504fca3
winstxnhdw <winstxnhdw@gmail.com>
2024-01-03 12:02:34 +0800

    feat: add `ESP` for Landmines and Turrets

--- lc-hax/Scripts/Core/GameListener.cs
+++ lc-hax/Scripts/Core/GameListener.cs
@@ -6,11 +6,29 @@ namespace Hax;
 public class GameListener : MonoBehaviour {
     public static event Action? onGameStart;
     public static event Action? onGameEnd;
+    public static event Action? onShipLand;
+    public static event Action? onShipLeave;
 
     bool InGame { get; set; } = false;
+    bool ShipLand { get; set; } = false;
 
     void Update() {
         this.InGameListener();
+        this.ShipDoorListener();
+    }
+
+    void ShipDoorListener() {
+        if (!Helper.StartOfRound.IsNotNull(out StartOfRound startOfRound)) return;
+
+        if (startOfRound.shipHasLanded && !this.ShipLand) {
+            this.ShipLand = true;
+            GameListener.onShipLand?.Invoke();
+        }
+
+        else if (!startOfRound.shipHasLanded && this.ShipLand) {
+            this.ShipLand = false;
+            GameListener.onShipLeave?.Invoke();
+        }
     }
 
     void InGameListener() {
