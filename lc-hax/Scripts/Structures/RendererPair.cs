5f531b421b732c92263239a2d4a66c4588aacb97
winstxnhdw <winstxnhdw@gmail.com>
2024-01-04 16:58:39 +0800

    fix: vector multiplication is returning wrong screen values

--- lc-hax/Scripts/Structures/RendererPair.cs
+++ lc-hax/Scripts/Structures/RendererPair.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
 public readonly struct RendererPair<T, R>(T gameObject, R renderer) where T : Object where R : Renderer {
-    public readonly R Renderer { get; } = renderer;
     public readonly T GameObject { get; } = gameObject;
+    public readonly R Renderer { get; } = renderer;
 }
