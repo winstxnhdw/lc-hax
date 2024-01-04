c66e361f537ee8311397b13ee12cf44343f15c9d
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-01-04 22:26:57 +0800

    fix: use correct parameter name

--- lc-hax/Scripts/Patches/InvisiblePatch.cs
+++ lc-hax/Scripts/Patches/InvisiblePatch.cs
@@ -1,17 +1,16 @@
-/*using GameNetcodeStuff;
+using GameNetcodeStuff;
 using HarmonyLib;
 using UnityEngine;
 using Hax;
 
 [HarmonyPatch(typeof(PlayerControllerB), "UpdatePlayerPositionServerRpc")]
 class InvisiblePatch {
-    static void Prefix(ref Vector3 pos, ref bool inElevator, ref bool exhausted, ref bool isPlayerGrounded) {
+    static void Prefix(ref Vector3 newPos, ref bool inElevator, ref bool exhausted, ref bool isPlayerGrounded) {
         if (Setting.EnableInvisible) {
-            pos = new Vector3(0, -100, 0);
+            newPos = new Vector3(0, -100, 0);
             inElevator = false;
             exhausted = false;
             isPlayerGrounded = true;
         }
     }
 }
-*/
