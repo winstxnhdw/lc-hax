b200560fff6f5483afa756d228103734d65c6331
winstxnhdw <winstxnhdw@gmail.com>
2024-01-04 16:41:56 +0800

    build: `launch.bat` will now pull latest - `launch-dev.bat` will only build and inject

---
+++ launch-dev.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set project_name=lc-hax
+
+:begin
+dotnet build %project_name% -c Release
+start /wait /b ./submodules/SharpMonoInjectorCore/dist/SharpMonoInjector.exe inject -p "Lethal Company" -a bin/%project_name%.dll -n Hax -c Loader -m Load
+
+pause
+goto begin
--- launch.bat
+++ launch.bat
@@ -1,9 +1,4 @@
 @echo off
 
-:begin
-set project_name=lc-hax
-dotnet build %project_name% -c Release
-start /wait /b ./submodules/SharpMonoInjectorCore/dist/SharpMonoInjector.exe inject -p "Lethal Company" -a bin/%project_name%.dll -n Hax -c Loader -m Load
-
-pause
-goto begin
+git pull --rebase --autostash
+call launch-dev.bat
