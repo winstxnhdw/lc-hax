98914e77f5f0465126d83161fb09e5233d61e705
winstxnhdw <winstxnhdw@gmail.com>
2024-01-06 00:51:04 +0800

    fix: add overload for `First`

--- lc-hax/Scripts/Extensions/First.cs
+++ lc-hax/Scripts/Extensions/First.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 public static partial class Extensions {
-    public static T? First<T>(this IEnumerable<T> array, Func<T, bool> predicate) {
-        return array.FirstOrDefault(predicate);
-    }
+    public static T? First<T>(this IEnumerable<T> array, Func<T, bool> predicate) => array.FirstOrDefault(predicate);
+
+    public static T? First<T>(this IEnumerable<T> array) => array.FirstOrDefault();
 }
