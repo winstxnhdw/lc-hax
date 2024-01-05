7f24121f20a491ef056e21961037f809e1be6d84
winstxnhdw <winstxnhdw@gmail.com>
2024-01-05 21:07:24 +0800

    feat: `anti-kick` will now automatically toggle invisibility

--- lc-hax/Scripts/Modules/AntiKickMod.cs
+++ lc-hax/Scripts/Modules/AntiKickMod.cs
@@ -5,11 +5,26 @@ namespace Hax;
 public sealed class AntiKickMod : MonoBehaviour {
     void OnEnable() {
         InputListener.onBackslashPress += this.ToggleAntiKick;
+        GameListener.onGameStart += this.PrintInvisibleWarning;
     }
 
     void OnDisable() {
         InputListener.onBackslashPress -= this.ToggleAntiKick;
+        GameListener.onGameStart += this.PrintInvisibleWarning;
     }
 
-    void ToggleAntiKick() => Setting.EnableAntiKick = !Setting.EnableAntiKick;
+    void ToggleAntiKick() {
+        if (Helper.LocalPlayer is not null) {
+            Console.Print("You cannot toggle anti-kick while in-game!");
+            return;
+        }
+
+        Setting.EnableAntiKick = !Setting.EnableAntiKick;
+        Setting.EnableInvisible = Setting.EnableAntiKick;
+    }
+
+    void PrintInvisibleWarning() {
+        if (!Setting.EnableAntiKick) return;
+        Console.Print("You are invisible! Do /invis to disable!");
+    }
 }
