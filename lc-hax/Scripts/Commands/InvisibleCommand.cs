e8014976140432c7a85d0b1e5373f29b6706aa92
winstxnhdw <winstxnhdw@gmail.com>
2024-01-05 21:01:15 +0800

    fix: turn invisible without having to move first

--- lc-hax/Scripts/Commands/InvisibleCommand.cs
+++ lc-hax/Scripts/Commands/InvisibleCommand.cs
@@ -2,8 +2,12 @@ namespace Hax;
 
 [Command("/invis")]
 public class InvisibleCommand : ICommand {
-    public void Execute(string[] _) {
+    public void Execute(string[] args) {
         Setting.EnableInvisible = !Setting.EnableInvisible;
         Console.Print($"Invisible: {(Setting.EnableInvisible ? "enabled" : "disabled")}");
+
+        if (Setting.EnableInvisible) {
+            _ = Helper.LocalPlayer?.Reflect().InvokeInternalMethod("UpdatePlayerPositionServerRpc");
+        }
     }
 }
