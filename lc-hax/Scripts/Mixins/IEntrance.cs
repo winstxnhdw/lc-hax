d1126ef8d02a5171ec1078ce619a8c9f608ea266
winstxnhdw <winstxnhdw@gmail.com>
2024-01-05 22:31:45 +0800

    refactor: use null conditional

--- lc-hax/Scripts/Mixins/IEntrance.cs
+++ lc-hax/Scripts/Mixins/IEntrance.cs
@@ -1,12 +1,9 @@
-using GameNetcodeStuff;
-
 namespace Hax;
 
 public interface IEntrance { }
 
 public static class IEntranceMixin {
     public static void EntranceTeleport(this IEntrance _, bool outside) {
-        if (!Helper.LocalPlayer.IsNotNull(out PlayerControllerB localPlayer)) return;
-        localPlayer.TeleportPlayer(RoundManager.FindMainEntranceScript(outside).entrancePoint.position);
+        Helper.LocalPlayer?.TeleportPlayer(RoundManager.FindMainEntranceScript(outside).entrancePoint.position);
     }
 }
