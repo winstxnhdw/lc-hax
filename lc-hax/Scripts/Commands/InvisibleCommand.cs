cc897af180c34d7e82c2396bd9e5dc15b117aa54
winstxnhdw <winstxnhdw@gmail.com>
2024-01-06 23:07:38 +0800

    fix: `UpdatePlayerPositionServerRpc` require args

--- lc-hax/Scripts/Commands/InvisibleCommand.cs
+++ lc-hax/Scripts/Commands/InvisibleCommand.cs
@@ -1,13 +1,20 @@
+using UnityEngine;
+
 namespace Hax;
 
 [Command("/invis")]
 public class InvisibleCommand : ICommand {
+    void ImmediatelyUpdatePlayerPosition() =>
+        Helper.LocalPlayer?
+              .Reflect()
+              .InvokeInternalMethod("UpdatePlayerPositionServerRpc", Vector3.zero, true, false, true);
+
     public void Execute(string[] args) {
         Setting.EnableInvisible = !Setting.EnableInvisible;
         Console.Print($"Invisible: {(Setting.EnableInvisible ? "enabled" : "disabled")}");
 
         if (Setting.EnableInvisible) {
-            _ = Helper.LocalPlayer?.Reflect().InvokeInternalMethod("UpdatePlayerPositionServerRpc");
+            this.ImmediatelyUpdatePlayerPosition();
         }
     }
 }
