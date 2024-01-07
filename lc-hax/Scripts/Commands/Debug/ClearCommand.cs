dc83b1841187b4d3484371edb389a220267fd892
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-01-07 18:22:21 +0900

    fix: must use `text` field

--- lc-hax/Scripts/Commands/Debug/ClearCommand.cs
+++ lc-hax/Scripts/Commands/Debug/ClearCommand.cs
@@ -4,6 +4,6 @@ namespace Hax;
 public class ClearCommand : ICommand {
     public void Execute(string[] _) {
         if (!Helper.HUDManager.IsNotNull(out HUDManager hudManager)) return;
-        hudManager.chatText = "";
+        hudManager.chatText.text = "";
     }
 }
