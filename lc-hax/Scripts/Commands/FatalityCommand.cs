f5b57bf40daced441d4287f9a3808079d4eb7100
winstxnhdw <winstxnhdw@gmail.com>
2024-01-08 23:28:21 +0800

    refactor: try to abstract duplicate code

--- lc-hax/Scripts/Commands/FatalityCommand.cs
+++ lc-hax/Scripts/Commands/FatalityCommand.cs
@@ -3,42 +3,61 @@ using Hax;
 
 [Command("/fatality")]
 public class FatalityCommand : ICommand {
+    T? GetEnemy<T>() where T : EnemyAI {
+        if (Helper.FindObject<T>() is not T enemy) return null;
+        if (Helper.LocalPlayer is not PlayerControllerB localPlayer) return null;
+
+        enemy.ChangeEnemyOwnerServerRpc(localPlayer.actualClientId);
+        return enemy;
+    }
+
+    Result HandleGiant(PlayerControllerB targetPlayer) {
+        if (this.GetEnemy<ForestGiantAI>() is not ForestGiantAI forestGiant) {
+            return new Result(message: "Enemy has not yet spawned!");
+        }
+
+        forestGiant.GrabPlayerServerRpc((int)targetPlayer.playerClientId);
+        return new Result(true);
+    }
+
+    Result HandleJester(PlayerControllerB targetPlayer) {
+        if (this.GetEnemy<JesterAI>() is not JesterAI jester) {
+            return new Result(message: "Enemy has not yet spawned!");
+        }
+
+        jester.KillPlayerServerRpc((int)targetPlayer.playerClientId);
+        return new Result(true);
+    }
+
+    Result HandleMask(PlayerControllerB targetPlayer) {
+        if (this.GetEnemy<MaskedPlayerEnemy>() is not MaskedPlayerEnemy spider) {
+            return new Result(message: "Enemy has not yet spawned!");
+        }
+
+        spider.KillPlayerAnimationServerRpc((int)targetPlayer.playerClientId);
+        return new Result(true);
+    }
+
     public void Execute(string[] args) {
         if (args.Length < 2) {
             Chat.Print("Usage: /fatality <player> <enemy>");
             return;
         }
 
-        if (Helper.LocalPlayer is not PlayerControllerB localPlayer) {
-            Chat.Print("Local player not found!");
+        if (Helper.GetActivePlayer(args[0]) is not PlayerControllerB targetPlayer) {
+            Chat.Print($"Unable to find player: {args[0]}!");
             return;
         }
 
-        if (Helper.GetActivePlayer(args[0]) is not PlayerControllerB player) {
-            Chat.Print("Invalid player!");
-            return;
-        }
+        Result result = args[1].ToLower() switch {
+            "giant" => this.HandleGiant(targetPlayer),
+            "jester" => this.HandleJester(targetPlayer),
+            "mask" => this.HandleMask(targetPlayer),
+            _ => new Result(message: "Enemy has either not yet been implemented or does not exist!")
+        };
 
-        if (args[1] is "giant") {
-            ForestGiantAI? forestGiant = Helper.FindObject<ForestGiantAI>();
-            forestGiant?.ChangeEnemyOwnerServerRpc(localPlayer.actualClientId);
-            forestGiant?.GrabPlayerServerRpc((int)player.playerClientId);
-        }
-
-        else if (args[1] is "jester") {
-            JesterAI? jester = Helper.FindObject<JesterAI>();
-            jester?.ChangeEnemyOwnerServerRpc(localPlayer.actualClientId);
-            jester?.KillPlayerServerRpc((int)player.playerClientId);
-        }
-
-        else if (args[1] is "mask") {
-            MaskedPlayerEnemy? spider = Helper.FindObject<MaskedPlayerEnemy>();
-            spider?.ChangeEnemyOwnerServerRpc(localPlayer.actualClientId);
-            spider?.KillPlayerAnimationServerRpc((int)player.playerClientId);
-        }
-
-        else {
-            Chat.Print("Enemy fatality has not yet been implemented or does not exist!");
+        if (!result.Success) {
+            Chat.Print(result.Message);
         }
     }
 }
