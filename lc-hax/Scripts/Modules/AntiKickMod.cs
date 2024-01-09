56e799ead0ac9e983b5320306ff249f95fc31abf
winstxnhdw <winstxnhdw@gmail.com>
2024-01-10 07:06:29 +0800

    feat: make antikick more robust

--- lc-hax/Scripts/Modules/AntiKickMod.cs
+++ lc-hax/Scripts/Modules/AntiKickMod.cs
@@ -1,8 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Hax;
 
 public sealed class AntiKickMod : MonoBehaviour {
+    Coroutine? AntiKickCoroutine { get; set; }
+
     void OnEnable() {
         InputListener.onBackslashPress += this.ToggleAntiKick;
         GameListener.onGameStart += this.PrintInvisibleWarning;
@@ -13,6 +16,15 @@ public sealed class AntiKickMod : MonoBehaviour {
         GameListener.onGameStart += this.PrintInvisibleWarning;
     }
 
+    IEnumerator SendSpoofedValues() {
+        Reflector? playerReflector = Helper.LocalPlayer?.Reflect();
+
+        while (true) {
+            _ = playerReflector?.InvokeInternalMethod("SendNewPlayerValuesServerRpc");
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
     void ToggleAntiKick() {
         if (Helper.LocalPlayer is not null) {
             Chat.Print("You cannot toggle anti-kick while in-game!");
@@ -21,6 +33,14 @@ public sealed class AntiKickMod : MonoBehaviour {
 
         Setting.EnableAntiKick = !Setting.EnableAntiKick;
         Setting.EnableInvisible = Setting.EnableAntiKick;
+
+        if (Setting.EnableAntiKick) {
+            this.AntiKickCoroutine = this.StartCoroutine(this.SendSpoofedValues());
+        }
+
+        else {
+            this.StopCoroutine(this.AntiKickCoroutine);
+        }
     }
 
     void PrintInvisibleWarning() {
