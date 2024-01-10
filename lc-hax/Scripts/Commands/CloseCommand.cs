f0ea5aa53877875dabc489e3232a1217941b75e2
winstxnhdw <winstxnhdw@gmail.com>
2024-01-10 22:18:39 +0800

    refactor: move common commands in subfolders

--- lc-hax/Scripts/Commands/CloseCommand.cs
+++
@@ -1,8 +0,0 @@
-using Hax;
-
-[Command("/close")]
-public class CloseCommand : ICommand {
-    public void Execute(string[] args) {
-        Helper.CloseShipDoor(true);
-    }
-}
--- lc-hax/Scripts/Commands/OpenCommand.cs
+++
@@ -1,8 +0,0 @@
-using Hax;
-
-[Command("/open")]
-public class OpenCommand : ICommand {
-    public void Execute(string[] args) {
-        Helper.CloseShipDoor(false);
-    }
-}
