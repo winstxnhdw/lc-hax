2c8f62e65eba53b00503392565428b0416d51317
winstxnhdw <winstxnhdw@gmail.com>
2024-01-10 22:50:49 +0800

    feat: `sell` won't deposit defensive weapons

--- lc-hax/Scripts/Commands/SellCommand.cs
+++ lc-hax/Scripts/Commands/SellCommand.cs
@@ -5,14 +5,21 @@ using Hax;
 public class SellCommand : ICommand {
     public void Execute(string[] _) {
         if (Helper.LocalPlayer is not PlayerControllerB player) return;
-        if (Helper.FindObject<DepositItemsDesk>().Unfake() is not DepositItemsDesk depositItemsDesk) return;
-        if (player.currentlyHeldObjectServer.Unfake() is GrabbableObject heldObject) {
-            depositItemsDesk.PlaceItemOnCounter(player);
+
+        if (Helper.FindObject<DepositItemsDesk>().Unfake() is not DepositItemsDesk depositItemsDesk) {
+            Chat.Print("You must be at the company to use this command!");
+            return;
+        }
+
+        if (player.currentlyHeldObjectServer.Unfake() is not null) {
+            Chat.Print("You must not be holding anything to use this command!");
+            return;
         }
 
         HaxObjects.Instance?.GrabbableObjects.ForEach(nullableGrabbableObject => {
             if (!nullableGrabbableObject.IsNotNull(out GrabbableObject grabbableObject)) return;
             if (grabbableObject.scrapValue <= 0) return;
+            if (grabbableObject.itemProperties.isDefensiveWeapon) return;
 
             player.currentlyHeldObjectServer = grabbableObject;
             depositItemsDesk.PlaceItemOnCounter(player);
