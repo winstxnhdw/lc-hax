beb215618da180827797521bac4758962f559d47
winstxnhdw <winstxnhdw@gmail.com>
2024-01-12 00:36:42 +0800

    feat: remove permanently broken build features

--- lc-hax/Scripts/Commands/TeleportCommand.cs
+++ lc-hax/Scripts/Commands/TeleportCommand.cs
@@ -3,7 +3,7 @@ using GameNetcodeStuff;
 using Hax;
 
 [Command("/tp")]
-public class TeleportCommand : ITeleporter, ICommand {
+public class TeleportCommand : ICommand {
     Vector3? GetCoordinates(string[] args) {
         bool isValidX = float.TryParse(args[0], out float x);
         bool isValidY = float.TryParse(args[1], out float y);
@@ -35,46 +35,16 @@ public class TeleportCommand : ITeleporter, ICommand {
         return new Result(true);
     }
 
-    Result TeleportPlayerToPosition(PlayerControllerB player, Vector3 position) {
-        this.PrepareToTeleport(this.TeleportPlayerToPositionLater(player, position));
-        return new Result(true);
-    }
-
-    Result TeleportPlayerToPosition(string[] args) {
-        if (!Helper.GetPlayer(args[0]).IsNotNull(out PlayerControllerB player)) {
-            return new Result(message: "Player not found!");
-        }
-
-        Vector3? coordinates = this.GetCoordinates(args[1..]);
-
-        return coordinates is null
-            ? new Result(message: "Invalid coordinates!")
-            : this.TeleportPlayerToPosition(player, coordinates.Value);
-    }
-
-    Result TeleportPlayerToPlayer(string[] args) {
-        PlayerControllerB? sourcePlayer = Helper.GetPlayer(args[0]);
-        PlayerControllerB? targetPlayer = Helper.GetPlayer(args[1]);
-
-        return sourcePlayer is null || targetPlayer is null
-            ? new Result(message: "Player not found!")
-            : this.TeleportPlayerToPosition(sourcePlayer, targetPlayer.transform.position);
-    }
-
     public void Execute(string[] args) {
         if (args.Length is 0) {
             Chat.Print("Usage: /tp <player>");
             Chat.Print("Usage: /tp <x> <y> <z>");
-            Chat.Print("Usage: /tp <player> <x> <y> <z>");
-            Chat.Print("Usage: /tp <player> <player>");
             return;
         }
 
         Result result = args.Length switch {
             1 => this.TeleportToPlayer(args),
-            2 => this.TeleportPlayerToPlayer(args),
             3 => this.TeleportToPosition(args),
-            4 => this.TeleportPlayerToPosition(args),
             _ => new Result(message: "Invalid arguments!")
         };
 
