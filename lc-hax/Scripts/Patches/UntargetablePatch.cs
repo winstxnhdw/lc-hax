7ec05200277eabb6661633889562370994c084b9
Zero <knuklesetn@hotmail.com>
2024-01-12 22:55:23 +0100

    Update UntargetablePatch.cs
    Fixed targetPlayer and Nutcracker

--- lc-hax/Scripts/Patches/UntargetablePatch.cs
+++ lc-hax/Scripts/Patches/UntargetablePatch.cs
@@ -7,10 +7,32 @@ using Hax;
 [HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.PlayerIsTargetable))]
 class UntargetablePatch {
     public static bool Prefix(PlayerControllerB playerScript, ref bool __result) {
-        if (!Setting.EnableUntargetable && !Setting.EnableGodMode) return true;
+        if (!Setting.EnableUntargetable) return true;
         if (Helper.LocalPlayer?.actualClientId != playerScript.actualClientId) return true;
 
         __result = false;
         return false;
     }
 }
+
+[HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.Update))]
+class UntargetableEnemyUpdatePatch {
+    public static bool Prefix(EnemyAI __instance) {
+        if (!Setting.EnableUntargetable) return true;
+        if (!__instance.targetPlayer) return true;
+        if (Helper.LocalPlayer?.actualClientId != __instance.targetPlayer.actualClientId) return true;
+
+        __instance.targetPlayer = null;
+        __instance.movingTowardsTargetPlayer = false;
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(NutcrackerEnemyAI), nameof(NutcrackerEnemyAI.CheckLineOfSightForLocalPlayer))]
+class UntargetableNutcrackerPatch {
+    static bool Prefix() => !Setting.EnableUntargetable;
+}
+
+//TODO: PufferAI.Update
+//__instance.playerIsInLOS = false;
+//__instance.closestSeenPlayer = null;
