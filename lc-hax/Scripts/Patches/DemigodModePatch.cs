65bccbe39ad3c743d714535913993c6bec027b7c
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-01-13 22:57:56 +0800

    fix/docs: allow player to hit others in full god mode (#248)

--- lc-hax/Scripts/Patches/DemigodModePatch.cs
+++
@@ -1,8 +0,0 @@
-using GameNetcodeStuff;
-using HarmonyLib;
-using Hax;
-
-[HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.DamagePlayer))]
-class DemigodModePatch {
-    static bool Prefix() => !Setting.EnableDemigodMode;
-}
