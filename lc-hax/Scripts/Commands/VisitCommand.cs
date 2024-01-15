b313d597248f870bd387d10e6d837c4099e5ea9e
winstxnhdw <winstxnhdw@gmail.com>
2024-01-16 02:13:58 +0800

    feat(visit): use fuzzy match

--- lc-hax/Scripts/Commands/VisitCommand.cs
+++ lc-hax/Scripts/Commands/VisitCommand.cs
@@ -1,26 +1,10 @@
-using Hax;
 using System;
 using System.Linq;
+using System.Collections.Generic;
+using Hax;
 
 [Command("/visit")]
 public class VisitCommand : ICommand {
-    bool IsValidLevelIndex(string levelIndex, out int chosenLevelId) =>
-        Enum.TryParse(levelIndex, out chosenLevelId) &&
-        Enum.IsDefined(typeof(Level), chosenLevelId);
-
-    bool TryParseLevel(string levelNameOrId, out int levelIndex) {
-        if (int.TryParse(levelNameOrId, out _)) {
-            return this.IsValidLevelIndex(levelNameOrId, out levelIndex);
-        }
-
-        if (Enum.TryParse(levelNameOrId, true, out Level _)) {
-            return this.IsValidLevelIndex(levelNameOrId, out levelIndex);
-        }
-
-        levelIndex = -1;
-        return false;
-    }
-
     public void Execute(ReadOnlySpan<string> args) {
         if (args.Length is 0) {
             Chat.Print("Usage: /visit <moon>");
@@ -32,13 +16,19 @@ public class VisitCommand : ICommand {
             return;
         }
 
-        if (!this.TryParseLevel(args[0], out int levelIndex)) {
-            Chat.Print("Invalid level!");
-            string levels = Enum.GetValues(typeof(Level)).Cast<Level>().Aggregate(string.Empty, (current, level) => current + $"{level} : {(int)level}");
-            Chat.Print(levels);
+        if (Helper.StartOfRound is not StartOfRound startOfRound) {
+            Chat.Print("StartOfRound not found!");
             return;
         }
 
-        Helper.StartOfRound?.ChangeLevelServerRpc(levelIndex, terminal.groupCredits);
+        Dictionary<string, int> levels = startOfRound.levels.ToDictionary(
+            level => level.name[..(level.name.Length - "Level".Length)].ToUpper(),
+            level => level.levelID
+        );
+
+        string key = Helper.FuzzyMatch(args[0].ToUpper(), [.. levels.Keys]);
+        Helper.StartOfRound?.ChangeLevelServerRpc(levels[key], terminal.groupCredits);
+
+        Chat.Print($"Visiting {key}!");
     }
 }
