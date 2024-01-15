7a49d796f24dceff4d6df74c6e35260e5e9fd4d6
winstxnhdw <winstxnhdw@gmail.com>
2024-01-16 03:25:21 +0800

    fix: operand is type `sbyte`

--- lc-hax/Scripts/Patches/InfiniteDepositPatch.cs
+++ lc-hax/Scripts/Patches/InfiniteDepositPatch.cs
@@ -6,7 +6,7 @@ using HarmonyLib;
 class InfiniteDepositPatch {
     static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
         foreach (CodeInstruction instruction in instructions) {
-            yield return instruction.opcode == OpCodes.Ldc_I4_S && instruction.operand.Equals(12)
+            yield return instruction.opcode == OpCodes.Ldc_I4_S && instruction.operand.Equals((sbyte)12)
                 ? new CodeInstruction(OpCodes.Ldc_I4, int.MaxValue)
                 : instruction;
         }
