7f07c36bd3130c18b27ae4a3026a4f6d34e5a212
winstxnhdw <winstxnhdw@gmail.com>
2024-01-15 23:00:04 +0800

    refactor: just check `placingObject` for `null`

--- lc-hax/Scripts/Patches/UnconstrainedBuildPatch.cs
+++ lc-hax/Scripts/Patches/UnconstrainedBuildPatch.cs
@@ -6,10 +6,10 @@ using Hax;
 [HarmonyPatch(typeof(ShipBuildModeManager), "PlayerMeetsConditionsToBuild")]
 class UnconstrainedBuildPatch {
     static bool Prefix(ref bool __result, ref bool ___CanConfirmPosition, ref PlaceableShipObject? ___placingObject) {
-        if (!___placingObject.IsNotNull(out PlaceableShipObject placingObject)) return true;
+        if (___placingObject == null) return true;
 
-        placingObject.AllowPlacementOnCounters = true;
-        placingObject.AllowPlacementOnWalls = true;
+        ___placingObject.AllowPlacementOnCounters = true;
+        ___placingObject.AllowPlacementOnWalls = true;
         ___CanConfirmPosition = true;
         __result = Helper.LocalPlayer?.inTerminalMenu is false;
 
