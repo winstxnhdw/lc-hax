6721f0cfbd0168eff27d1f68a1e8bb861bec97e3
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-01-15 22:47:20 +0800

    fix: return if null

--- lc-hax/Scripts/Patches/UnconstrainedBuildPatch.cs
+++ lc-hax/Scripts/Patches/UnconstrainedBuildPatch.cs
@@ -6,7 +6,7 @@ using Hax;
 [HarmonyPatch(typeof(ShipBuildModeManager), "PlayerMeetsConditionsToBuild")]
 class UnconstrainedBuildPatch {
     static bool Prefix(ref bool __result, ref bool ___CanConfirmPosition, ref PlaceableShipObject? ___placingObject) {
-        if (___placingObject.IsNotNull(out PlaceableShipObject placingObject)) return true;
+        if (!___placingObject.IsNotNull(out PlaceableShipObject placingObject)) return true;
 
         placingObject.AllowPlacementOnCounters = true;
         placingObject.AllowPlacementOnWalls = true;
