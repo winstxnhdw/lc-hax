311992c935dbeb707fdbb4edc015287ae15b8921
winstxnhdw <winstxnhdw@gmail.com>
2024-01-16 21:59:18 +0800

    fix: fake death should not skip method if disabled

--- lc-hax/Scripts/Patches/FakeDeathPatch.cs
+++ lc-hax/Scripts/Patches/FakeDeathPatch.cs
@@ -5,7 +5,7 @@ using Hax;
 [HarmonyPatch(typeof(PlayerControllerB), "KillPlayerClientRpc")]
 class FakeDeathPatch {
     static bool Prefix(ref int playerId) {
-        if (!Setting.EnableFakeDeath) return false;
+        if (!Setting.EnableFakeDeath) return true;
 
         Setting.EnableFakeDeath = false;
         return Helper.LocalPlayer?.actualClientId != (ulong)playerId;
