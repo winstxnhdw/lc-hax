f6a81bb61d59ab7558b7af65723959021320e9be
winstxnhdw <winstxnhdw@gmail.com>
2024-01-19 01:25:54 +0800

    feat/docs: properties should be singular

--- lc-hax/Scripts/Commands/BlockCommand.cs
+++ lc-hax/Scripts/Commands/BlockCommand.cs
@@ -3,44 +3,43 @@ using Hax;
 
 [Command("/block")]
 public class BlockCommand : ICommand {
+    string BlockCredit() {
+        Setting.EnableBlockCredits = !Setting.EnableBlockCredits;
+
+        return $"{(Setting.EnableBlockCredits
+            ? "Blocking all incoming credits!"
+            : "No longer blocking credits!")}";
+    }
+
+    string BlockEnemy() {
+        Setting.EnableUntargetable = !Setting.EnableUntargetable;
+
+        return $"{(Setting.EnableUntargetable
+            ? "Enemies will no longer target you!"
+            : "Enemies can now target you!")}";
+    }
+
+    string BlockRadar() {
+        Setting.EnableBlockRadar = !Setting.EnableBlockRadar;
+
+        return $"{(Setting.EnableBlockRadar
+            ? "Blocking radar targets!"
+            : "No longer blocking radar targets!")}";
+    }
+
     public void Execute(ReadOnlySpan<string> args) {
         if (args.Length is 0) {
             Chat.Print("Usage: /block <property>");
             return;
         }
 
-        if (args[0] is "credits") {
-            Setting.EnableBlockCredits = !Setting.EnableBlockCredits;
+        string result = args[0] switch {
+            "credit" => this.BlockCredit(),
+            "enemy" => this.BlockEnemy(),
+            "radar" => this.BlockRadar(),
+            _ => "Invalid property!"
+        };
 
-            Chat.Print(
-                $"{(Setting.EnableBlockCredits
-                    ? "Blocking all incoming credits!"
-                    : "No longer blocking credits!")}"
-            );
-        }
-
-        else if (args[0] is "enemy") {
-            Setting.EnableUntargetable = !Setting.EnableUntargetable;
-
-            Chat.Print(
-                $"{(Setting.EnableUntargetable
-                    ? "Enemies will no longer target you!"
-                    : "Enemies can now target you!")}"
-            );
-        }
-
-        else if (args[0] is "radar") {
-            Setting.EnableBlockRadar = !Setting.EnableBlockRadar;
-
-            Chat.Print(
-                $"{(Setting.EnableBlockRadar
-                    ? "Blocking radar targets!"
-                    : "No longer blocking radar targets!")}"
-            );
-        }
-
-        else {
-            Chat.Print($"Invalid property!");
-        }
+        Chat.Print(result);
     }
 }
