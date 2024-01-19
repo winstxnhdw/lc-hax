9afa1ed6acfee91e90ca3e27ff5bcb3cac7308a6
winstxnhdw <winstxnhdw@gmail.com>
2024-01-20 06:43:11 +0800

    fix: skip client RPC on self activate

--- lc-hax/Scripts/Patches/GiftBoxPatch.cs
+++ lc-hax/Scripts/Patches/GiftBoxPatch.cs
@@ -2,10 +2,25 @@
 
 using HarmonyLib;
 
-[HarmonyPatch(typeof(GiftBoxItem), nameof(GiftBoxItem.ItemActivate))]
+[HarmonyPatch(typeof(GiftBoxItem))]
 class GiftBoxPatch {
-    static bool Prefix(ref GiftBoxItem __instance) {
+    static bool LocalPlayerActivated { get; set; } = false;
+
+    [HarmonyPatch(nameof(GiftBoxItem.ItemActivate))]
+    static bool Prefix(GiftBoxItem __instance) {
+        GiftBoxPatch.LocalPlayerActivated = true;
         __instance.OpenGiftBoxServerRpc();
         return false;
     }
+
+    [HarmonyPatch(nameof(GiftBoxItem.OpenGiftBoxClientRpc))]
+    static bool Prefix() {
+        bool skipFlag = !GiftBoxPatch.LocalPlayerActivated;
+        GiftBoxPatch.LocalPlayerActivated = false;
+        return skipFlag;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(GiftBoxItem.OpenGiftBoxNoPresentClientRpc))]
+    static bool NoPresentPrefix() => false;
 }
