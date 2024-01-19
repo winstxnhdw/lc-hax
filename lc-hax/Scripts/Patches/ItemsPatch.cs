e4e4e0383cdacbb4766300f7e3cbf9b6421ed4fd
winstxnhdw <winstxnhdw@gmail.com>
2024-01-20 05:46:10 +0800

    refactor: use `itemProperties` directly

--- lc-hax/Scripts/Patches/ItemsPatch.cs
+++ lc-hax/Scripts/Patches/ItemsPatch.cs
@@ -1,11 +1,12 @@
+#pragma warning disable IDE1006
+
 using HarmonyLib;
 
 [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.Start))]
 class ItemsPatch {
-    static void Postfix(ref GrabbableObject __instance) {
-        if (__instance != null && __instance.itemProperties != null) {
-            __instance.itemProperties.canBeGrabbedBeforeGameStart = true;
-            __instance.itemProperties.twoHanded = false;
-        }
+    static void Postfix(Item? ___itemProperties) {
+        if (___itemProperties == null) return;
+        ___itemProperties.canBeGrabbedBeforeGameStart = true;
+        ___itemProperties.twoHanded = false;
     }
 }
