4aab23a023d2b466b73761b465793ac2b1201119
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-01-19 16:12:55 +0100

    Fix A few things
    Remove DemiGod setting (unused)
    Add a EnemyListener
    Clean KillCommand from unused setting
    update csproj

--- lc-hax/Scripts/Helpers/Enemies.cs
+++ lc-hax/Scripts/Helpers/Enemies.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace Hax;
 
 public static partial class Helper {
-    public static List<EnemyAI> Enemies => Helper.RoundManager?.SpawnedEnemies ?? [];
+    public static List<EnemyAI> Enemies => EnemyListener.ActiveEnemies;
 
     public static T? GetEnemy<T>() where T : EnemyAI =>
         Helper.Enemies.First(enemy => enemy is T) is T enemy ? enemy : null;
---
+++ lc-hax/Scripts/Listeners/EnemyListener.cs
@@ -0,0 +1,21 @@
+#pragma warning disable IDE1006
+
+using GameNetcodeStuff;
+using HarmonyLib;
+using System.Collections.Generic;
+
+[HarmonyPatch(typeof(EnemyAI))]
+class EnemyListener {
+    [HarmonyPatch(nameof(EnemyAI.OnDestroy))]
+    static void Prefix(ref EnemyAI __instance) {
+        if(ActiveEnemies.Contains(__instance)) ActiveEnemies.Remove(__instance);
+    }
+
+    [HarmonyPatch(nameof(EnemyAI.Start))]
+    static void Postfix(ref EnemyAI __instance) {
+        if (!ActiveEnemies.Contains(__instance)) ActiveEnemies.Add(__instance);
+    }
+
+
+    public static List<EnemyAI> ActiveEnemies = new();
+}
