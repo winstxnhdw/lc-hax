8e2e892d704291d6d204c73cbad901e6ee7fada8
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-01-25 21:41:14 +0100

    Revert "Fix ESP not covering maskeds"
    This reverts commit 0ca784e966114bf112835edf72f832f462a025c1.

--- lc-hax/Scripts/Modules/ESPMod.cs
+++ lc-hax/Scripts/Modules/ESPMod.cs
@@ -67,25 +67,12 @@ public class ESPMod : MonoBehaviour {
         Helper.ActiveEnemies.WhereIsNotNull().ForEach(enemy => {
             if (enemy.isEnemyDead) return;
             if (enemy is DocileLocustBeesAI or DoublewingAI) return;
-            Renderer? nullableRenderer = null;
 
-            if (enemy is RedLocustBees redLocust) {
-                nullableRenderer = redLocust.meshRenderers.First();
-            }
-            if (enemy is MaskedPlayerEnemy masked) {
-                nullableRenderer = masked.skinnedMeshRenderers.First();
-            }
-
-            if (nullableRenderer == null) {
-                nullableRenderer = enemy.meshRenderers.First();
-            }
-
-            if (nullableRenderer == null) {
-                nullableRenderer = enemy.skinnedMeshRenderers.First();
-            }
+            Renderer? nullableRenderer = enemy is RedLocustBees
+                ? enemy.meshRenderers.First()
+                : enemy.skinnedMeshRenderers.First();
 
             if (nullableRenderer.Unfake() is not Renderer renderer) {
-                Console.WriteLine($"Failed to get renderer for {enemy.enemyType.enemyName}!");
                 return;
             }
 
