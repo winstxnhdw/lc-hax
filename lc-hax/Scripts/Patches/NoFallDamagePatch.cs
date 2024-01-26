4ca18282078b7662c33e6582c61bf3e9f94d0416
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-01-26 13:43:48 +0100

    Discard changes to lc-hax/Scripts/Patches/NoFallDamagePatch.cs

--- lc-hax/Scripts/Patches/NoFallDamagePatch.cs
+++ lc-hax/Scripts/Patches/NoFallDamagePatch.cs
@@ -4,5 +4,5 @@ using Hax;
 
 [HarmonyPatch(typeof(PlayerControllerB), "DamagePlayer")]
 class NoFallDamagePatch {
-    static bool Prefix(CauseOfDeath causeOfDeath) => !(Setting.EnableGodMode || (Setting.DisableFallDamage && causeOfDeath == CauseOfDeath.Gravity));
+    static bool Prefix(CauseOfDeath causeOfDeath) => !Setting.DisableFallDamage || causeOfDeath != CauseOfDeath.Gravity;
 }
