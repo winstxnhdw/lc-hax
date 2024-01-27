0b69bf10257f22924e3b1d03615f6019f000f4d6
winstxnhdw <winstxnhdw@gmail.com>
2024-01-28 01:43:46 +0800

    refactor: combine masked dep with enemy dep

--- lc-hax/Scripts/Patches/Dependencies/EnemyDependencyPatch.cs
+++ lc-hax/Scripts/Patches/Dependencies/EnemyDependencyPatch.cs
@@ -5,7 +5,6 @@ using Hax;
 
 [HarmonyPatch(typeof(EnemyAI))]
 class EnemyDependencyPatch {
-
     [HarmonyPatch(nameof(EnemyAI.OnDestroy))]
     static void Prefix(EnemyAI __instance) {
         _ = Helper.Enemies.Remove(__instance);
@@ -13,6 +12,19 @@ class EnemyDependencyPatch {
 
     [HarmonyPatch(nameof(EnemyAI.Start))]
     static void Postfix(EnemyAI __instance) {
+        _ = Helper.Enemies.Add(__instance);
+    }
+}
+
+[HarmonyPatch(typeof(MaskedPlayerEnemy))]
+class MaskedDependencyPatch {
+    [HarmonyPatch(nameof(MaskedPlayerEnemy.OnDestroy))]
+    static void Prefix(MaskedPlayerEnemy __instance) {
+        _ = Helper.Enemies.Remove(__instance);
+    }
+
+    [HarmonyPatch(nameof(MaskedPlayerEnemy.Start))]
+    static void Postfix(MaskedPlayerEnemy __instance) {
         _ = Helper.Enemies.Add(__instance);
     }
 }
