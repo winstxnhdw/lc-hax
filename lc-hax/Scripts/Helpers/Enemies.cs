3dc48a89da53093550e3d007f065947273f8eec3
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-01-27 17:57:04 +0100

    Update lc-hax/Scripts/Helpers/Enemies.cs
    I mean is the same, just the compiler will remove Helper.Enemies automatically.
    
    Co-authored-by: Winston H. <56998716+winstxnhdw@users.noreply.github.com>

--- lc-hax/Scripts/Helpers/Enemies.cs
+++ lc-hax/Scripts/Helpers/Enemies.cs
@@ -6,5 +6,5 @@ public static partial class Helper {
     public static HashSet<EnemyAI> Enemies { get; } = [];
 
     public static T? GetEnemy<T>() where T : EnemyAI =>
-        Enemies.First(enemy => enemy is T) is T enemy ? enemy : null;
+        Helper.Enemies.First(enemy => enemy is T) is T enemy ? enemy : null;
 }
