836246673858ff6dff97c4fec8931770b7f77bc7
winstxnhdw <winstxnhdw@gmail.com>
2024-01-28 01:44:24 +0800

    style: always include braces if not an if-guard

--- lc-hax/Scripts/Patches/Dependencies/NetworkBehaviourDependencyPatch.cs
+++ lc-hax/Scripts/Patches/Dependencies/NetworkBehaviourDependencyPatch.cs
@@ -1,14 +1,15 @@
 #pragma warning disable IDE1006
 
 using HarmonyLib;
-using Hax;
 using Unity.Netcode;
+using Hax;
 
 [HarmonyPatch(typeof(NetworkBehaviour))]
 class NetworkBehaviourDependencyPatch {
     [HarmonyPatch(nameof(NetworkBehaviour.OnDestroy))]
     static void Prefix(NetworkBehaviour __instance) {
-        if (__instance is GrabbableObject grabbableObject)
+        if (__instance is GrabbableObject grabbableObject) {
             _ = Helper.Grabbables.Remove(grabbableObject);
+        }
     }
 }
