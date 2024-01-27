8e02b8388789104902def58837174227b4bff03b
winstxnhdw <winstxnhdw@gmail.com>
2024-01-28 01:35:58 +0800

    feat/docs: `mask` without arguments will target self

--- lc-hax/Scripts/Commands/MaskCommand.cs
+++ lc-hax/Scripts/Commands/MaskCommand.cs
@@ -3,43 +3,36 @@ using Hax;
 
 [Command("/mask")]
 public class MaskCommand : ICommand {
-
-    public void Execute(StringArray args) {
-        if (args.Length == 0) {
-            Chat.Print("Usage: /mask self OR /mask <player> (optional) <amount>");
-            return;
-        }
-
-        if (Helper.LocalPlayer?.currentlyHeldObjectServer is not HauntedMaskItem hauntedMaskItem) {
-            Chat.Print("You are not holding a mask!");
-            return;
-        }
-        PlayerControllerB targetPlayer;
-
-        if (args[0].ToLower() == "self") {
-            targetPlayer = Helper.LocalPlayer;
-        }
-        else {
-            if (Helper.GetActivePlayer(args[0]) is not PlayerControllerB player) {
-                Chat.Print("Player not found!");
-                return;
-            }
-            targetPlayer = player;
-        }
-
-        int Amount = 1;
-
-        if (args.Length >= 3 && int.TryParse(args[2], out int parsedTargetAmount) && parsedTargetAmount >= 0) {
-            Amount = parsedTargetAmount;
-        }
-
-        this.SpawnMimicForPlayer(targetPlayer, hauntedMaskItem, Amount);
-    }
-
-    public void SpawnMimicForPlayer(PlayerControllerB player, HauntedMaskItem mask, int amount) {
-        if (player is null || mask is null) return;
+    void SpawnMimicOnPlayer(PlayerControllerB player, HauntedMaskItem mask, int amount = 1) {
         for (int i = 0; i < amount; i++) {
             mask.CreateMimicServerRpc(player.isInsideFactory, player.transform.position);
         }
+    }
+
+    public void Execute(StringArray args) {
+        if (Helper.LocalPlayer is not PlayerControllerB localPlayer) return;
+        if (localPlayer.currentlyHeldObjectServer is not HauntedMaskItem hauntedMaskItem) {
+            Chat.Print("You are not holding a mask!");
+            return;
+        }
+
+        PlayerControllerB? targetPlayer = args.Length is 0 ? localPlayer : Helper.GetActivePlayer(args[0]);
+
+        if (targetPlayer is null) {
+            Chat.Print("Target player is not alive or found!");
+            return;
+        }
+
+        if (args.Length < 3) {
+            this.SpawnMimicOnPlayer(targetPlayer, hauntedMaskItem);
+            return;
+        }
+
+        if (!int.TryParse(args[2], out int amount)) {
+            Chat.Print("Invalid amount!");
+            return;
+        }
+
+        this.SpawnMimicOnPlayer(targetPlayer, hauntedMaskItem, amount);
     }
 }
