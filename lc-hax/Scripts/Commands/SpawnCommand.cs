d3077d0689231310fde50e234a3fe354e075996e
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-01-28 18:04:08 +0100

    Update lc-hax/Scripts/Commands/SpawnCommand.cs
    Co-authored-by: Winston H. <56998716+winstxnhdw@users.noreply.github.com>

--- lc-hax/Scripts/Commands/SpawnCommand.cs
+++ lc-hax/Scripts/Commands/SpawnCommand.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-[Command("/spawn")]
+[DebugCommand("/spawn")]
 public class SpawnCommand : ICommand {
 
     private void SpawnEnemyOnPlayer(PlayerControllerB player, GameObject prefab, ulong amount = 1) {
