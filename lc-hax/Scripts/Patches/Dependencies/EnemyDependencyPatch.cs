8fdea890619b3cf07541caa78e9aecac4c17e0c0
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-01-28 03:38:41 +0100

    Update EnemyDependencyPatch.cs

--- lc-hax/Scripts/Patches/Dependencies/EnemyDependencyPatch.cs
+++ lc-hax/Scripts/Patches/Dependencies/EnemyDependencyPatch.cs
@@ -8,12 +8,12 @@ using Unity.Netcode;
 internal class EnemyDependencyPatch {
 
     [HarmonyPatch(nameof(EnemyAI.OnDestroy))]
-    private static void Prefix(EnemyAI __instance) {
+     static void Prefix(EnemyAI __instance) {
         _ = Helper.Enemies.Remove(__instance);
     }
 
     [HarmonyPatch(nameof(EnemyAI.Start))]
-    private static void Postfix(EnemyAI __instance) {
+     static void Postfix(EnemyAI __instance) {
         _ = Helper.Enemies.Add(__instance);
     }
 }
@@ -22,12 +22,12 @@ internal class EnemyDependencyPatch {
 internal class MaskedDependencyPatch {
 
     [HarmonyPatch(nameof(MaskedPlayerEnemy.OnDestroy))]
-    private static void Prefix(MaskedPlayerEnemy __instance) {
+     static void Prefix(MaskedPlayerEnemy __instance) {
         _ = Helper.Enemies.Remove(__instance);
     }
 
     [HarmonyPatch(nameof(MaskedPlayerEnemy.Start))]
-    private static void Postfix(MaskedPlayerEnemy __instance) {
+     static void Postfix(MaskedPlayerEnemy __instance) {
         _ = Helper.Enemies.Add(__instance);
     }
 }
@@ -36,10 +36,9 @@ internal class MaskedDependencyPatch {
 internal class RoundManagerDependencyPatch {
 
     [HarmonyPatch(nameof(RoundManager.SpawnEnemyGameObject))]
-    private static void Postfix(NetworkObjectReference __result) {
+     static void Postfix(NetworkObjectReference __result) {
         NetworkObject networkObject;
         if (__result.TryGet(out networkObject)) {
-            // Once you have the NetworkObject, get the EnemyAI component
             if (!networkObject.TryGetComponent(out EnemyAI AI)) return;
             if (AI != null) {
                 _ = Helper.Enemies.Add(AI);
