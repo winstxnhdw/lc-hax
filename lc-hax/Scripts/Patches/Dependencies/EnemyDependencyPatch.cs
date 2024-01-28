02d780c96feeef684c2381fb2c8c6a6c2580cafb
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-01-28 03:53:42 +0100

    Remove unneeded hookings, this is the definitive listener

--- lc-hax/Scripts/Patches/Dependencies/EnemyDependencyPatch.cs
+++ lc-hax/Scripts/Patches/Dependencies/EnemyDependencyPatch.cs
@@ -11,25 +11,6 @@ internal class EnemyDependencyPatch {
     static void Prefix(EnemyAI __instance) {
         _ = Helper.Enemies.Remove(__instance);
     }
-
-    [HarmonyPatch(nameof(EnemyAI.Start))]
-    static void Postfix(EnemyAI __instance) {
-        _ = Helper.Enemies.Add(__instance);
-    }
-}
-
-[HarmonyPatch(typeof(MaskedPlayerEnemy))]
-internal class MaskedDependencyPatch {
-
-    [HarmonyPatch(nameof(MaskedPlayerEnemy.OnDestroy))]
-    static void Prefix(MaskedPlayerEnemy __instance) {
-        _ = Helper.Enemies.Remove(__instance);
-    }
-
-    [HarmonyPatch(nameof(MaskedPlayerEnemy.Start))]
-    static void Postfix(MaskedPlayerEnemy __instance) {
-        _ = Helper.Enemies.Add(__instance);
-    }
 }
 
 [HarmonyPatch(typeof(RoundManager))]
