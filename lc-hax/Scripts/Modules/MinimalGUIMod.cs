be07da1add89bfd9cb6dc98581da1ae7916fb48e
D1GQ <83262852+D1GQ@users.noreply.github.com>
2024-02-01 17:53:36 -0600

    Small gui overhaul

--- lc-hax/Scripts/Modules/MinimalGUIMod.cs
+++ lc-hax/Scripts/Modules/MinimalGUIMod.cs
@@ -3,37 +3,58 @@ using Hax;
 
 public class MinimalGUIMod : MonoBehaviour {
     bool InGame { get; set; } = false;
+    bool ShowLobbyInfo { get; set; } = true;
 
     void OnEnable() {
         GameListener.onGameStart += this.ToggleInGame;
         GameListener.onGameEnd += this.ToggleNotInGame;
+        InputListener.onF9Press += LobbyInfo;
     }
 
     void OnDisable() {
         GameListener.onGameStart -= this.ToggleInGame;
         GameListener.onGameEnd -= this.ToggleNotInGame;
+        InputListener.onF9Press -= LobbyInfo;
     }
 
     void OnGUI() {
         GUIStyle labelStyle = GUI.skin.label;
-        string lobbyIdText = $"Lobby Id: {Setting.ConnectedLobbyId}";
-        string antiKickText = $"Anti-Kick: {(Setting.EnableAntiKick ? "On" : "Off")}";
+        string lobbyIdText = "";
+
+        if (ShowLobbyInfo) {
+            lobbyIdText = $"Lobby ID: <color=#0EE600>{(Setting.ConnectedLobbyId)}</color><color=#999999>" +
+                    $"\nShift F4: Disconnect" +
+                    $"\nShift F3: Disconnect + ReJoin" +
+                    $"\nF9: Toggle Lobby Info</color>";
+        }
+        if (ShowLobbyInfo && !this.InGame) {
+            lobbyIdText = $"Lobby ID: <color=#0EE600>{(Setting.ConnectedLobbyId)}</color><color=#999999>" +
+                $"\nShift F3: Join ID" +
+                $"\nShift C: Copy ID" +
+                $"\nShift V: Paste Clipboard To ID" +
+                $"\nF9: Toggle Lobby Info</color>";
+        }
+        string antiKickText = $"Anti-Kick: {(Setting.EnableAntiKick ? "<color=#0EE600>On</color>" : "<color=#E60000>Off</color>")}";
 
         Vector2 lobbyIdSize = labelStyle.CalcSize(new GUIContent(lobbyIdText));
-        float lobbyIdXPosition = Screen.width - lobbyIdSize.x - 10;
+        float lobbyIdXPosition = 10f;
         float lobbyIdYPosition = 0;
-        Rect lobbyIdRect = new(lobbyIdXPosition, lobbyIdYPosition, lobbyIdSize.x, lobbyIdSize.y);
+        Rect lobbyIdRect = new Rect(lobbyIdXPosition, lobbyIdYPosition, lobbyIdSize.x, lobbyIdSize.y);
 
         Vector2 antiKickSize = labelStyle.CalcSize(new GUIContent(antiKickText));
         float antiKickXPosition = Screen.width - antiKickSize.x - 10;
-        float antiKickYPosition = lobbyIdYPosition + lobbyIdSize.y;
-        Rect antiKickRect = new(antiKickXPosition, antiKickYPosition, antiKickSize.x, antiKickSize.y);
+        float antiKickYPosition = 0;
+        Rect antiKickRect = new Rect(antiKickXPosition, antiKickYPosition, antiKickSize.x, antiKickSize.y);
 
         GUI.Label(lobbyIdRect, lobbyIdText);
 
         if (!this.InGame) {
             GUI.Label(antiKickRect, antiKickText);
         }
+    }
+
+    void LobbyInfo() {
+        ShowLobbyInfo = !ShowLobbyInfo;
     }
 
     void ToggleInGame() => this.InGame = true;
