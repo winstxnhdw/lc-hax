a10a1641e95dcfc60e6781f5dfd29f989f0d1ee2
D1GQ <83262852+D1GQ@users.noreply.github.com>
2024-02-01 17:52:18 -0600

    Added paste restrictions while in lobby

--- lc-hax/Scripts/Modules/LobbyID.cs
+++ lc-hax/Scripts/Modules/LobbyID.cs
@@ -4,31 +4,32 @@ using Hax;
 
 public class LobbyID : MonoBehaviour {
     bool IsShiftHeld { get; set; } = false;
+        bool InGame { get; set; } = false;
 
     void OnEnable() {
-        InputListener.onShiftButtonHold += this.HoldShift;
+        InputListener.onShiftButtonHold += HoldShift;
         InputListener.onCPress += this.CopyToClipboard;
         InputListener.onVPress += this.PasteFromClipboard;
     }
 
     void OnDisable() {
-        InputListener.onShiftButtonHold -= this.HoldShift;
+        InputListener.onShiftButtonHold -= HoldShift;
         InputListener.onCPress -= this.CopyToClipboard;
         InputListener.onVPress -= this.PasteFromClipboard;
     }
 
-    void HoldShift(bool isHeld) => this.IsShiftHeld = isHeld;
+    void HoldShift(bool isHeld) => IsShiftHeld = isHeld;
 
     void CopyToClipboard() {
-        if (!this.IsShiftHeld || Setting.ConnectedLobbyId == null) return;
+        if (!IsShiftHeld) return;
         GUIUtility.systemCopyBuffer = Setting.ConnectedLobbyId.ToString();
     }
 
     void PasteFromClipboard() {
-        if (!this.IsShiftHeld) return;
+        if (!IsShiftHeld || Helper.LocalPlayer is not null) return;
         string clipboardText = GUIUtility.systemCopyBuffer;
         if (ulong.TryParse(clipboardText, out ulong lobbyIdValue)) {
-            SteamId lobbyId = new() { Value = lobbyIdValue };
+            SteamId lobbyId = new SteamId { Value = lobbyIdValue };
             Setting.ConnectedLobbyId = lobbyId;
         }
     }
