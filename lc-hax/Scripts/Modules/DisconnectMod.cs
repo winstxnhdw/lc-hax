0c0f5604bebc7ff1429a7778abb6b1492b33d7ae
D1GQ <83262852+D1GQ@users.noreply.github.com>
2024-02-01 16:41:44 -0600

    Added Disconnect&Rejoin/RejoinLobbyID with Shift F3

--- lc-hax/Scripts/Modules/DisconnectMod.cs
+++ lc-hax/Scripts/Modules/DisconnectMod.cs
@@ -1,17 +1,22 @@
 using Hax;
 using UnityEngine;
+using Steamworks;
+using System.Collections;
 
 public sealed class DisconnectMod : MonoBehaviour {
     bool IsShiftHeld { get; set; } = false;
+    bool HasAnnouncedGameJoin { get; set; } = false;
 
     void OnEnable() {
         InputListener.onShiftButtonHold += this.HoldShift;
         InputListener.onF4Press += this.TryToDisconnect;
+        InputListener.onF3Press += this.Rejoin;
     }
 
     void OnDisable() {
         InputListener.onShiftButtonHold -= this.HoldShift;
         InputListener.onF4Press -= this.TryToDisconnect;
+        InputListener.onF3Press -= this.Rejoin;
     }
 
     void HoldShift(bool isHeld) => this.IsShiftHeld = isHeld;
@@ -21,5 +26,20 @@ public sealed class DisconnectMod : MonoBehaviour {
 
         GameNetworkManager.Instance.Disconnect();
         Setting.DisconnectedVoluntarily = true;
+    }
+
+    void Rejoin() {
+        if (!this.IsShiftHeld) return;
+        GameNetworkManager.Instance.Disconnect();
+        Setting.DisconnectedVoluntarily = true;
+        _ = this.StartCoroutine(this.JoinLastGame());
+    }
+
+    IEnumerator JoinLastGame() {
+        yield return new WaitForSeconds(0.2f);
+        this.HasAnnouncedGameJoin = false;
+        if (Setting.ConnectedLobbyId is SteamId lobbyId) {
+            GameNetworkManager.Instance.StartClient(lobbyId);
+        }
     }
 }
