cfb70a49bd1ec40688d52bf73b7717b965b6823c
winstxnhdw <winstxnhdw@gmail.com>
2024-02-02 22:05:27 +0800

    feat/docs: make `su` implicit

--- lc-hax/Scripts/Commands/Superuser/SuperuserCommand.cs
+++ lc-hax/Scripts/Commands/Superuser/SuperuserCommand.cs
@@ -1,11 +1,11 @@
 using Hax;
 
-public class SuperuserCommand(ICommand command) : ICommand {
+public class PrivilegedCommand(ICommand command) : ICommand {
     ICommand Command { get; } = command;
 
     public void Execute(StringArray args) {
-        if (!Setting.IsSuperuser) {
-            Chat.Print("You must be a superuser to use this command!");
+        if (Helper.LocalPlayer?.IsHost is false) {
+            Chat.Print("You must be the host to use this command!");
             return;
         }
 
