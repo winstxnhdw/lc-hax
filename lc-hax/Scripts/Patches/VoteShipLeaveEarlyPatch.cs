200d97198ba9e56cc6f0c593e4d3a01cbf35ebc2
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-03 12:47:29 +0100

    Fix

--- lc-hax/Scripts/Patches/VoteShipLeaveEarlyPatch.cs
+++ lc-hax/Scripts/Patches/VoteShipLeaveEarlyPatch.cs
@@ -8,6 +8,7 @@ class VoteShipLeaveEarlyPatch {
     [HarmonyPatch(typeof(HUDManager), "Update")]
     [HarmonyPostfix]
     static void BlockVoteEarlyHud(ref float ___holdButtonToEndGameEarlyHoldTime, ref UnityEngine.UI.Image ___holdButtonToEndGameEarlyMeter) {
+        if (PossessionMod.Instance == null) return;
         if (PossessionMod.Instance.IsPossessed) {
             ___holdButtonToEndGameEarlyHoldTime = 0.0f;
             ___holdButtonToEndGameEarlyMeter?.gameObject.SetActive(false);
@@ -17,6 +18,7 @@ class VoteShipLeaveEarlyPatch {
     [HarmonyPatch(typeof(TimeOfDay), nameof(TimeOfDay.VoteShipToLeaveEarly))]
     [HarmonyPostfix]
     static bool BlockVoteEarlyRPC() {
+        if (PossessionMod.Instance == null) return true;
         return !PossessionMod.Instance.IsPossessed;
     }
 
