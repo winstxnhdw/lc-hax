e8a27f7a3263126e11e839f03c4842e2b9754126
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-03 12:23:17 +0100

    block scan if we are possessing a enemy

---
+++ lc-hax/Scripts/Patches/PlayerScanPatch.cs
@@ -0,0 +1,12 @@
+using HarmonyLib;
+
+[HarmonyPatch(typeof(HUDManager), "CanPlayerScan")]
+class PlayerScanPatch {
+    static bool Prefix(ref bool __result) {
+        if (PossessionMod.Instance.IsPossessed) {
+            __result = false;
+            return false;
+        }
+        return true;
+    }
+}
