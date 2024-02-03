eead016e427582721413f7bda71ba0460af25ed4
winstxnhdw <winstxnhdw@gmail.com>
2024-02-03 18:10:44 +0800

    fix: SFX will not be played due to incorrect early returns

--- lc-hax/Scripts/Helpers/Grabbables.cs
+++ lc-hax/Scripts/Helpers/Grabbables.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using GameNetcodeStuff;
 using UnityEngine;
 
 namespace Hax;
@@ -6,47 +7,56 @@ namespace Hax;
 public static partial class Helper {
     public static HashSet<GrabbableObject> Grabbables { get; } = [];
 
+    static void DropSFX(this GrabbableObject item) {
+        if (!item.gameObject.TryGetComponent(out AudioSource audio)) return;
+
+        audio.PlayOneShot(item.itemProperties.dropSFX);
+
+        Helper.RoundManager?.PlayAudibleNoise(
+            noisePosition: item.transform.position,
+            noiseRange: 8.0f,
+            noiseLoudness: 0.5f,
+            timesPlayedInSameSpot: 0,
+            noiseIsInsideClosedShip: item.isInElevator && Helper.StartOfRound?.hangarDoorsClosed is true,
+            noiseID: 941
+        );
+    }
+
     public static void InteractWithProp(this GrabbableObject item, bool playSFX = false) {
-        if (item == null || Helper.LocalPlayer == null) return;
-        if (!item.IsOwner) {
-            item.ChangeOwnershipOfProp(Helper.LocalPlayer.actualClientId);
+        if (Helper.LocalPlayer is PlayerControllerB localPlayer && !item.IsOwner) {
+            item.ChangeOwnershipOfProp(localPlayer.actualClientId);
         }
+
+        if (playSFX) {
+            item.DropSFX();
+        }
+
         if (item.TryGetComponent(out AnimatedItem animated)) {
             animated.EquipItem();
             return;
         }
+
         if (item.TryGetComponent(out NoisemakerProp noiseprop)) {
             noiseprop.ItemActivate(true, true);
             return;
         }
+
         if (item.TryGetComponent(out BoomboxItem BoomBox)) {
             BoomBox.ItemActivate(true, true);
             return;
         }
+
         if (item.TryGetComponent(out WhoopieCushionItem fartcushion)) {
             fartcushion.Fart();
             return;
         }
 
-        if (playSFX) item.DropSFX();
         item.UseItemOnClient(true);
     }
 
-    public static void DropSFX(this GrabbableObject item) {
-        if (item == null) return;
-        if (item.itemProperties.dropSFX != null) {
-            item.gameObject.GetComponent<AudioSource>().PlayOneShot(item.itemProperties.dropSFX);
-            if (item.IsOwner) {
-                RoundManager.Instance.PlayAudibleNoise(item.transform.position, 8f, 0.5f, 0, item.isInElevator && StartOfRound.Instance.hangarDoorsClosed, 941);
-            }
-        }
-    }
-
     public static void ShootShotgun(this ShotgunItem item, Transform origin) {
-        if (item == null || origin == null) return;
         item.gunShootAudio.volume = 0.15f;
         item.shotgunRayPoint = origin;
         item.ShootGunAndSync(false);
-
     }
 }
