4946f02e80a9b93eb71dd3c640ff2d2c7e6c6b41
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-02-03 16:45:54 +0800

    chore: tell the user that the lobby ID has been copied

--- lc-hax/Scripts/Commands/LobbyCommand.cs
+++ lc-hax/Scripts/Commands/LobbyCommand.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [Command("/lobby")]
 public class LobbyCommand : ICommand {
     public void Execute(StringArray _) {
-        Chat.Print($"Lobby ID: {Setting.ConnectedLobbyId}");
+        Chat.Print($"The lobby ID {Setting.ConnectedLobbyId} has been copied!");
         GUIUtility.systemCopyBuffer = Setting.ConnectedLobbyId.ToString();
     }
 }
