af7a8ceeb6660c76230ae8411b132d2aa77c9efb
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-06 15:56:46 +0100

    Discard changes to lc-hax/Scripts/Modules/Possession/Controllers/EyelessDogController.cs

--- lc-hax/Scripts/Modules/Possession/Controllers/EyelessDogController.cs
+++ lc-hax/Scripts/Modules/Possession/Controllers/EyelessDogController.cs
@@ -1,6 +1,9 @@
+using System;
+
 internal class EyelessDogController : IEnemyController<MouthDogAI> {
     internal void UseSecondarySkill(MouthDogAI enemyInstance) {
         if (enemyInstance.currentBehaviourStateIndex is 2) return;
+        Console.WriteLine("Switching to lunge");
         enemyInstance.SwitchToBehaviourServerRpc(2);
     }
 
