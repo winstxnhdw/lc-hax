3440948caa4154c85862cc91ee6e7be48a085674
winstxnhdw <winstxnhdw@gmail.com>
2024-02-06 15:45:56 +0800

    refactor: use interface instead of extension methods

--- lc-hax/Scripts/Modules/Enemy Controls/Enemies/SpringManEnemyController.cs
+++
@@ -1,4 +0,0 @@
-public static class SpringManController {
-    public static bool HasStopped(this SpringManAI instance) => instance != null && instance.Reflect().GetInternalField<bool>("hasStopped");
-    public static bool StopMoving(this SpringManAI instance) => instance != null && instance.Reflect().GetInternalField<bool>("stoppingMovement");
-}
---
+++ lc-hax/Scripts/Modules/Possession/Controllers/ForestGiantController.cs
@@ -0,0 +1,16 @@
+using Hax;
+
+enum GiantState {
+    DEFAULT = 0,
+    CHASE = 1
+}
+
+public class ForestGiantController : IEnemyController<ForestGiantAI> {
+    public void UseSecondarySkill(ForestGiantAI enemyInstance) => enemyInstance.SetBehaviourState(GiantState.CHASE);
+
+    public void ReleaseSecondarySkill(ForestGiantAI enemyInstance) => enemyInstance.SetBehaviourState(GiantState.DEFAULT);
+
+    public bool IsAbleToMove(ForestGiantAI enemyInstance) => enemyInstance.Reflect().GetInternalField<bool>("inEatingPlayerAnimation");
+
+    public string GetSecondarySkillName(ForestGiantAI _) => "(HOLD) Chase";
+}
