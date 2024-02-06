9a0d0e95eb6c147fb60f05e0d9c03de3e2f95b7d
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-06 12:38:58 +0100

    Sync branch here

--- lc-hax/Scripts/Core/InputListener.cs
+++ lc-hax/Scripts/Core/InputListener.cs
@@ -2,62 +2,58 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputListener : MonoBehaviour {
-    public static event Action<bool>? onShiftButtonHold;
-    public static event Action<bool>? onFButtonHold;
-    public static event Action<bool>? onEButtonHold;
-    public static event Action<bool>? onRButtonHold;
-    public static event Action? onMiddleButtonPress;
-    public static event Action? onLeftButtonPress;
-    public static event Action? onLeftButtonRelease;
-    public static event Action? onRightButtonPress;
-    public static event Action? onRightButtonRelease;
-    public static event Action? onLeftArrowKeyPress;
-    public static event Action? onRightArrowKeyPress;
-    public static event Action? onPausePress;
-    public static event Action? onEqualsPress;
-    public static event Action? onLeftBracketPress;
-    public static event Action? onRightBracketPress;
-    public static event Action? onBackslashPress;
-    public static event Action? onZPress;
-    public static event Action? onXPress;
-    public static event Action? onNPress;
-    public static event Action? onUpArrowPress;
-    public static event Action? onDownArrowPress;
-    public static event Action? onF4Press;
-    public static event Action? onF5Press;
-    public static event Action<bool>? onLeftButtonHold;
-    public static event Action<bool>? onRightButtonHold;
+internal class InputListener : MonoBehaviour {
+    internal static event Action<bool>? OnShiftButtonHold;
+    internal static event Action<bool>? OnFButtonHold;
+    internal static event Action<bool>? OnEButtonHold;
+    internal static event Action<bool>? OnRButtonHold;
+    internal static event Action? OnMiddleButtonPress;
+    internal static event Action? OnLeftButtonPress;
+    internal static event Action? OnRightButtonPress;
+    internal static event Action? OnRightButtonRelease;
+    internal static event Action? OnLeftArrowKeyPress;
+    internal static event Action? OnRightArrowKeyPress;
+    internal static event Action? OnPausePress;
+    internal static event Action? OnEqualsPress;
+    internal static event Action? OnLeftBracketPress;
+    internal static event Action? OnRightBracketPress;
+    internal static event Action? OnBackslashPress;
+    internal static event Action? OnZPress;
+    internal static event Action? OnXPress;
+    internal static event Action? OnNPress;
+    internal static event Action? OnUpArrowPress;
+    internal static event Action? OnDownArrowPress;
+    internal static event Action? OnF4Press;
+    internal static event Action? OnF5Press;
+    internal static event Action<bool>? OnRightButtonHold;
 
     (Func<bool>, Action)[] InputActions { get; } = [
-        (() => Mouse.current.middleButton.wasPressedThisFrame, () => InputListener.onMiddleButtonPress?.Invoke()),
-        (() => Mouse.current.leftButton.wasPressedThisFrame, () => InputListener.onLeftButtonPress?.Invoke()),
-        (() => Mouse.current.leftButton.wasReleasedThisFrame, () => InputListener.onLeftButtonRelease?.Invoke()),
-        (() => Mouse.current.rightButton.wasPressedThisFrame, () => InputListener.onRightButtonPress?.Invoke()),
-        (() => Mouse.current.rightButton.wasReleasedThisFrame, () => InputListener.onRightButtonRelease?.Invoke()),
-        (() => Keyboard.current[Key.Pause].wasPressedThisFrame, () => InputListener.onPausePress?.Invoke()),
-        (() => Keyboard.current[Key.Equals].wasPressedThisFrame, () => InputListener.onEqualsPress?.Invoke()),
-        (() => Keyboard.current[Key.LeftArrow].wasPressedThisFrame, () => InputListener.onLeftArrowKeyPress?.Invoke()),
-        (() => Keyboard.current[Key.RightArrow].wasPressedThisFrame, () => InputListener.onRightArrowKeyPress?.Invoke()),
-        (() => Keyboard.current[Key.LeftBracket].wasPressedThisFrame, () => InputListener.onLeftBracketPress?.Invoke()),
-        (() => Keyboard.current[Key.RightBracket].wasPressedThisFrame, () => InputListener.onRightBracketPress?.Invoke()),
-        (() => Keyboard.current[Key.Backslash].wasPressedThisFrame, () => InputListener.onBackslashPress?.Invoke()),
-        (() => Keyboard.current[Key.Z].wasPressedThisFrame, () => InputListener.onZPress?.Invoke()),
-        (() => Keyboard.current[Key.X].wasPressedThisFrame, () => InputListener.onXPress?.Invoke()),
-        (() => Keyboard.current[Key.N].wasPressedThisFrame, () => InputListener.onNPress?.Invoke()),
-        (() => Keyboard.current[Key.UpArrow].wasPressedThisFrame, () => InputListener.onUpArrowPress?.Invoke()),
-        (() => Keyboard.current[Key.DownArrow].wasPressedThisFrame, () => InputListener.onDownArrowPress?.Invoke()),
-        (() => Keyboard.current[Key.F4].wasPressedThisFrame, () => InputListener.onF4Press?.Invoke()),
-        (() => Keyboard.current[Key.F5].wasPressedThisFrame, () => InputListener.onF5Press?.Invoke()),
+        (() => Mouse.current.middleButton.wasPressedThisFrame, () => InputListener.OnMiddleButtonPress?.Invoke()),
+        (() => Mouse.current.leftButton.wasPressedThisFrame, () => InputListener.OnLeftButtonPress?.Invoke()),
+        (() => Mouse.current.rightButton.wasPressedThisFrame, () => InputListener.OnRightButtonPress?.Invoke()),
+        (() => Mouse.current.rightButton.wasReleasedThisFrame, () => InputListener.OnRightButtonRelease?.Invoke()),
+        (() => Keyboard.current[Key.Pause].wasPressedThisFrame, () => InputListener.OnPausePress?.Invoke()),
+        (() => Keyboard.current[Key.Equals].wasPressedThisFrame, () => InputListener.OnEqualsPress?.Invoke()),
+        (() => Keyboard.current[Key.LeftArrow].wasPressedThisFrame, () => InputListener.OnLeftArrowKeyPress?.Invoke()),
+        (() => Keyboard.current[Key.RightArrow].wasPressedThisFrame, () => InputListener.OnRightArrowKeyPress?.Invoke()),
+        (() => Keyboard.current[Key.LeftBracket].wasPressedThisFrame, () => InputListener.OnLeftBracketPress?.Invoke()),
+        (() => Keyboard.current[Key.RightBracket].wasPressedThisFrame, () => InputListener.OnRightBracketPress?.Invoke()),
+        (() => Keyboard.current[Key.Backslash].wasPressedThisFrame, () => InputListener.OnBackslashPress?.Invoke()),
+        (() => Keyboard.current[Key.Z].wasPressedThisFrame, () => InputListener.OnZPress?.Invoke()),
+        (() => Keyboard.current[Key.X].wasPressedThisFrame, () => InputListener.OnXPress?.Invoke()),
+        (() => Keyboard.current[Key.N].wasPressedThisFrame, () => InputListener.OnNPress?.Invoke()),
+        (() => Keyboard.current[Key.UpArrow].wasPressedThisFrame, () => InputListener.OnUpArrowPress?.Invoke()),
+        (() => Keyboard.current[Key.DownArrow].wasPressedThisFrame, () => InputListener.OnDownArrowPress?.Invoke()),
+        (() => Keyboard.current[Key.F4].wasPressedThisFrame, () => InputListener.OnF4Press?.Invoke()),
+        (() => Keyboard.current[Key.F5].wasPressedThisFrame, () => InputListener.OnF5Press?.Invoke()),
     ];
 
     void Update() {
-        InputListener.onShiftButtonHold?.Invoke(Keyboard.current[Key.LeftShift].isPressed);
-        InputListener.onFButtonHold?.Invoke(Keyboard.current[Key.F].isPressed);
-        InputListener.onRButtonHold?.Invoke(Keyboard.current[Key.R].isPressed);
-        InputListener.onEButtonHold?.Invoke(Keyboard.current[Key.E].isPressed);
-        InputListener.onLeftButtonHold?.Invoke(Mouse.current.leftButton.isPressed);
-        InputListener.onRightButtonHold?.Invoke(Mouse.current.rightButton.isPressed);
+        InputListener.OnShiftButtonHold?.Invoke(Keyboard.current[Key.LeftShift].isPressed);
+        InputListener.OnFButtonHold?.Invoke(Keyboard.current[Key.F].isPressed);
+        InputListener.OnRButtonHold?.Invoke(Keyboard.current[Key.R].isPressed);
+        InputListener.OnEButtonHold?.Invoke(Keyboard.current[Key.E].isPressed);
+        InputListener.OnRightButtonHold?.Invoke(Mouse.current.rightButton.isPressed);
 
         foreach ((Func<bool> keyPressed, Action eventAction) in this.InputActions) {
             if (!keyPressed()) continue;
