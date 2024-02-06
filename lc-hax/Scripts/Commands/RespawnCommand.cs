3f2eaa5f626f8b197b84ad3d5a7e6b512e85ba1c
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-06 15:21:08 +0100

    Rewrite Respawn()

--- lc-hax/Scripts/Commands/RespawnCommand.cs
+++ lc-hax/Scripts/Commands/RespawnCommand.cs
@@ -1,89 +1,110 @@
-using Hax;
 using GameNetcodeStuff;
+using Hax;
+using UnityEngine;
 
 [Command("/respawn")]
 public class RespawnCommand : ICommand {
-    public void RespawnLocalPlayer(PlayerControllerB localPlayer) {
+
+    public void RespawnLocalPlayer() {
+        if (Helper.LocalPlayer is not PlayerControllerB localPlayer) return;
         if (Helper.StartOfRound is not StartOfRound startOfRound) return;
         if (Helper.HUDManager is not HUDManager hudManager) return;
         if (Helper.SoundManager is not SoundManager soundManager) return;
 
         startOfRound.allPlayersDead = false;
-        localPlayer.ResetPlayerBloodObjects(localPlayer.isPlayerDead);
-        if (localPlayer.isPlayerDead || localPlayer.isPlayerControlled) {
-            localPlayer.isClimbingLadder = false;
-            localPlayer.ResetZAndXRotation();
-            localPlayer.thisController.enabled = true;
-            localPlayer.health = 100;
-            localPlayer.disableLookInput = false;
-            if (localPlayer.isPlayerDead) {
-                localPlayer.isPlayerDead = false;
-                localPlayer.isPlayerControlled = true;
-                localPlayer.isInElevator = true;
-                localPlayer.isInHangarShipRoom = true;
-                localPlayer.isInsideFactory = false;
-                localPlayer.wasInElevatorLastFrame = false;
-                startOfRound.SetPlayerObjectExtrapolate(false);
-                localPlayer.TeleportPlayer(startOfRound.playerSpawnPositions[0].position, false, 0f, false, true);
-                localPlayer.setPositionOfDeadPlayer = false;
-                localPlayer.DisablePlayerModel(startOfRound.allPlayerObjects[localPlayer.playerClientId], true, true);
-                localPlayer.helmetLight.enabled = false;
-                localPlayer.Crouch(false);
-                localPlayer.criticallyInjured = false;
-                localPlayer.playerBodyAnimator?.SetBool("Limp", false);
-                localPlayer.bleedingHeavily = false;
-                localPlayer.activatingItem = false;
-                localPlayer.twoHanded = false;
-                localPlayer.inSpecialInteractAnimation = false;
-                localPlayer.disableSyncInAnimation = false;
-                localPlayer.inAnimationWithEnemy = null;
-                localPlayer.holdingWalkieTalkie = false;
-                localPlayer.speakingToWalkieTalkie = false;
-                localPlayer.isSinking = false;
-                localPlayer.isUnderwater = false;
-                localPlayer.sinkingValue = 0f;
-                localPlayer.statusEffectAudio.Stop();
-                localPlayer.DisableJetpackControlsLocally();
-                localPlayer.health = 100;
-                localPlayer.mapRadarDotAnimator.SetBool("dead", false);
-                if (localPlayer.IsOwner) {
-                    hudManager.gasHelmetAnimator.SetBool("gasEmitting", false);
-                    localPlayer.hasBegunSpectating = false;
-                    hudManager.RemoveSpectateUI();
-                    hudManager.gameOverAnimator.SetTrigger("revive");
-                    localPlayer.hinderedMultiplier = 1f;
-                    localPlayer.isMovementHindered = 0;
-                    localPlayer.sourcesCausingSinking = 0;
-                    localPlayer.reverbPreset = startOfRound.shipReverb;
-                }
+        localPlayer.ResetPlayerBloodObjects();
+        localPlayer.isClimbingLadder = false;
+        localPlayer.ResetZAndXRotation();
+        localPlayer.thisController.enabled = true;
+        localPlayer.health = 100;
+        localPlayer.disableLookInput = false;
+        localPlayer.isPlayerDead = false;
+        localPlayer.isPlayerControlled = true;
+        localPlayer.isInElevator = true;
+        localPlayer.isInHangarShipRoom = true;
+        localPlayer.isInsideFactory = false;
+        localPlayer.wasInElevatorLastFrame = false;
+        startOfRound.SetPlayerObjectExtrapolate(false);
+        localPlayer.TeleportPlayer(startOfRound.playerSpawnPositions[0].position, false, 0f, false, true);
+        localPlayer.setPositionOfDeadPlayer = false;
+        localPlayer.DisablePlayerModel(startOfRound.allPlayerObjects[localPlayer.playerClientId], true, true);
+
+        if (localPlayer.TryGetComponent(out Light helmetLight)) {
+            helmetLight.enabled = false;
+        }
+
+        localPlayer.Crouch(false);
+        localPlayer.criticallyInjured = false;
+
+        if (localPlayer.playerBodyAnimator != null) {
+            localPlayer.playerBodyAnimator.SetBool("Limp", false);
+        }
+
+        localPlayer.bleedingHeavily = false;
+        localPlayer.activatingItem = false;
+        localPlayer.twoHanded = false;
+        localPlayer.inSpecialInteractAnimation = false;
+        localPlayer.disableSyncInAnimation = false;
+        localPlayer.inAnimationWithEnemy = null;
+        localPlayer.holdingWalkieTalkie = false;
+        localPlayer.speakingToWalkieTalkie = false;
+        localPlayer.isSinking = false;
+        localPlayer.isUnderwater = false;
+        localPlayer.sinkingValue = 0f;
+
+        if (localPlayer.TryGetComponent(out AudioSource statusEffectAudio)) {
+            statusEffectAudio.Stop();
+        }
+
+        localPlayer.DisableJetpackControlsLocally();
+        localPlayer.health = 100;
+
+        if (localPlayer.mapRadarDotAnimator != null) {
+            localPlayer.mapRadarDotAnimator.SetBool("dead", false);
+        }
+
+        if (localPlayer.IsOwner) {
+            if (hudManager.TryGetComponent(out Animator gasHelmetAnimator)) {
+                gasHelmetAnimator.SetBool("gasEmitting", false);
             }
-            soundManager.earsRingingTimer = 0f;
-            localPlayer.voiceMuffledByEnemy = false;
-            soundManager.playerVoicePitchTargets[localPlayer.playerClientId] = 1f;
-            soundManager.SetPlayerPitch(1f, (int)localPlayer.playerClientId);
 
-            if (localPlayer.currentVoiceChatIngameSettings == null) {
-                startOfRound.RefreshPlayerVoicePlaybackObjects();
+            localPlayer.hasBegunSpectating = false;
+            hudManager.RemoveSpectateUI();
+            if (hudManager.TryGetComponent(out Animator gameOverAnimator)) {
+                gameOverAnimator.SetTrigger("revive");
             }
 
-            if (localPlayer.currentVoiceChatIngameSettings != null) {
-                if (localPlayer.currentVoiceChatIngameSettings.voiceAudio == null) {
-                    localPlayer.currentVoiceChatIngameSettings.InitializeComponents();
-                }
+            localPlayer.hinderedMultiplier = 1f;
+            localPlayer.isMovementHindered = 0;
+            localPlayer.sourcesCausingSinking = 0;
+            localPlayer.reverbPreset = startOfRound.shipReverb;
+        }
 
-                if (localPlayer.currentVoiceChatIngameSettings.voiceAudio == null) {
-                    return;
-                }
+        soundManager.earsRingingTimer = 0f;
+        localPlayer.voiceMuffledByEnemy = false;
+        soundManager.playerVoicePitchTargets[localPlayer.playerClientId] = 1f;
+        soundManager.SetPlayerPitch(1f, (int)localPlayer.playerClientId);
 
-                localPlayer.currentVoiceChatIngameSettings.voiceAudio.GetComponent<OccludeAudio>().overridingLowPass = false;
+        if (localPlayer.currentVoiceChatIngameSettings == null) {
+            startOfRound.RefreshPlayerVoicePlaybackObjects();
+        }
+
+        if (localPlayer.currentVoiceChatIngameSettings != null) {
+            if (localPlayer.currentVoiceChatIngameSettings.voiceAudio == null) {
+                localPlayer.currentVoiceChatIngameSettings.InitializeComponents();
+            }
+
+            if (localPlayer.currentVoiceChatIngameSettings.voiceAudio != null) {
+                if (localPlayer.currentVoiceChatIngameSettings.voiceAudio
+                    .TryGetComponent(out OccludeAudio occludeAudio)) {
+                    occludeAudio.overridingLowPass = false;
+                }
             }
         }
     }
 
     public void Execute(StringArray _) {
-        if (Helper.LocalPlayer is not PlayerControllerB localPlayer) return;
-
-        this.RespawnLocalPlayer(localPlayer);
+        this.RespawnLocalPlayer();
         Chat.Print("No one can see you in this state!");
     }
 }
--- lc-hax/Scripts/Helpers/Managers.cs
+++ lc-hax/Scripts/Helpers/Managers.cs
@@ -6,6 +6,7 @@ internal static partial class Helper {
     internal static RoundManager? RoundManager => RoundManager.Instance.Unfake();
 
     internal static StartOfRound? StartOfRound => StartOfRound.Instance.Unfake();
+    internal static SoundManager? SoundManager => SoundManager.Instance.Unfake();
 
     internal static Terminal? Terminal => Helper.HUDManager?.Reflect().GetInternalField<Terminal>("terminalScript").Unfake();
 }
