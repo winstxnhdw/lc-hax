d3ac318c14484701a8f4fe9b79ea2ed7378bb97a
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-07 20:20:01 +0100

    Update lc-hax/Scripts/Commands/Debug/LevelEnemiesCommand.cs
    Co-authored-by: Winston H. <56998716+winstxnhdw@users.noreply.github.com>

--- lc-hax/Scripts/Commands/Debug/LevelEnemiesCommand.cs
+++ lc-hax/Scripts/Commands/Debug/LevelEnemiesCommand.cs
@@ -7,7 +7,6 @@ public class LevelEnemiesCommand : ICommand {
     public void Execute(StringArray _) {
         if (Helper.RoundManager == null || Helper.RoundManager.currentLevel == null) return;
 
-        // Using string.Join to concatenate enemy names
         string levelEnemies = string.Join(", ", Helper.AllSpawnableEnemies.Keys);
 
         Logger.Write(levelEnemies);
