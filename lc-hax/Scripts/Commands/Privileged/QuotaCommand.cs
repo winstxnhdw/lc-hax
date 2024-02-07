e18fd84d386d4649e1fc000974d994422fbea933
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-07 14:50:22 +0100

    Fix!

--- lc-hax/Scripts/Commands/Privileged/QuotaCommand.cs
+++ lc-hax/Scripts/Commands/Privileged/QuotaCommand.cs
@@ -3,35 +3,29 @@ using Hax;
 [PrivilegedCommand("/quota")]
 public class QuotaCommand : ICommand {
 
-    void SetQuota(ulong profit, ulong fulfilled = 0) {
-        if (Helper.TimeOfDay != null) {
-            Helper.TimeOfDay.profitQuota = (int)profit;
-            Helper.TimeOfDay.quotaFulfilled = (int)fulfilled;
-            Helper.TimeOfDay.UpdateProfitQuotaCurrentTime();
-        }
+    private void SetQuota(TimeOfDay timeOfDay, ushort profit, ushort fulfilled = 0) {
+        timeOfDay.profitQuota = profit;
+        timeOfDay.quotaFulfilled = fulfilled;
+        timeOfDay.UpdateProfitQuotaCurrentTime();
     }
 
     public void Execute(StringArray args) {
-        if (Helper.TimeOfDay == null) {
-            Chat.Print("TimeOfDay helper is not available.");
-            return;
-        }
-
+        if (Helper.TimeOfDay is not TimeOfDay timeOfDay) return;
         if (args.Length < 1) {
             Chat.Print("Usage: /quota <amount> <fulfilled?>");
             return;
         }
 
-        if (!ulong.TryParse(args[0], out ulong amount)) {
+        if (!ushort.TryParse(args[0], out ushort amount)) {
             Chat.Print("Invalid amount!");
             return;
         }
 
-        ulong fulfilled = 0;
-        if (args.Length > 1 && !ulong.TryParse(args[1], out fulfilled)) {
+        ushort fulfilled = 0;
+        if (args.Length > 1 && !ushort.TryParse(args[1], out fulfilled)) {
             Chat.Print("Invalid fulfilled amount, setting to 0.");
         }
 
-        this.SetQuota(amount, fulfilled);
+        this.SetQuota(timeOfDay, amount, fulfilled);
     }
 }
--- lc-hax/Scripts/Patches/DeathNotifierPatch.cs
+++ lc-hax/Scripts/Patches/DeathNotifierPatch.cs
@@ -6,10 +6,9 @@ using Hax;
 class DeathNotifierPatch {
     static void Postfix(int playerId, int causeOfDeath) {
         if (!Setting.EnablePlayerDeathNotifications) return;
-        if (Helper.GetPlayer((ulong)playerId) is PlayerControllerB player) {
-            if (player.IsSelf()) return;
-            Helper.DisplayNotification($"{player.playerUsername} ",
-                $"{player.playerUsername} was killed by {(CauseOfDeath)causeOfDeath}", true);
-        }
+        if (Helper.GetPlayer((ulong)playerId) is not PlayerControllerB player) return;
+        if (player.IsSelf()) return;
+        Helper.DisplayNotification($"{player.playerUsername} ",
+            $"{player.playerUsername} was killed by {(CauseOfDeath)causeOfDeath}", true);
     }
 }
