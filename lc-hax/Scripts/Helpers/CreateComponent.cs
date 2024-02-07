0e59bc302057b1b32811a9c84add6986ab4d5dc6
winstxnhdw <winstxnhdw@gmail.com>
2024-02-08 05:20:12 +0800

    chore: add name to `lc-hax` game objects

--- lc-hax/Scripts/Helpers/CreateComponent.cs
+++ lc-hax/Scripts/Helpers/CreateComponent.cs
@@ -6,10 +6,12 @@ namespace Hax;
 internal static partial class Helper {
     static Dictionary<string, Queue<Component>> ComponentPools { get; } = [];
 
+    internal static T CreateComponent<T>(string name) where T : Component => new GameObject(name).AddComponent<T>();
+
     internal static T CreateComponent<T>() where T : Component => new GameObject().AddComponent<T>();
 
     internal static T CreateComponent<T>(string poolName, int poolSize = 1) where T : Component {
-        T gameObject = Helper.CreateComponent<T>();
+        T gameObject = Helper.CreateComponent<T>(poolName);
 
         if (!Helper.ComponentPools.TryGetValue(poolName, out Queue<Component> pool)) {
             pool = new Queue<Component>(poolSize);
