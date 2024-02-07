48a7f28dc8c33c14bbcd0d490e18ee3b6937ab35
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-07 14:52:49 +0100

    Update SpawnCommand.cs

--- lc-hax/Scripts/Commands/Privileged/SpawnCommand.cs
+++ lc-hax/Scripts/Commands/Privileged/SpawnCommand.cs
@@ -46,7 +46,7 @@ public class SpawnCommand : ICommand {
 
         string enemyNamePart = args[1];
         KeyValuePair<string, GameObject> enemyEntry = Helper.AllSpawnableEnemies
-            .FirstOrDefault(e => e.Key.Contains(enemyNamePart, StringComparison.OrdinalIgnoreCase));
+            .First(e => e.Key.Contains(enemyNamePart, StringComparison.OrdinalIgnoreCase));
 
         if (enemyEntry.Value == null) {
             Chat.Print($"Enemy '{enemyNamePart}' not found.");
