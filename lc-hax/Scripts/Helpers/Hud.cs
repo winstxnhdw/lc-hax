0d7ecd96db22ba4db9716eea6f97555dcb350b1b
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-07 14:32:22 +0100

    Update lc-hax/Scripts/Helpers/Hud.cs
    Co-authored-by: Winston H. <56998716+winstxnhdw@users.noreply.github.com>

--- lc-hax/Scripts/Helpers/Hud.cs
+++ lc-hax/Scripts/Helpers/Hud.cs
@@ -3,8 +3,7 @@ namespace Hax;
 internal static partial class Helper {
 
     internal static void DisplayNotification(string title, string Text, bool isWarning = false) {
-        if (Helper.HUDManager == null) return;
-        Helper.HUDManager.DisplayTip(title, Text, isWarning, false);
+        Helper.HUDManager?.DisplayTip(title, Text, isWarning, false);
     }
 
 }
