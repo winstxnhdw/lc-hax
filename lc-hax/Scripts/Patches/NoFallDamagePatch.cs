4f52a8e82ae40ddfc6c1568092e8c99fa3ef9567
winstxnhdw <winstxnhdw@gmail.com>
2024-02-10 04:06:30 +0800

    refactor: use `ResetFallGravity` instead

--- lc-hax/Scripts/Patches/NoFallDamagePatch.cs
+++
@@ -1,8 +0,0 @@
-using GameNetcodeStuff;
-using HarmonyLib;
-using Hax;
-
-[HarmonyPatch(typeof(PlayerControllerB), "DamagePlayer")]
-class NoFallDamagePatch {
-    static bool Prefix(CauseOfDeath causeOfDeath) => !Setting.DisableFallDamage || causeOfDeath != CauseOfDeath.Gravity;
-}
