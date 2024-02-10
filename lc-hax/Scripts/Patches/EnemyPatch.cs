a972fbe808e90f3bc9eb46756e379e7c0a3e9434
winstxnhdw <winstxnhdw@gmail.com>
2024-02-10 22:20:48 +0800

    chore: remove redundant enemy patch

--- lc-hax/Scripts/Patches/EnemyPatch.cs
+++ lc-hax/Scripts/Patches/EnemyPatch.cs
@@ -1,16 +1,8 @@
 #pragma warning disable IDE1006
 
-using GameNetcodeStuff;
 using HarmonyLib;
 
-[HarmonyPatch(typeof(EnemyAI))]
+[HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.SyncPositionToClients))]
 class EnemyPatch {
-    [HarmonyPatch(nameof(EnemyAI.SyncPositionToClients))]
     static void Prefix(EnemyAI __instance) => __instance.updatePositionThreshold = 0.0f;
-
-    [HarmonyPatch(nameof(EnemyAI.CancelSpecialAnimationWithPlayer))]
-    static void Postfix(EnemyAI __instance) {
-        if (__instance.inSpecialAnimationWithPlayer is not PlayerControllerB player) return;
-        player.disableLookInput = false;
-    }
 }
