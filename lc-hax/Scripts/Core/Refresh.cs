3537ecdd4c495231c6887d538a2e1c73a0d70695
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-10 17:31:05 +0100

    Update Refresh.cs

--- lc-hax/Scripts/Core/Refresh.cs
+++ lc-hax/Scripts/Core/Refresh.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using UnityEngine;
 
 internal sealed class Refresh : MonoBehaviour {
-    private void Awake() {
+    void Start() {
         if (Helper.LocalPlayer == null) return;
         if (Helper.StartOfRound?.shipHasLanded is true) {
             this.FillEnemyList();
@@ -12,12 +12,12 @@ internal sealed class Refresh : MonoBehaviour {
         Destroy(this);
     }
 
-    private void FillEnemyList() =>
+    void FillEnemyList() =>
         Helper.FindObjects<EnemyAI>().WhereIsNotNull()
             .Where(enemy => enemy.IsSpawned)
             .ForEach(enemy => { _ = Helper.Enemies.Add(enemy); });
 
-    private void FillScrapList() =>
+    void FillScrapList() =>
         Helper.FindObjects<GrabbableObject>().WhereIsNotNull()
             .Where(scrap => scrap.IsSpawned)
             .ForEach(scrap => { _ = Helper.Grabbables.Add(scrap); });
