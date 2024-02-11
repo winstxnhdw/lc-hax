af6537a00d1ca4d23b07da83b902ac020c37bdca
winstxnhdw <winstxnhdw@gmail.com>
2024-02-11 19:46:30 +0800

    feat/docs: make it possible to change prefixes in runtime

--- lc-hax/Scripts/Static/State.cs
+++ lc-hax/Scripts/Static/State.cs
@@ -3,6 +3,7 @@ using Steamworks;
 namespace Hax;
 
 internal static class State {
+    internal static char CommandPrefix { get; set; } = '/';
     internal static int ShovelHitForce { get; set; } = 1;
     internal static bool DisconnectedVoluntarily { get; set; } = false;
     internal static SteamId? ConnectedLobbyId { get; set; } = null;
