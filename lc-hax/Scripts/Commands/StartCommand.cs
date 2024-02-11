ce8d513ba85b76f65cd553acee9772de03fd09a6
winstxnhdw <winstxnhdw@gmail.com>
2024-02-12 03:48:58 +0800

    fix: setting `travellingToNewLevel` sometimes breaks `start`

--- lc-hax/Scripts/Commands/StartCommand.cs
+++ lc-hax/Scripts/Commands/StartCommand.cs
@@ -4,8 +4,10 @@ using Hax;
 internal class StartGameCommand : ICommand {
     public void Execute(StringArray _) {
         if (Helper.StartOfRound is not StartOfRound startOfRound) return;
+        if (startOfRound.travellingToNewLevel) {
+            Chat.Print("You cannot start the game while travelling to new level!");
+        }
 
-        startOfRound.travellingToNewLevel = false;
         startOfRound.StartGameServerRpc();
     }
 }
