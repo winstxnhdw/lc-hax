14a468ffdb569347894244870ff04d4454f13e55
winstxnhdw <winstxnhdw@gmail.com>
2024-02-13 22:31:11 +0800

    feat: remove requirement to hold defensive weapon for `killclick`

--- lc-hax/Scripts/Modules/KillClickMod.cs
+++ lc-hax/Scripts/Modules/KillClickMod.cs
@@ -8,14 +8,11 @@ internal sealed class KillClickMod : MonoBehaviour {
     void OnEnable() => InputListener.OnLeftButtonPress += this.Kill;
     void OnDisable() => InputListener.OnLeftButtonPress -= this.Kill;
 
-    bool IsHoldingADefensiveWeapon() =>
-        Helper.LocalPlayer?.currentlyHeldObjectServer.itemProperties.isDefensiveWeapon is not false;
-
     void Kill() {
         if (!Setting.EnableKillOnLeftClick) return;
-        if (Helper.LocalPlayer is not PlayerControllerB localPlayer) return;
         if (Helper.CurrentCamera is not Camera camera) return;
-        if (this.IsHoldingADefensiveWeapon()) return;
+        if (Helper.LocalPlayer is not PlayerControllerB localPlayer) return;
+        if (localPlayer.currentlyHeldObjectServer?.itemProperties.isDefensiveWeapon is not false) return;
 
         this.RaycastHits.SphereCastForward(camera.transform).Range().ForEach(i => {
             if (!this.RaycastHits[i].collider.TryGetComponent(out EnemyAICollisionDetect enemy)) return;
