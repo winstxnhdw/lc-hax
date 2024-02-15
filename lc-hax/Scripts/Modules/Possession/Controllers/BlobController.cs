99e7ee7a7ce1baa8fda1e4a807de0a1b25a0e554
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-15 12:57:07 +0100

    Update BlobController.cs

--- lc-hax/Scripts/Modules/Possession/Controllers/BlobController.cs
+++ lc-hax/Scripts/Modules/Possession/Controllers/BlobController.cs
@@ -1,18 +1,24 @@
+using Hax;
+using UnityEngine;
+
 internal class BlobController : IEnemyController<BlobAI> {
 
     public void SetTamedTimer(BlobAI instance, float time) => _ = instance.Reflect().SetInternalField("tamedTimer", time);
 
     public void SetAngeredTimer(BlobAI instance, float time) => _ = instance.Reflect().SetInternalField("angeredTimer", time);
 
-    public void UseSecondarySkill(BlobAI enemyInstance) => this.SetTamedTimer(enemyInstance, 10f);
+    public void UseSecondarySkill(BlobAI enemyInstance) {
+        this.SetAngeredTimer(enemyInstance, 0f);
+        this.SetTamedTimer(enemyInstance, 2f);
+    }
 
     public void ReleaseSecondarySkill(BlobAI enemyInstance) => this.SetTamedTimer(enemyInstance, 0f);
 
-    public void UsePrimarySkill(BlobAI enemyInstance) => this.SetAngeredTimer(enemyInstance, 10f);
+    public void UsePrimarySkill(BlobAI enemyInstance) => this.SetAngeredTimer(enemyInstance, 18f);
 
 
     public float? InteractRange(BlobAI _) => 3.5f;
 
-    public float? SprintMultiplier(BlobAI _) => 6.8f;
+    public float? SprintMultiplier(BlobAI _) => 9.8f;
 }
 
