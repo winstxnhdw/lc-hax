a872154a356109b937c9989914f317701cbf7241
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-16 14:42:51 +0100

    remove redundant parameter

--- lc-hax/Scripts/Modules/Possession/PossessionMod.cs
+++ lc-hax/Scripts/Modules/Possession/PossessionMod.cs
@@ -161,12 +161,12 @@ internal sealed class PossessionMod : MonoBehaviour {
             }
         }
 
-        this.UpdateCameraPosition(camera, enemy, characterMovement);
+        this.UpdateCameraPosition(camera, enemy);
         this.UpdateCameraRotation(camera);
         this.InteractWithAmbient();
     }
 
-    void UpdateCameraPosition(Camera camera, EnemyAI enemy, CharacterMovement movement) =>
+    void UpdateCameraPosition(Camera camera, EnemyAI enemy) =>
         camera.transform.position = enemy.transform.position + (3.0f * (Vector3.up - enemy.transform.forward));
 
 
