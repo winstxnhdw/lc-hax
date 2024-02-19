35148f3d7e59fa79c7852cb98bf976596469fb53
winstxnhdw <winstxnhdw@gmail.com>
2024-02-20 01:50:21 +0800

    feat: only clear Steam ID from clipboard if manually joining lobby

--- lc-hax/Scripts/Patches/Dependencies/LobbyDependencyPatch.cs
+++ lc-hax/Scripts/Patches/Dependencies/LobbyDependencyPatch.cs
@@ -1,11 +1,17 @@
-using HarmonyLib;
+using System.Text.RegularExpressions;
+using UnityEngine;
 using Steamworks;
-using Hax;
 using Steamworks.Data;
+using HarmonyLib;
+using Hax;
 
 [HarmonyPatch(typeof(GameNetworkManager), nameof(GameNetworkManager.JoinLobby))]
 class LobbyDependencyPatch {
     static void Postfix(Lobby lobby, SteamId id) {
+        if (State.DisconnectedVoluntarily && Regex.IsMatch(GUIUtility.systemCopyBuffer, @"^\d{17}$")) {
+            GUIUtility.systemCopyBuffer = "";
+        }
+
         State.ConnectedLobby = new ConnectedLobby(lobby, id);
         State.DisconnectedVoluntarily = false;
     }
