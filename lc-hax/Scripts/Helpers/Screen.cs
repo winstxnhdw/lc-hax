8b7628a6638d6a6685a98417c4f716f974ad2ddc
winstxnhdw <winstxnhdw@gmail.com>
2024-02-21 16:21:53 +0800

    perf: use `sqrMagnitude` instead

--- lc-hax/Scripts/Helpers/Screen.cs
+++ lc-hax/Scripts/Helpers/Screen.cs
@@ -4,13 +4,4 @@ namespace Hax;
 
 internal static partial class Helper {
     internal static Vector2 GetScreenCentre() => new Vector2(Screen.width, Screen.height) * 0.5f;
-
-    internal static Vector3 WorldToEyesPoint(this Camera camera, Vector3 worldPosition) {
-        Vector3 screen = camera.WorldToViewportPoint(worldPosition);
-        screen.x *= Screen.width;
-        screen.y *= Screen.height;
-        screen.y = Screen.height - screen.y;
-
-        return screen;
-    }
 }
