f374c9cb60ca11fc096bd442258abd16a1325133
winstxnhdw <winstxnhdw@gmail.com>
2024-02-21 22:08:09 +0800

    feat: write analysers for commands and patches

---
+++ analysers/CommandCodeFix.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Composition;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(CommandCodeFix)), Shared]
+public class CommandCodeFix : CodeFixProvider {
+    public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(CommandAnalyzer.DiagnosticID);
+
+    public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context) {
+        Diagnostic diagnostic = context.Diagnostics.First();
+        TextSpan diagnosticSpan = diagnostic.Location.SourceSpan;
+
+        CodeAction codeAction = CodeAction.Create(
+            "Implement ICommand interface",
+            cancellationToken => this.ImplementICommandAsync(context.Document, diagnosticSpan, cancellationToken),
+            nameof(CommandCodeFix)
+        );
+
+        context.RegisterCodeFix(codeAction, diagnostic);
+
+        await Task.CompletedTask;
+    }
+
+    async Task<Document> ImplementICommandAsync(Document document, TextSpan diagnosticSpan, CancellationToken cancellationToken) {
+        SyntaxNode? root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+        if (root?.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf().OfType<ClassDeclarationSyntax>().First() is not ClassDeclarationSyntax classDeclaration) {
+            return document;
+        }
+
+        BaseListSyntax baseList = classDeclaration?.BaseList ?? SyntaxFactory.BaseList();
+        BaseListSyntax newBaseList = baseList.AddTypes(SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName("ICommand")));
+
+        return classDeclaration?.WithBaseList(newBaseList) is not ClassDeclarationSyntax newClassDeclaration
+            ? document
+            : document.WithSyntaxRoot(root.ReplaceNode(classDeclaration, newClassDeclaration));
+    }
+}
