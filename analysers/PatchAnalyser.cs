571814969c3a819eaf8ce2affa880d1e9bba05e7
winstxnhdw <winstxnhdw@gmail.com>
2024-02-21 22:08:09 +0800

    feat: write analysers for commands and patches

---
+++ analysers/PatchAnalyser.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+internal class PatchAnalyzer : DiagnosticAnalyzer {
+    internal const string DiagnosticID = "HAX002";
+
+    static DiagnosticDescriptor Rule { get; } = new(
+        DiagnosticID,
+        "Patch instance requires double underscore prefix",
+        "Instance parameter of Patch method '{0}' must be named '__instance'",
+        "Usage",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(PatchAnalyzer.Rule);
+
+    public override void Initialize(AnalysisContext context) {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        context.RegisterSyntaxNodeAction(AnalyseSyntaxNode, SyntaxKind.MethodDeclaration);
+    }
+
+    static void AnalyseSyntaxNode(SyntaxNodeAnalysisContext context) {
+        if (context.Node is not MethodDeclarationSyntax methodDeclaration) return;
+        if (context.SemanticModel.GetDeclaredSymbol(methodDeclaration, context.CancellationToken) is not IMethodSymbol methodSymbol) return;
+        if (!methodSymbol.ContainingType.Name.EndsWith("Patch")) return;
+        if (!methodSymbol.Parameters.Any(parameter => parameter.Name is "instance")) return;
+
+        context.ReportDiagnostic(
+            Diagnostic.Create(PatchAnalyzer.Rule, methodDeclaration.Identifier.GetLocation(), methodSymbol.Name)
+        );
+    }
+}
