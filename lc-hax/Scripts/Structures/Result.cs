d88d2d7c4065563563579c37274318c818a6bf24
winstxnhdw <winstxnhdw@gmail.com>
2024-02-22 15:38:22 +0800

    chore: remove redundant `readonly` modifier

--- lc-hax/Scripts/Structures/Result.cs
+++ lc-hax/Scripts/Structures/Result.cs
@@ -1,4 +1,4 @@
 readonly ref struct Result(bool success = false, string? message = null) {
-    internal readonly bool Success { get; } = success;
-    internal readonly string? Message { get; } = message;
+    internal bool Success { get; } = success;
+    internal string? Message { get; } = message;
 }
