5070d28f1959068d171a93db982003fea2b6ccd1
xAstroBoy <307941036+xAstroBoy@users.noreply.github.com>
2024-02-23 20:11:44 +0100

    Update MaskedController.cs

--- lc-hax/Scripts/Modules/Possession/Controllers/MaskedController.cs
+++ lc-hax/Scripts/Modules/Possession/Controllers/MaskedController.cs
@@ -1,4 +1,11 @@
+using GameNetcodeStuff;
+
 internal class MaskedPlayerController : IEnemyController<MaskedPlayerEnemy> {
+
+    void SetstartingKillAnimationLocalClient(MaskedPlayerEnemy enemy, bool value) =>
+        enemy.Reflect().SetInternalField("startingKillAnimationLocalClient", value);
+
+    bool GetinKillAnimation(MaskedPlayerEnemy enemy) => enemy.Reflect().GetInternalField<bool>("inKillAnimation");
     public void UsePrimarySkill(MaskedPlayerEnemy enemy) => enemy.SetHandsOutServerRpc(!enemy.creatureAnimator.GetBool("HandsOut"));
 
     public void UseSecondarySkill(MaskedPlayerEnemy enemy) => enemy.SetCrouchingServerRpc(!enemy.creatureAnimator.GetBool("Crouching"));
@@ -9,4 +16,15 @@ internal class MaskedPlayerController : IEnemyController<MaskedPlayerEnemy> {
 
     public void OnOutsideStatusChange(MaskedPlayerEnemy enemy) => enemy.StopSearch(enemy.searchForPlayers, true);
 
+
+    public void OnPlayerCollision(MaskedPlayerEnemy enemy, PlayerControllerB player) {
+        if(this.GetinKillAnimation(enemy)) return;
+        PlayerControllerB playerControllerB = enemy.MeetsStandardPlayerCollisionConditions(player.playerCollider);
+        if (playerControllerB != null) {
+
+            this.SetstartingKillAnimationLocalClient(enemy, true);
+            enemy.KillPlayerAnimationServerRpc((int)playerControllerB.actualClientId);
+        }
+
+    }
 }
