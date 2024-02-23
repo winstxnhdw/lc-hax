305908e39c9d8d8117ab132a7b3788a10614b31e
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-23 21:55:36 +0100

    Add Kills for inside enemies if they are outside

--- lc-hax/Scripts/Modules/Possession/Controllers/CrawlerController.cs
+++ lc-hax/Scripts/Modules/Possession/Controllers/CrawlerController.cs
@@ -1,6 +1,12 @@
 using GameNetcodeStuff;
+using UnityEngine;
 
 internal class CrawlerController : IEnemyController<CrawlerAI> {
+
+    float GettimeSinceHittingPlayer(CrawlerAI enemy) => enemy.Reflect().GetInternalField<float>("timeSinceHittingPlayer");
+
+    void SettimeSinceHittingPlayer(CrawlerAI enemy, float value) => enemy.Reflect().SetInternalField("timeSinceHittingPlayer", value);
+
     public float InteractRange(CrawlerAI _) => 1.5f;
 
     public bool SyncAnimationSpeedEnabled(CrawlerAI _) => false;
@@ -11,6 +17,15 @@ internal class CrawlerController : IEnemyController<CrawlerAI> {
 
     public void OnOutsideStatusChange(CrawlerAI enemy) => enemy.StopSearch(enemy.searchForPlayers, true);
 
-    public void OnCollideWithPlayer(CrawlerAI enemy, PlayerControllerB player) => enemy.OnCollideWithPlayer(player.playerCollider);
+    public void OnCollideWithPlayer(CrawlerAI enemy, PlayerControllerB player) {
+        if (enemy.isOutside) {
+            if (this.GettimeSinceHittingPlayer(enemy) < 0.65f) {
+                return;
+            }
+            this.SettimeSinceHittingPlayer(enemy, 0f);
+            player.DamagePlayer(40, true, true, CauseOfDeath.Mauling, 0, false, default);
+            enemy.HitPlayerServerRpc((int)GameNetworkManager.Instance.localPlayerController.playerClientId);
+        }
+    }
 
 }
