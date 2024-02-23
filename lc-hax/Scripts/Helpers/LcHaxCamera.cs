3f0cf561b2a2739563d4ee7e00bf2dba5e4a9c64
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-23 17:33:04 +0100

    Update LcHaxCamera.cs

--- lc-hax/Scripts/Helpers/LcHaxCamera.cs
+++ lc-hax/Scripts/Helpers/LcHaxCamera.cs
@@ -30,13 +30,12 @@ internal static partial class Helper {
         }
     }
 
-    internal static Camera GetCustomCamera(bool Spawn = true) {
+    internal static Camera GetCustomCamera() {
         if (Helper.LocalPlayer?.gameplayCamera is not Camera camData) return null;
         if (CustomCamera is not null) return CustomCamera;
         if (CustomCameraObj is null) {
             CustomCameraObj = new GameObject("lc-hax Camera");
         }
-        if(!Spawn) return null;
         Camera? newCam = CustomCameraObj.AddComponent<Camera>();
         if (newCam is null) return null;
         newCam.transform.position = camData.transform.position;
