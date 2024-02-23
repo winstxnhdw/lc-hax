f3ff12efed9af4347f00dfe53265dda5319b9c0b
xAstroBoy <307941036+xAstroBoy@users.noreply.github.com>
2024-02-23 20:42:42 +0100

    Minor fixes and call collision of enemies that aren't supposed to be outside.

--- lc-hax/Scripts/Modules/Possession/Controllers/MaskedController.cs
+++ lc-hax/Scripts/Modules/Possession/Controllers/MaskedController.cs
@@ -17,14 +17,5 @@ internal class MaskedPlayerController : IEnemyController<MaskedPlayerEnemy> {
     public void OnOutsideStatusChange(MaskedPlayerEnemy enemy) => enemy.StopSearch(enemy.searchForPlayers, true);
 
 
-    public void OnPlayerCollision(MaskedPlayerEnemy enemy, PlayerControllerB player) {
-        if(this.GetinKillAnimation(enemy)) return;
-        PlayerControllerB playerControllerB = enemy.MeetsStandardPlayerCollisionConditions(player.playerCollider);
-        if (playerControllerB != null) {
-
-            this.SetstartingKillAnimationLocalClient(enemy, true);
-            enemy.KillPlayerAnimationServerRpc((int)playerControllerB.actualClientId);
-        }
-
-    }
+    public void OnPlayerCollision(MaskedPlayerEnemy enemy, PlayerControllerB player) => enemy.OnCollideWithPlayer(player.playerCollider);
 }
