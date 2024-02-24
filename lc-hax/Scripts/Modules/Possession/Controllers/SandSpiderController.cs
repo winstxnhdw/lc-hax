fe3aa9ab253c3016d4874c6c15e02572ce8195ca
xAstroBoy <307941036+xAstroBoy@users.noreply.github.com>
2024-02-24 22:50:19 +0100

    Fix Jester & Spider

--- lc-hax/Scripts/Modules/Possession/Controllers/SandSpiderController.cs
+++ lc-hax/Scripts/Modules/Possession/Controllers/SandSpiderController.cs
@@ -19,9 +19,8 @@ internal class SandSpiderController : IEnemyController<SandSpiderAI> {
 
     public void Update(SandSpiderAI enemy, bool isAIControlled) {
         enemy.meshContainerPosition = enemy.transform.position;
-        enemy.meshContainerTarget = enemy.transform.forward;
         enemy.SyncMeshContainerPositionToClients();
-        if (!isAIControlled) enemy.homeNode = enemy.ChooseClosestNodeToPosition(enemy.transform.position, false, 2);
+        if(!isAIControlled) enemy.homeNode = enemy.ChooseClosestNodeToPosition(enemy.transform.position, false, 2);
     }
 
     public bool SyncAnimationSpeedEnabled(SandSpiderAI enemy) => false;
@@ -44,8 +43,7 @@ internal class SandSpiderController : IEnemyController<SandSpiderAI> {
             return;
         }
 
-        if (!Physics.Raycast(enemy.abdomen.position, Vector3.down, out RaycastHit groundHit, 10.0f,
-                startOfRound.collidersAndRoomMask)) {
+        if (!Physics.Raycast(enemy.abdomen.position, Vector3.down, out RaycastHit groundHit, 10.0f, startOfRound.collidersAndRoomMask)) {
             return;
         }
 
@@ -58,7 +56,7 @@ internal class SandSpiderController : IEnemyController<SandSpiderAI> {
     public void OnCollideWithPlayer(SandSpiderAI enemy, PlayerControllerB player) {
         if (enemy.isOutside) {
             if (this.GetOnWall(enemy)) return;
-            if (this.GetSpoolingPlayerBody(enemy)) return;
+            if(this.GetSpoolingPlayerBody(enemy)) return;
             if (this.GetTimeSinceHittingPlayer(enemy) > 1f) {
                 this.SetTimeSinceHittingPlayer(enemy, 0.0f);
                 player.DamagePlayer(90, true, true, CauseOfDeath.Mauling, 0, false, default);
@@ -67,3 +65,4 @@ internal class SandSpiderController : IEnemyController<SandSpiderAI> {
         }
     }
 }
+
