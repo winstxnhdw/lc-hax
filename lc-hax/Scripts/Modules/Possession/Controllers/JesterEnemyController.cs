2fa4b89507b5e7e0925aa163f7cdaf2c5f793203
xAstroBoy <307941036+xAstroBoy@users.noreply.github.com>
2024-02-25 00:20:09 +0100

    Apply suggestions from code review
    Co-authored-by: Winston H. <56998716+winstxnhdw@users.noreply.github.com>

--- lc-hax/Scripts/Modules/Possession/Controllers/JesterEnemyController.cs
+++ lc-hax/Scripts/Modules/Possession/Controllers/JesterEnemyController.cs
@@ -45,7 +45,7 @@ internal class JesterController : IEnemyController<JesterAI> {
         if (enemy.isOutside) {
             if (player.IsDead()) return;
             if (!enemy.IsBehaviourState(JesterState.OPEN)) return;
-            enemy.KillPlayerServerRpc((int)player.playerClientId);
+            enemy.KillPlayerServerRpc(player.PlayerIndex());
         }
     }
 }
