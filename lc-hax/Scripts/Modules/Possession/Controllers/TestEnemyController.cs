c2bec69205a0f1042a026b5484bb180bbb1536af
D1GQ <83262852+D1GQ@users.noreply.github.com>
2024-02-25 19:38:54 +0000

    Fixed TestEnemy possession position.

--- lc-hax/Scripts/Modules/Possession/Controllers/TestEnemyController.cs
+++ lc-hax/Scripts/Modules/Possession/Controllers/TestEnemyController.cs
@@ -1,4 +1,10 @@
 internal class TestEnemyController : IEnemyController<TestEnemy> {
+    public void GetCameraPosition(TestEnemy enemy) {
+        PossessionMod.CamOffsetY = 2f;
+        PossessionMod.CamOffsetZ = -4.5f;
+        PossessionMod.EnemyYPositionOffset = 1.5f;
+    }
+
     public bool CanUseEntranceDoors(TestEnemy _) => true;
 
     public float InteractRange(TestEnemy _) => 4.5f;
