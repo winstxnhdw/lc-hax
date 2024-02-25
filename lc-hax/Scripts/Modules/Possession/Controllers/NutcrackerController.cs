e4993dbd454efb2dfd05abc5be7b5be19f87fa73
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-25 13:07:57 +0100

    restore chqnges

--- lc-hax/Scripts/Modules/Possession/Controllers/NutcrackerController.cs
+++ lc-hax/Scripts/Modules/Possession/Controllers/NutcrackerController.cs
@@ -60,4 +60,14 @@ internal class NutcrackerController : IEnemyController<NutcrackerEnemyAI> {
         enemy.StopSearch(enemy.patrol, true);
     }
 
+    public void OnCollideWithPlayer(NutcrackerEnemyAI enemy, PlayerControllerB player) {
+        if (enemy.isOutside) {
+            if (enemy.isEnemyDead) return;
+            if (this.GetTimeSinceHittingPlayer(enemy) < 1f) return;
+            if (this.GetStunNormalizedTimer(enemy) >= 0f) return;
+
+            this.SetTimeSinceHittingPlayer(enemy, 0f);
+            enemy.LegKickPlayerServerRpc(player.PlayerIndex());
+        }
+    }
 }
