ae2687fa6edf95269840e6974f2573bca01df35b
winstxnhdw <winstxnhdw@gmail.com>
2024-02-26 19:22:11 +0800

    feat: preserve all names in antikick

--- lc-hax/Scripts/Extensions/ForEach.cs
+++ lc-hax/Scripts/Extensions/ForEach.cs
@@ -9,6 +9,14 @@ static partial class Extensions {
         }
     }
 
+    internal static void ForEach<T>(this IEnumerable<T> array, Action<int, T> action) {
+        int i = 0;
+
+        foreach (T item in array) {
+            action(i++, item);
+        }
+    }
+
     internal static void ForEach<T>(this T[] array, Action<int, T> action) {
         for (int i = 0; i < array.Length; i++) {
             action(i, array[i]);
