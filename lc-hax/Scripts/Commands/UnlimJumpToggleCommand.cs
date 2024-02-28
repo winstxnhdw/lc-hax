5b2951c493a8de1d00b7cb26d8fe85d54f3c80ff
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-28 13:04:10 +0100

    Update lc-hax/Scripts/Commands/UnlimJumpToggleCommand.cs
    Co-authored-by: Winston H. <56998716+winstxnhdw@users.noreply.github.com>

--- lc-hax/Scripts/Commands/UnlimJumpToggleCommand.cs
+++ lc-hax/Scripts/Commands/UnlimJumpToggleCommand.cs
@@ -1,7 +1,7 @@
 using Hax;
 
-[Command("unlimjump")]
-internal class UnlimJumpToggleCommand : ICommand {
+[Command("jump")]
+class UnlimitedJumpCommand : ICommand {
     public void Execute(StringArray _) {
         Setting.UnlimitedJump = !Setting.UnlimitedJump;
         Helper.SendNotification(title: "Unlimited Jump", body: Setting.UnlimitedJump ? " enabled" : "disabled");
