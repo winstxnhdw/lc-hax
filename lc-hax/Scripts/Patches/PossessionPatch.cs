832b651c0b949fdf03cde0e76ea30550fcfc8c85
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-28 10:55:18 +0100

    Update PossessionPatch.cs

--- lc-hax/Scripts/Patches/PossessionPatch.cs
+++ lc-hax/Scripts/Patches/PossessionPatch.cs
@@ -12,22 +12,11 @@ class PossessionPatch {
         return false;
     }
 
-    static bool ReloadUI = false;
-
     [HarmonyPatch(typeof(HUDManager), "Update")]
     static void Prefix(HUDManager __instance, ref float ___holdButtonToEndGameEarlyHoldTime, ref bool ___hasLoadedSpectateUI) {
         if (PossessionMod.Instance is null or { IsPossessed: true }) {
             ___holdButtonToEndGameEarlyHoldTime = 0.0f;
             __instance.holdButtonToEndGameEarlyMeter?.gameObject.SetActive(false);
-            __instance.HUDAnimator.SetTrigger("hideHud");
-            __instance.scanInfoAnimator.SetBool("display", false);
-            ReloadUI = true;
-        } else if (ReloadUI) {
-            __instance.scanInfoAnimator.SetBool("display", true);
-            ReloadUI = false;
-        } else {
-            __instance.HUDAnimator.SetTrigger("revealHud");
-        }
     }
 
     [HarmonyPatch(typeof(TimeOfDay), nameof(TimeOfDay.VoteShipToLeaveEarly))]
