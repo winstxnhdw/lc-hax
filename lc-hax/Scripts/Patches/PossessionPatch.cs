cca8314918d7d827bda31b85e8c40231c55c3a2c
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-28 10:50:41 +0100

    Restore Spectator hud.

--- lc-hax/Scripts/Patches/PossessionPatch.cs
+++ lc-hax/Scripts/Patches/PossessionPatch.cs
@@ -12,27 +12,6 @@ class PossessionPatch {
         return false;
     }
 
-    static bool ReloadUI = false;
-
-    [HarmonyPatch(typeof(HUDManager), "Update")]
-    static void Prefix(HUDManager __instance, ref float ___holdButtonToEndGameEarlyHoldTime, ref bool ___hasLoadedSpectateUI) {
-        if (PossessionMod.Instance is null or { IsPossessed: true }) {
-            ___holdButtonToEndGameEarlyHoldTime = 0.0f;
-            __instance.holdButtonToEndGameEarlyMeter?.gameObject.SetActive(false);
-            __instance.RemoveSpectateUI();
-            ___hasLoadedSpectateUI = true;
-            __instance.HUDAnimator.SetTrigger("hideHud");
-            __instance.scanInfoAnimator.SetBool("display", false);
-            ReloadUI = true;
-        } else if (ReloadUI) {
-            ___hasLoadedSpectateUI = false;
-            __instance.scanInfoAnimator.SetBool("display", true);
-            ReloadUI = false;
-        } else {
-            __instance.HUDAnimator.SetTrigger("revealHud");
-        }
-    }
-
     [HarmonyPatch(typeof(TimeOfDay), nameof(TimeOfDay.VoteShipToLeaveEarly))]
     static bool Prefix() => PossessionMod.Instance is null or { IsPossessed: false };
 }
