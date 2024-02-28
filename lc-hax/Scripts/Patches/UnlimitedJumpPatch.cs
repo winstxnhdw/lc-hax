1849e8731a7aa47a5632a154329e6020b0cc42fa
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-02-28 20:21:17 +0800

    refactor: chain reflectors

--- lc-hax/Scripts/Patches/UnlimitedJumpPatch.cs
+++ lc-hax/Scripts/Patches/UnlimitedJumpPatch.cs
@@ -12,23 +12,24 @@ class UnlimitedJumpPatch {
     static bool Prefix(PlayerControllerB __instance) {
         if (!Setting.EnableUnlimitedJump) return true;
         if (!__instance.IsSelf()) return true;
+        if (__instance is { inSpecialInteractAnimation: true, isTypingChat: true, isPlayerControlled: false }) return true;
         if (__instance.quickMenuManager.isMenuOpen) return true;
-        if (!__instance.isPlayerControlled) return true;
-        if (__instance.inSpecialInteractAnimation) return true;
-        if (__instance.isTypingChat) return true;
-        Reflector<PlayerControllerB> player = __instance.Reflect();
 
-        _ = player.SetInternalField("playerSlidingTimer", 0f);
-        _ = player.SetInternalField("isJumping", true);
-        __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter - 0.08f, 0f, 1f);
+        Reflector<PlayerControllerB> playerReflector = __instance.Reflect();
+
+        __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter - 0.08f, 0.0f, 1.0f);
         __instance.movementAudio.PlayOneShot(StartOfRound.Instance.playerJumpSFX);
-        Coroutine? jumpCoroutine = player.GetInternalField<Coroutine>("jumpCoroutine");
 
-        if (jumpCoroutine != null) {
+        Coroutine? jumpCoroutine = 
+            playerReflector.SetInternalField("playerSlidingTimer", 0.0f)
+                           .SetInternalField("isJumping", true)
+                           .GetInternalField<Coroutine>("jumpCoroutine");
+
+        if (jumpCoroutine is not null) {
             __instance.StopCoroutine(jumpCoroutine);
         }
         // create a new coroutine to handle the jump
-        _ = player.SetInternalField("jumpCoroutine", __instance.StartCoroutine(player.InvokeInternalMethod<IEnumerator>("PlayerJump")));
+        _ = player.SetInternalField("jumpCoroutine", __instance.StartCoroutine(playerReflector.InvokeInternalMethod<IEnumerator>("PlayerJump")));
         return false;
     }
 }
