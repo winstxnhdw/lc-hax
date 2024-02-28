d299cb4f49391cb9fe8102060a6af4c6a25372e5
xAstroBoy <307941036+xAstroBoy@users.noreply.github.com>
2024-02-28 10:56:43 +0100

    Update PossessionPatch.cs

--- lc-hax/Scripts/Patches/PossessionPatch.cs
+++ lc-hax/Scripts/Patches/PossessionPatch.cs
@@ -13,14 +13,14 @@ class PossessionPatch {
     }
 
     [HarmonyPatch(typeof(HUDManager), "Update")]
-    static void Prefix(HUDManager __instance, ref float ___holdButtonToEndGameEarlyHoldTime, ref bool ___hasLoadedSpectateUI) {
+    static void Prefix(HUDManager __instance, ref float ___holdButtonToEndGameEarlyHoldTime,
+        ref bool ___hasLoadedSpectateUI) {
         if (PossessionMod.Instance is null or { IsPossessed: true }) {
             ___holdButtonToEndGameEarlyHoldTime = 0.0f;
             __instance.holdButtonToEndGameEarlyMeter?.gameObject.SetActive(false);
         }
-
-        [HarmonyPatch(typeof(TimeOfDay), nameof(TimeOfDay.VoteShipToLeaveEarly))]
-        static bool Prefix() {
-            return PossessionMod.Instance is null or { IsPossessed: false };
-        }
     }
+
+    [HarmonyPatch(typeof(TimeOfDay), nameof(TimeOfDay.VoteShipToLeaveEarly))]
+    static bool Prefix() => PossessionMod.Instance is null or { IsPossessed: false };
+}
