947e63b8eed29ce596404ac3090bd88010ff0ad4
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-28 11:36:54 +0100

    Update PlayerIsTargetablePatch.cs

--- lc-hax/Scripts/Modules/Possession/DependencyPatch/PlayerIsTargetablePatch.cs
+++ lc-hax/Scripts/Modules/Possession/DependencyPatch/PlayerIsTargetablePatch.cs
@@ -1,12 +1,19 @@
 using GameNetcodeStuff;
 using HarmonyLib;
 using System;
+using Hax;
 using UnityEngine;
 
 [HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.PlayerIsTargetable))]
 public static class PlayerIsTargetablePatch {
-    public static bool Prefix(ref bool __result, EnemyAI __instance, PlayerControllerB playerScript,
-        ref bool cannotBeInShip, ref bool overrideInsideFactoryCheck) {
+    public static bool Prefix(ref bool __result, EnemyAI __instance, PlayerControllerB playerScript, ref bool cannotBeInShip, ref bool overrideInsideFactoryCheck) {
+        if (playerScript.IsDead()) {
+            return true;
+        }
+        if (playerScript.inAnimationWithEnemy != null) {
+            return true;
+        }
+
         overrideInsideFactoryCheck = true;
         cannotBeInShip = false;
 
@@ -15,18 +22,11 @@ public static class PlayerIsTargetablePatch {
             return false;
         }
 
-        if (SkipTargetCheck(playerScript, __instance)) {
+        if (__instance.isOutside != __instance.enemyType.isOutsideEnemy) {
             __result = true;
             return false;
         }
 
         return true;
-    }
-
-    private static bool SkipTargetCheck(PlayerControllerB playerScript, EnemyAI enemyAI) {
-        return !playerScript.isPlayerControlled ||
-               playerScript.isPlayerDead ||
-               playerScript.inAnimationWithEnemy != null ||
-               enemyAI.isOutside == enemyAI.enemyType.isOutsideEnemy;
     }
 }
