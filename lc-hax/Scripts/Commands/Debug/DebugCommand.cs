87d6f2cb29dac07e7d2722814ae85006311a9027
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-28 13:48:19 +0100

    Cleanup

--- lc-hax/Scripts/Commands/Debug/DebugCommand.cs
+++ lc-hax/Scripts/Commands/Debug/DebugCommand.cs
@@ -2,7 +2,7 @@ class DebugCommand(ICommand command) : ICommand {
     ICommand Command { get; } = command;
 
     public void Execute(StringArray args) {
-        Chat.Print("This debug command is for testing purposes and is not meant for use!");
+        //Chat.Print("This debug command is for testing purposes and is not meant for use!");
         this.Command.Execute(args);
     }
 }
