c1f1618dbb83244150e7b7218889bf2265f67701
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-02-29 14:10:08 +0100

    Discard changes to lc-hax/Scripts/Commands/UnlimitedJumpCommand.cs

--- lc-hax/Scripts/Commands/UnlimitedJumpCommand.cs
+++
@@ -1,9 +0,0 @@
-using Hax;
-
-[Command("jump")]
-class UnlimitedJumpCommand : ICommand {
-    public void Execute(StringArray _) {
-        Setting.EnableUnlimitedJump = !Setting.EnableUnlimitedJump;
-        Helper.SendNotification(title: "Unlimited Jump", body: Setting.EnableUnlimitedJump ? " enabled" : "disabled");
-    }
-}
