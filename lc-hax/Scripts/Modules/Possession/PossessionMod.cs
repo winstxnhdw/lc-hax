17b78305944d6b2a9308fb45a6d128a24a08b566
D1GQ <83262852+D1GQ@users.noreply.github.com>
2024-02-29 08:58:18 +0000

    Fixed reoccurring IsAbleToMove issues

--- lc-hax/Scripts/Modules/Possession/PossessionMod.cs
+++ lc-hax/Scripts/Modules/Possession/PossessionMod.cs
@@ -206,7 +206,9 @@ internal sealed class PossessionMod : MonoBehaviour {
         if (controller.IsAbleToMove(enemy)) {
             controller.OnMovement(enemy, this.CharacterMovement.IsMoving, this.CharacterMovement.IsSprinting);
             this.UpdateEnemyPosition(enemy);
+            CharacterMovement.AbleToMove = true;
         }
+        else { CharacterMovement.AbleToMove = false;}
 
         if (controller.IsAbleToRotate(enemy)) {
             this.UpdateEnemyRotation();
