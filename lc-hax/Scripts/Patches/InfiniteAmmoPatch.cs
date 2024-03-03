edaab1494c131d72686c161886624255438cebfa
D1GQ <83262852+D1GQ@users.noreply.github.com>
2024-03-03 12:16:38 -0600

    feat/fix: add anti-flash, fix fake reload and full pan range (#441)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Winston H <56998716+winstxnhdw@users.noreply.github.com>
    Co-authored-by: winstxnhdw <winstxnhdw@gmail.com>

--- lc-hax/Scripts/Patches/InfiniteAmmoPatch.cs
+++ lc-hax/Scripts/Patches/InfiniteAmmoPatch.cs
@@ -4,8 +4,8 @@ using HarmonyLib;
 
 [HarmonyPatch(typeof(ShotgunItem), nameof(ShotgunItem.ItemActivate))]
 class InfiniteShotgunAmmoPatch {
-    static void Prefix(ShotgunItem __instance, EnemyAI? ___heldByEnemy) {
-        if (___heldByEnemy is not null) return;
+    static void Prefix(ShotgunItem __instance, ref EnemyAI? ___heldByEnemy) {
+        if (__instance.isReloading || ___heldByEnemy is not null) return;
         __instance.shellsLoaded = 3;
     }
 }
