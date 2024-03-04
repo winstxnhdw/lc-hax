48066908457f44616302f3b0afb377a4438e3b5e
winstxnhdw <winstxnhdw@gmail.com>
2024-03-05 05:04:27 +0800

    refactor(LookDownPatch): abstract into reusable method

--- lc-hax/Scripts/Patches/LookDownPatch.cs
+++ lc-hax/Scripts/Patches/LookDownPatch.cs
@@ -5,9 +5,7 @@ using HarmonyLib;
 
 [HarmonyPatch(typeof(PlayerControllerB))]
 class LookDownPatch {
-    [HarmonyTranspiler]
-    [HarmonyPatch("CalculateSmoothLookingInput")]
-    static IEnumerable<CodeInstruction> SmoothLookingTranspiler(IEnumerable<CodeInstruction> instructions) {
+    static IEnumerable<CodeInstruction> CalculateLookingInputTranspiler(IEnumerable<CodeInstruction> instructions) {
         foreach (CodeInstruction instruction in instructions) {
             if (instruction.opcode == OpCodes.Ldc_R4 && instruction.operand.Equals(60.0f)) {
                 instruction.operand = 90.0f;
@@ -16,16 +14,14 @@ class LookDownPatch {
             yield return instruction;
         }
     }
+
+    [HarmonyTranspiler]
+    [HarmonyPatch("CalculateSmoothLookingInput")]
+    static IEnumerable<CodeInstruction> SmoothLookingTranspiler(IEnumerable<CodeInstruction> instructions) =>
+        LookDownPatch.CalculateLookingInputTranspiler(instructions);
 
     [HarmonyTranspiler]
     [HarmonyPatch("CalculateNormalLookingInput")]
-    static IEnumerable<CodeInstruction> NormalLookingTranspiler(IEnumerable<CodeInstruction> instructions) {
-        foreach (CodeInstruction instruction in instructions) {
-            if (instruction.opcode == OpCodes.Ldc_R4 && instruction.operand.Equals(60.0f)) {
-                instruction.operand = 90.0f;
-            }
-
-            yield return instruction;
-        }
-    }
+    static IEnumerable<CodeInstruction> NormalLookingTranspiler(IEnumerable<CodeInstruction> instructions) =>
+        LookDownPatch.CalculateLookingInputTranspiler(instructions);
 }
