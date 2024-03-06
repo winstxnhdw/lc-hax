ae6b88fe9f7abc524d51e59ecfd17ecc43cec2c8
winstxnhdw <winstxnhdw@gmail.com>
2024-03-06 18:38:52 +0800

    fix: use RPC to move object

--- lc-hax/Scripts/Commands/SpinCommand.cs
+++ lc-hax/Scripts/Commands/SpinCommand.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
 using Hax;
+using System;
 
 [Command("spin")]
 class SpinCommand : ICommand {
+    Action PlaceObjectAtRotation(PlaceableShipObject shipObject, float timeElapsed) => () => {
+        Vector3 rotation = new(0.0f, timeElapsed * 810.0f, 0.0f);
+
+        Helper.PlaceObjectAtPosition(
+            shipObject,
+            shipObject.transform.position,
+            rotation
+        );
+    };
+
+    Action SpinObject(PlaceableShipObject shipObject, ulong duration) => () =>
+        Helper.CreateComponent<TransientBehaviour>($"Spin {shipObject.name}")
+              .Init(timeElapsed => this.PlaceObjectAtRotation(shipObject, timeElapsed), duration);
+
     public void Execute(StringArray args) {
         if (args.Length is 0) {
             Chat.Print("Usage: spin <duration>");
@@ -12,10 +27,7 @@ class SpinCommand : ICommand {
             Chat.Print("Invalid duration!");
         }
 
-        Helper.FindObjects<PlaceableShipObject>().ForEach(shipObject =>
-            Helper
-                .CreateComponent<TransientBehaviour>($"Spin {shipObject.name}")
-                .Init(timeElapsed => shipObject.transform.Rotate(Vector3.up, timeElapsed * 810), duration)
-        );
+        Helper.FindObjects<PlaceableShipObject>()
+              .ForEach(shipObject => this.SpinObject(shipObject, duration));
     }
 }
