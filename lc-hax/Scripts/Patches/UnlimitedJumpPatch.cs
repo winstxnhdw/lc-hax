02318b20e854a51d090826d2fadece1d5b7606d9
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-03-07 20:09:34 +0800

    refactor: remove redundant conditionals

--- lc-hax/Scripts/Patches/UnlimitedJumpPatch.cs
+++ lc-hax/Scripts/Patches/UnlimitedJumpPatch.cs
@@ -10,9 +10,11 @@ using UnityEngine;
 class UnlimitedJumpPatch {
     static bool Prefix(PlayerControllerB __instance) {
         if (!Setting.EnableUnlimitedJump || !__instance.IsSelf()) return true;
-        if (__instance is { inSpecialInteractAnimation: true, isTypingChat: true, isPlayerControlled: false }) return false;
-        if (__instance.quickMenuManager.isMenuOpen) return false;
+        if (!__instance.isPlayerControlled) return false;
+        if (__instance.inSpecialInteractAnimation) return false;
         if (__instance.isTypingChat) return false;
+        if (__instance.quickMenuManager.isMenuOpen) return false;
+
         __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter - 0.08f, 0.0f, 1.0f);
         __instance.movementAudio.PlayOneShot(StartOfRound.Instance.playerJumpSFX);
 
