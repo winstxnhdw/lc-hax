8f422fd7bf27f94cbc1c7390a6bda16ab305aefc
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-03-10 12:58:24 +0100

    Update WeightToggle.cs

--- lc-hax/Scripts/Commands/Mod Toggles/WeightToggle.cs
+++ lc-hax/Scripts/Commands/Mod Toggles/WeightToggle.cs
@@ -1,4 +1,6 @@
+using GameNetcodeStuff;
 using Hax;
+using UnityEngine;
 
 [Command("weight")]
 internal class WeightToggle : ICommand {
@@ -6,5 +8,17 @@ internal class WeightToggle : ICommand {
         if (WeightMod.Instance is not WeightMod weight) return;
         weight.enabled = !weight.enabled;
         Helper.SendNotification("Weight Mod", weight.enabled ? " enabled" : "disabled");
+        if (Helper.LocalPlayer is not PlayerControllerB player) return;
+        player.carryWeight = 1.0f;
+        if (!weight.enabled) {
+            // calculate weight by held items
+            foreach(GrabbableObject item in player.ItemSlots) {
+
+                player.carryWeight += Mathf.Clamp(player.carryWeight - (item.itemProperties.weight - 1f), 0f, 10f);
+            }
+        }
+        else {
+            player.carryWeight = 1.0f;
+        }
     }
 }
