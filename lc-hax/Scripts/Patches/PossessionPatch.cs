1d93b7f00da61da80842238c10d5813d2ae2a08f
winstxnhdw <winstxnhdw@gmail.com>
2024-03-18 00:34:00 +0800

    feat: allow enemies to attack outside

--- lc-hax/Scripts/Patches/PossessionPatch.cs
+++ lc-hax/Scripts/Patches/PossessionPatch.cs
@@ -1,6 +1,8 @@
 #pragma warning disable IDE1006
 
+using GameNetcodeStuff;
 using HarmonyLib;
+using Hax;
 
 [HarmonyPatch]
 class PossessionPatch {
@@ -19,5 +21,28 @@ class PossessionPatch {
 
         ___holdButtonToEndGameEarlyHoldTime = 0.0f;
         __instance.holdButtonToEndGameEarlyMeter?.gameObject.SetActive(false);
+    }
+
+    [HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.PlayerIsTargetable))]
+    static bool Prefix(
+        EnemyAI __instance,
+        PlayerControllerB playerScript,
+        ref bool cannotBeInShip,
+        ref bool overrideInsideFactoryCheck,
+        ref bool __result
+    ) {
+        if (PossessionMod.Instance is { IsPossessed: true } && PossessionMod.Instance.PossessedEnemy == __instance) {
+            if (playerScript.IsDead()) return true;
+
+            __result = true;
+            return false;
+        }
+
+        if (__instance.isOutside != __instance.enemyType.isOutsideEnemy) {
+            overrideInsideFactoryCheck = true;
+            cannotBeInShip = false;
+        }
+
+        return true;
     }
 }
