edb083cea83b2da498c46406605ab1d7d1860758
winstxnhdw <winstxnhdw@gmail.com>
2024-03-19 14:23:28 +0800

    feat/docs: set all placeable objects upright

---
+++ lc-hax/Scripts/Commands/UprightCommand.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Hax;
+
+[Command("upright")]
+class UprightCommand : ICommand {
+    void SetObjectUpright(PlaceableShipObject shipObject) {
+        Vector3 uprightRotation = shipObject.transform.eulerAngles;
+        uprightRotation.x = -90.0f;
+
+        Helper.PlaceObjectAtPosition(shipObject, shipObject.transform.position, uprightRotation);
+    }
+
+    public void Execute(StringArray _) {
+        Helper.FindObjects<PlaceableShipObject>()
+              .ForEach(this.SetObjectUpright);
+    }
+}
