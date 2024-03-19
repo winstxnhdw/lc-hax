74363dc9fa0b7d44c8d379ad4ec4a96cff06be9c
winstxnhdw <winstxnhdw@gmail.com>
2024-03-19 14:22:35 +0800

    fix(jump): fix typing bug with `jump` command

--- lc-hax/Scripts/Patches/UnlimitedJumpPatch.cs
+++ lc-hax/Scripts/Patches/UnlimitedJumpPatch.cs
@@ -10,8 +10,10 @@ using UnityEngine;
 class UnlimitedJumpPatch {
     static bool Prefix(PlayerControllerB __instance) {
         if (!Setting.EnableUnlimitedJump || !__instance.IsSelf()) return true;
-        if (__instance is { inSpecialInteractAnimation: true, isTypingChat: true, isPlayerControlled: false }) return true;
-        if (__instance.quickMenuManager.isMenuOpen) return true;
+        if (!__instance.isPlayerControlled) return false;
+        if (__instance.inSpecialInteractAnimation) return false;
+        if (__instance.isTypingChat) return false;
+        if (__instance.quickMenuManager.isMenuOpen) return false;
 
         __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter - 0.08f, 0.0f, 1.0f);
         __instance.movementAudio.PlayOneShot(StartOfRound.Instance.playerJumpSFX);
