c208d3cd30991f62f13c5281cbf12ef0221ff6da
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-04-23 20:35:03 +0200

    Update NoCooldownPatch.cs

--- lc-hax/Scripts/Patches/NoCooldownPatch.cs
+++ lc-hax/Scripts/Patches/NoCooldownPatch.cs
@@ -7,16 +7,34 @@ using HarmonyLib;
 [HarmonyPatch]
 class NoCooldownPatch {
     [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.RequireCooldown))]
-    static void Postfix(ref bool __result) {
-        if (!Setting.EnableNoCooldown) return;
+    [HarmonyPrefix]
+    static bool GrabbablePostFix(GrabbableObject __instance , ref bool __result) {
+        if (Setting.EnableNoCooldown) {
+            __result = false;
+            return false;
+        }
+
+        if (__instance is Shovel or ShotgunItem or KnifeItem) {
+            return true;
+        }
         __result = false;
+        return false;
     }
 
     [HarmonyPatch(typeof(Shovel), "reelUpShovel")]
-    static IEnumerator Postfix(IEnumerator reelUpShovel) {
+    [HarmonyPostfix]
+    static IEnumerator ShovelPostfix(IEnumerator reelUpShovel) {
         while (reelUpShovel.MoveNext()) {
             if (Setting.EnableNoCooldown && reelUpShovel.Current is WaitForSeconds) continue;
             yield return reelUpShovel.Current;
         }
+    }
+
+    [HarmonyPatch(typeof(InteractTrigger), nameof(InteractTrigger.Interact))]
+    [HarmonyPrefix]
+    static void TriggerPostFix(InteractTrigger __instance) {
+
+        if (__instance.GetComponent<DoorLock>() != null) return;
+        __instance.interactCooldown = false;
     }
 }
