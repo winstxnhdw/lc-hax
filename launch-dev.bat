d008cec98ad3288e14dfa7022b5b7e670836231d
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-05-07 11:44:51 +0200

    From now on, use debug builds.

--- launch-dev.bat
+++ launch-dev.bat
@@ -3,7 +3,7 @@
 set project_name=lc-hax
 
 :begin
-dotnet build %project_name% -c Release --nologo 
+dotnet build %project_name% -c Debug --nologo 
 start /wait /b ./submodules/SharpMonoInjectorCore/dist/SharpMonoInjector.exe inject -p "Lethal Company" -a bin/%project_name%.dll -n Hax -c Loader -m Load
 
 pause
