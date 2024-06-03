d432f427c52cd01dd6c73c9e2524f4888232c4f7
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2024-06-03 13:22:11 +0200

    Make godmode remove the hud effects as well.

--- lc-hax/Scripts/Patches/GodModePatch.cs
+++ lc-hax/Scripts/Patches/GodModePatch.cs
@@ -17,6 +17,13 @@ class GodModePatch {
         __instance.inAnimationWithEnemy = null;
         __instance.inSpecialInteractAnimation = false;
         __instance.health = 100;
+        if (Helper.HUDManager is HUDManager hudManager) {
+            hudManager.localPlayer.playerBodyAnimator.SetBool("Limp", false);
+            hudManager.HUDAnimator.SetBool("biohazardDamage", false);
+            hudManager.HUDAnimator.SetTrigger("HealFromCritical");
+            hudManager.UpdateHealthUI(hudManager.localPlayer.health, false);
+        }
+
         return false;
     }
 }
