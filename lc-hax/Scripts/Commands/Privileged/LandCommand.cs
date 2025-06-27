ef636079da97987b988a9c10f4139ef1d3b4cac9
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-06-27 20:06:38 +0100

    chore(start): return early if travelling to new level (#632)

--- lc-hax/Scripts/Commands/Privileged/LandCommand.cs
+++ lc-hax/Scripts/Commands/Privileged/LandCommand.cs
@@ -7,7 +7,8 @@ class LandCommand : ICommand {
     public async Task Execute(Arguments args, CancellationToken cancellationToken) {
         if (Helper.StartOfRound is not StartOfRound startOfRound) return;
         if (startOfRound.travellingToNewLevel) {
-            Chat.Print("You cannot start the game while travelling to a new level!");
+            Chat.Print("You cannot land while travelling to a new level!");
+            return;
         }
 
         float originalTimeScale = Time.timeScale;
