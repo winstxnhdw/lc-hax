db5be104633c6bbf5552059b07f3697eec948ac1
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-11-17 19:54:50 +0800

    chore: inline redundant field

--- lc-hax/Scripts/Loader.cs
+++ lc-hax/Scripts/Loader.cs
@@ -11,7 +11,6 @@ sealed class Loader : MonoBehaviour {
     const string HarmonyID = "winstxnhdw.lc-hax";
     static GameObject HaxGameObjects { get; } = new("Hax GameObjects");
     static GameObject HaxModules { get; } = new("Hax Modules");
-    static bool HasLoaded => Harmony.HasAnyPatches(Loader.HarmonyID);
     static void AddHaxModules<T>() where T : Component => Loader.HaxModules.AddComponent<T>();
     static void AddHaxGameObject<T>() where T : Component => Loader.HaxGameObjects.AddComponent<T>();
 
@@ -29,7 +28,7 @@ sealed class Loader : MonoBehaviour {
     internal static void Load() {
         Loader.LoadLibraries();
 
-        if (Loader.HasLoaded) {
+        if (Harmony.HasAnyPatches(Loader.HarmonyID)) {
             Logger.Write("lc-hax has already loaded!");
             return;
         }
